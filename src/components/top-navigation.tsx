df8f60a2a5f0838004ad096e8da49ee7f1df6865
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-02-07 21:02:56 +0900

    언어선택 UI

--- src/components/top-navigation.tsx
+++ src/components/top-navigation.tsx
@@ -1,8 +1,15 @@
 const TopNav = () => {
   return (
-    <div className="w-full fixed h-10 bg-slate-50 border-b-2 p-4 flex items-center justify-between">
-      <div>YM Light</div>
+    <div className="w-full fixed h-14 text-white border-b-2 bg-black border-yellow-500 p-4 flex items-center justify-between z-50">
+      <div className="font-bold text-xl">YM Light</div>
       <ul className="divide-x-2 flex [&>li]:px-2">
+        <li className="text-black">
+          <select>
+            <option>한국어</option>
+            <option>English</option>
+            <option>中國語</option>
+          </select>
+        </li>
         <li>Product</li>
         <li>About</li>
         <li>Contanct</li>
