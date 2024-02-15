a1ba9213824439b290eea82738fda1c0528a784c
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-02-15 23:59:56 +0900

    responsive

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Metadata } from "next";
 import Image from "next/image";
 import bannerImage from "@/../public/images/main-banner-1920.jpg";
 import productData from "@/db/productInfo-kor.json";
@@ -30,14 +29,11 @@ type ProductResponse = {
   products: ProductType[];
 };
 
-
-
 export default function Home() {
   const { products }: ProductResponse = productData;
 
   return (
     <div>
-
       <div className="h-[640px] bg-slate-300">
         <div className="relative h-full w-full bg-black">
           <Image
@@ -59,44 +55,16 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="mx-auto  pt-10">
-        <div className="text-2xl font-bold text-white">New Release</div>
-        <div className="flex justify-center pt-5">
-          <div className="grid gap-24 min-[320px]:grid-cols-2 md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 ">
-            {products.map((product, i) => (
-              <div
-                key={product.id}
-                className="h-80 w-64 rounded-xl bg-gray-800 shadow-md"
-              >
-                <div className="space-y-1 p-2">
-                  <div className="relative h-48  rounded-xl">
-                    <Image
-                      src={product1}
-                      fill
-                      style={{ objectFit: "cover", objectPosition: "top" }}
-                      quality={100}
-                      alt="product1"
-                      className="rounded-md"
-                    />
-                  </div>
-                  <div className="flex h-28 items-center justify-center rounded-xl text-white">
-                    {product.name}
-                  </div>
-                </div>
-              </div>
+      <div className="mx-auto pt-10">
+        <div className="flex flex-col items-center justify-center gap-y-10">
+          <div className="text-2xl font-bold text-white">New Release</div>
+          <div className="grid px-5 min-[320px]:grid-cols-2  sm:gap-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-24 gap-10">
+            {products.map((product) => (
+              <Card key={product.id} name={product.name} />
             ))}
-
-            {Array(8)
-              .fill(0)
-              .map((e, i) => (
-                <div key={i}>
-                  <Card />
-                </div>
-              ))}
           </div>
         </div>
       </div>
-      
     </div>
   );
 }
