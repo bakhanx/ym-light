9852b37edef077e13d7116c2778f21c51c1e1297
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-02-16 23:17:49 +0900

    텍스트 애니메이션 적용

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -33,9 +33,9 @@ export default function Home() {
   const { products }: ProductResponse = productData;
 
   return (
-    <div>
-      <div className="h-[640px] bg-slate-300">
-        <div className="relative h-full w-full bg-black">
+    <>
+      <div className="h-[640px]">
+        <div className="relative h-full w-full">
           <Image
             src={bannerImage}
             fill
@@ -43,11 +43,17 @@ export default function Home() {
             quality={100}
             alt="bannerImage"
           />
+
           <div className="absolute h-full w-full bg-black opacity-50" />
 
-          <div className="absolute z-10 flex h-full w-full flex-col justify-center pl-20 text-white">
-            <div className="text-5xl font-bold">YM Light</div>
-            <div className="w-[30%] text-balance pt-2 text-2xl">
+          <div className="absolute z-10 flex h-full flex-col justify-center pl-20 text-white ">
+            <div>
+              <span className="gradient-text text-5xl font-bold animate-gradient">
+                YM Light
+              </span>
+            </div>
+
+            <div className="text-balance pt-2 text-2xl">
               YM Lights are always made by experts with over 30 years of
               experience.
             </div>
@@ -58,13 +64,13 @@ export default function Home() {
       <div className="mx-auto pt-10">
         <div className="flex flex-col items-center justify-center gap-y-10">
           <div className="text-2xl font-bold text-white">New Release</div>
-          <div className="grid px-5 min-[320px]:grid-cols-2  sm:gap-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-24 gap-10">
+          <div className="grid gap-10 px-5  min-[320px]:grid-cols-2 sm:gap-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-24">
             {products.map((product) => (
               <Card key={product.id} name={product.name} />
             ))}
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
