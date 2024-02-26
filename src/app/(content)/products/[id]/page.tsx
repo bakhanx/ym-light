83971664b964923542f37286159626e36c1bcf95
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-02-26 22:38:20 +0900

    상품 정보

--- src/app/(content)/products/[id]/page.tsx
+++ src/app/(content)/products/[id]/page.tsx
@@ -1,3 +1,6 @@
+import productData from "@/db/productInfo-kor.json";
+import { Fragment } from "react";
+
 type Props = {
   params: {
     id: string;
@@ -5,7 +8,32 @@ type Props = {
 };
 
 const ProductDetail = ({ params }: Props) => {
-  return <div>This page is Product {params.id}</div>;
+  const product = productData.products.find(
+    (product) => String(product.id) === params.id,
+  );
+
+  console.log(product);
+
+  return (
+    <>
+      <div>This page is Product {params.id}</div>
+      {product && (
+        <div className="p-2">
+          <div>상품명 : {product.name}</div>
+          <div>가격 : {product.price}</div>
+          <div>색상 : {product.color}</div>
+          <div>성분 : {product.ingradient}</div>
+          <div>
+            사이즈 : Width: {product.size.width} / Height: {product.size.height}
+          </div>
+          <div>전구 : {product.bulb}</div>
+          <div>제조사 : {product.manufacture}</div>
+          <div>설명 : {product.description}</div>
+          <div>이미지 : {product.image}</div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ProductDetail;
