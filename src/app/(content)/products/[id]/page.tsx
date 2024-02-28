89f203a88a9b86fecd2149b3ed00e4fc2fcad2b4
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-02-28 18:44:06 +0900

    레이아웃

--- src/app/(content)/products/[id]/page.tsx
+++ src/app/(content)/products/[id]/page.tsx
@@ -17,15 +17,20 @@ const ProductDetail = ({ params }: Props) => {
   return (
     <>
       <div>This page is Product {params.id}</div>
-      <div className="my-container flex justify-center">
-        <div className="my-content m-auto w-[1280px] max-w-screen-xl px-10 pb-28 pt-8">
-          <div className="my-column_bind flex">
+      <div className="my-container">
+        <div className="my-content m-auto w-[1280px] max-w-screen-xl px-10 pb-28 pt-8 ">
+          <div
+            className="my-column_bind flex divide-x-2 divide-slate-300
+          "
+          >
             <div className="my-column-left w-[50%] pr-10">
               <div className="my-column-box">
                 <div className="my-banner-image ">
-                  <div className="aspect-square w-full bg-slate-500"></div>
+                  <div className="aspect-square w-full bg-slate-500">사진</div>
                 </div>
-                <div className="my-banner-func"></div>
+                <div className="my-banner-func pt-5">
+                  <div className="h-16 w-full bg-slate-500">기능</div>
+                </div>
               </div>
             </div>
 
@@ -34,25 +39,60 @@ const ProductDetail = ({ params }: Props) => {
                 <div className="my-product-info">
                   <div className="p-2">
                     <div className=""></div>
-                    <div>상품명 : {product?.name}</div>
-                    <div>가격 : {product?.price}</div>
-                    <div>색상 : {product?.color}</div>
-                    <div>성분 :{product?.ingradient.join(", ")}</div>
-                    <div>
-                      사이즈 : W: {product?.size.width}, H:{" "}
-                      {product?.size.height}
+                    <div className="text-2xl font-bold">{product?.name}</div>
+                    <div className="pt-5 text-xl font-bold">
+                      {product?.price}원
                     </div>
-                    <div>전구 : {product?.bulb}</div>
-                    <div>제조사 : {product?.manufacture}</div>
-                    <div>설명 : {product?.description}</div>
-                    <div>이미지 : {product?.image}</div>
+                    <div className="flex gap-x-5 pt-20">
+                      <p className="w-12 text-slate-200">색상</p>
+                      <div className="flex items-center gap-x-1">
+                        <div className="h-3 w-3 bg-white"></div>
+                        <p>{product?.color}</p>
+                      </div>
+                    </div>
                   </div>
                 </div>
-                <div className="my-btn-wrap"></div>
+                <div className="my-btn-wrap gap-y-5 flex flex-col mt-40">
+                  <button className="w-full bg-slate-500 p-5 rounded-md">구매하기</button>
+                  <button className="w-full bg-slate-500 p-5 rounded-md">장바구니</button>
+                  <button className="w-full bg-slate-500 p-5 rounded-md">찜하기</button>
+
+                </div>
               </div>
             </div>
           </div>
-          <div className="my-product-detail-item-conent"></div>
+
+          <div className="my-product-detail-item-conent mt-14 ">
+            <div className="my-product-detail-tap-wrap">
+              <div className="my-product-detail-tab flex justify-center gap-x-20 border-b-[1px] border-t-2 border-b-slate-300 py-5">
+                <div>상품 상세정보</div>
+                <div>상품평</div>
+                <div>상품 문의</div>
+                <div>교환 및 반품</div>
+              </div>
+            </div>
+
+            <div className="pt-10">
+              <div className="flex gap-x-5">
+                <p className="w-12 text-slate-200">성분 </p>
+                <div>
+                  <p>{product?.ingradient.join(", ")}</p>
+                </div>
+              </div>
+
+              <div className="flex gap-x-5">
+                <p className="w-12 text-slate-200">사이즈</p>
+                <div className="flex gap-x-2">
+                  <p>W: {product?.size.width}</p>
+                  <p>H: {product?.size.height}</p>
+                </div>
+              </div>
+              <div>전구 : {product?.bulb}</div>
+              <div>제조사 : {product?.manufacture}</div>
+              <div>설명 : {product?.description}</div>
+              <div>이미지 : {product?.image}</div>
+            </div>
+          </div>
         </div>
       </div>
     </>
