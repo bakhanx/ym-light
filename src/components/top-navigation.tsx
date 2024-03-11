186f003696631d76bb69aee8499e734b0de15208
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-03-11 21:24:26 +0900

    장바구니탭

--- src/components/top-navigation.tsx
+++ src/components/top-navigation.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 const TopNav = () => {
   return (
-    <div className="fixed z-50 flex h-14 w-full items-center justify-between border-b-2 border-yellow-500 bg-black py-10 px-20 text-white">
-      <div className="text-2xl font-bold bg-gradient-to-tr from-yellow-500 to-yellow-200 bg-clip-text text-transparent">
+    <div className="fixed z-50 flex h-14 w-full items-center justify-between border-b-2 border-yellow-500 bg-black px-20 py-10 text-white">
+      <div className="bg-gradient-to-tr from-yellow-500 to-yellow-200 bg-clip-text text-2xl font-bold text-transparent">
         <Link href="/">YM Light</Link>
       </div>
       <ul className="flex divide-x-2 [&>li]:px-2">
@@ -25,6 +25,25 @@ const TopNav = () => {
         </li>
         <li>
           <Link href="/faq">FAQ</Link>
+        </li>
+
+        <li>
+          <Link href="/cart">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+              />
+            </svg>
+          </Link>
         </li>
       </ul>
     </div>
