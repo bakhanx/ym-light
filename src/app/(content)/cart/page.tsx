9d86089c8c4906fe3269a1faaeb67231a4403d58
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-03-14 19:29:14 +0900

    장바구니 테스트

--- src/app/(content)/cart/page.tsx
+++ src/app/(content)/cart/page.tsx
@@ -1,60 +1,154 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
+const receivedData = [
+  { id: 0, name: "a", price: 100 },
+  { id: 1, name: "b", price: 200 },
+  { id: 2, name: "c", price: 300 },
+];
 
+type selectedItemType = {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+  totalPrice: number;
+};
 
 const Cart = () => {
-  const [num, setNum] = useState([
-    { id: 1, cnt: 1 },
-    { id: 2, cnt: 10 },
-  ]);
+  const [data, setData] = useState(receivedData);
 
-  const [test, setTest] = useState([{ id: 0 }, { id: 10 }]);
+  const [selectedItemList, setSelectedItemList] = useState<selectedItemType[]>(
+    [],
+  );
+  const [totalItemListPrice, setTotalItemListPrice] = useState(0);
 
-  const plusNum = (id: number) => {
-    setTest((prev) => {
-      let temp = [...prev];
-      temp[0].id = temp[0].id + 1;
-      console.log("a");
-      return temp;
-    });
+  const handleClick = (id: number, e: any) => {
+    const temp = [...quantity];
+    temp[id]++;
+    setQuantity(temp);
+
+    const clickedItem = data.find((v) => v.id === id) as any;
+    const isExistList = selectedItemList.find((v) => v.id === clickedItem.id);
+
+    if (!isExistList) {
+      const newClickedItem = {
+        ...clickedItem,
+        quantity: clickedItem.quantity ? clickedItem?.quantity + 1 : 1,
+        totalPrice: clickedItem.quantity
+          ? clickedItem.price * (clickedItem.quantity + 1)
+          : clickedItem.price,
+      };
+      setSelectedItemList((prev) => [...prev, newClickedItem]);
+    } else {
+      const newClickItem = [...selectedItemList];
+      const foundItem = newClickItem.find((v) => v.id === id);
+      if (foundItem) {
+        foundItem.quantity++;
+        foundItem.totalPrice = foundItem.price * foundItem.quantity;
+      }
+      setSelectedItemList(newClickItem);
+    }
   };
 
-  const handleClick = (
-    id: number,
-    event: React.MouseEvent<HTMLButtonElement>,
-  ) => {
-    console.log("버튼 클릭");
-    console.log(id);
+  const handleButtonClick = (id: number, buttonType: "add" | "substract") => {
+    const newClickItem = [...selectedItemList];
+    const foundItem = newClickItem.find((v) => v.id === id);
 
-    plusNum(id);
+    if (foundItem) {
+      if (buttonType === "add") {
+        foundItem.quantity++;
+        foundItem.totalPrice += foundItem.price;
+        setQuantity((prev) => {
+          const temp = [...prev];
+          temp[id]++;
+          return temp;
+        });
+      } else if (buttonType === "substract") {
+        if (quantity[id] === 1) return;
+        foundItem.quantity--;
+        foundItem.totalPrice -= foundItem.price;
+        setQuantity((prev) => {
+          const temp = [...prev];
+          temp[id]--;
+          return temp;
+        });
+      }
+    }
+    setSelectedItemList(newClickItem);
   };
+
+  const [quantity, setQuantity] = useState(Array(data.length).fill(0));
+
+  const handleChange = (id: number, e: any) => {
+    if (e.target.value < 1) {
+      return;
+    }
+    
+    const temp = [...quantity];
+    temp[id] = e.target.value;
+    setQuantity(temp);
+  };
+
+  const handleBlur = (id: number, e: any) => {
+    const newClickItem = [...selectedItemList];
+    const foundItem = newClickItem.find((v) => v.id === id);
+    if (foundItem) {
+      foundItem.quantity = e.target.value;
+      foundItem.totalPrice = foundItem.price * foundItem.quantity;
+    }
+    setSelectedItemList(newClickItem);
+  };
+
+  useEffect(() => {
+    setTotalItemListPrice(
+      selectedItemList
+        .map((item) => item.totalPrice)
+        .reduce((acc, cur) => acc + cur, 0),
+    );
+  }, [selectedItemList]);
 
   return (
-    <div className="p-10 pt-28">
-      <div key={0}>
-        <button
-          className="bg-blue-500 text-white"
-          onClick={(e) => handleClick(0, e)}
-        >
-          버튼1
-        </button>
-        <div>{num[0].cnt}</div>
-      </div>
-
-      <div key={1}>
-        <button
-          className="bg-blue-500 text-white"
-          onClick={(e) => handleClick(1, e)}
-        >
-          버튼2
-        </button>
-        <div>{num[1].cnt}</div>
+    <div className="flex flex-col gap-x-2 p-10 pt-28">
+      <div className="w-20 border">옵션 선택</div>
+      <div className="w-20 border">
+        {data.map((item) => (
+          <div key={item.id} onClick={(e) => handleClick(item.id, e)}>
+            {item.name}
+          </div>
+        ))}
       </div>
 
       <div>
-        Test : {test[0].id} {test[1].id}
+        {selectedItemList.map((item) => (
+          <div key={item.id}>
+            {item.id} : {item.name}, {item.price}, quantity : {item.quantity},
+            totalPrice :{item.totalPrice}
+            <button
+              className="border p-2"
+              onClick={() => handleButtonClick(item.id, "substract")}
+            >
+              -
+            </button>
+            <input
+              type="number"
+              className="border"
+              min={1}
+              value={quantity[item.id]}
+              onChange={(e) => handleChange(item.id, e)}
+              onBlur={(e) => handleBlur(item.id, e)}
+            />
+            <button
+              className="border p-2"
+              onClick={() => handleButtonClick(item.id, "add")}
+            >
+              +
+            </button>
+          </div>
+        ))}
       </div>
+
+      <div>총 가격 :{totalItemListPrice}</div>
     </div>
   );
 };
