79b069088b33a94d88326086fdd5a4922b9d59cb
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-03-20 16:37:51 +0900

    버튼 구조 변경

--- src/app/(content)/cart/_components.tsx/products.tsx
+++ src/app/(content)/cart/_components.tsx/products.tsx
@@ -7,38 +7,41 @@ const Products = () => {
   const handleSelectAllClick = () => {
     setisSelectAllClick((prev) => !prev);
   };
-  
+
   return (
-    <div className="flex relative pl-4">
-      <button
-        className={cls(
-          isSelectAllClick
-            ? "border-amber-400 bg-amber-400 text-white"
-            : " border-amber-400 text-gray-400 ",
-          "absolute flex items-center gap-x-1 rounded-md border-2 p-1 top-10",
-        )}
-        onClick={handleSelectAllClick}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={3}
-          stroke="currentColor"
-          className="h-3 w-3"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="m4.5 12.75 6 6 9-13.5"
-          />
-        </svg>
-      </button>
+    <div className="flex pl-4">
       <div className="flex justify-center divide-x-[1px]">
-        <div className="flex w-[848px] p-10">
+        <div className="relative flex w-[848px] p-10">
+
+          <button
+            className={cls(
+              isSelectAllClick
+                ? "border-amber-400 bg-amber-400 text-white"
+                : " border-amber-400 text-gray-400 ",
+              "absolute left-0 top-10 flex items-center gap-x-1 rounded-md border-2 p-1",
+            )}
+            onClick={handleSelectAllClick}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={3}
+              stroke="currentColor"
+              className="h-3 w-3"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m4.5 12.75 6 6 9-13.5"
+              />
+            </svg>
+          </button>
+
           <div className="relative">
             <div className="h-20 w-20 bg-slate-500"></div>
           </div>
+          
           <div className="flex flex-col px-10 ">
             <strong>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab earum
@@ -50,6 +53,23 @@ const Products = () => {
               <li>02. This is a option.</li>
             </ul>
           </div>
+
+          <button className="absolute right-6 text-gray-400">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18 18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
         </div>
 
         <div className="flex w-[200px] items-center justify-center p-5">
