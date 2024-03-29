5355d1787901b1d8adc92c1168d3360250e0db59
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-03-29 22:31:37 +0900

    heroicon/react

--- src/app/(content)/cart/page.tsx
+++ src/app/(content)/cart/page.tsx
@@ -3,6 +3,9 @@
 import { cls } from "@/libs/utils";
 import { useState } from "react";
 import Products from "./_components.tsx/products";
+import Link from "next/link";
+import { CheckIcon, TrashIcon } from "@heroicons/react/24/outline";
+
 
 const Cart = () => {
   const [totalQuantity, setTotalQuantity] = useState(3);
@@ -49,20 +52,7 @@ const Cart = () => {
                     )}
                     onClick={handleSelectAllClick}
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={3}
-                      stroke="currentColor"
-                      className="h-3 w-3"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="m4.5 12.75 6 6 9-13.5"
-                      />
-                    </svg>
+                     <CheckIcon className="h-3 w-3 stroke-[3px] " />
 
                     <span>전체선택</span>
                   </button>
@@ -70,20 +60,7 @@ const Cart = () => {
                 <div className="px-2">
                   <button className="flex items-center rounded-md border-2 p-1 text-gray-600">
                     <span>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="h-4 w-4"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                        />
-                      </svg>
+                      <TrashIcon className="h-4 w-4" />
                     </span>
                     <span>선택삭제</span>
                   </button>
@@ -108,20 +85,7 @@ const Cart = () => {
                   )}
                   onClick={handleSelectAllClick}
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={3}
-                    stroke="currentColor"
-                    className="h-3 w-3"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="m4.5 12.75 6 6 9-13.5"
-                    />
-                  </svg>
+                  <CheckIcon className="h-3 w-3 stroke-[3px] " />
                 </button>
                 <span className="w-[848px]">상품정보</span>
                 <span className="w-[200px]">상품금액</span>
@@ -138,7 +102,7 @@ const Cart = () => {
               ))}
 
             {/* Total */}
-            <div className="flex px-4 py-6 border-b">
+            <div className="flex border-b px-4 py-6">
               <div className="flex w-[848px] items-center justify-center gap-x-16 border-r">
                 <div className="flex flex-col">
                   <span className="text-center text-gray-400">상품금액</span>
