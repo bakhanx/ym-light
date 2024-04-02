6b652810c9100476717e53c41b110f1d7e6ec37b
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-04-02 18:22:57 +0900

    props ...rest 설정

--- src/components/form-input.tsx
+++ src/components/form-input.tsx
@@ -1,48 +1,43 @@
-import React from "react";
-import { text } from "stream/consumers";
+import React, { InputHTMLAttributes, TextareaHTMLAttributes } from "react";
 
 type FormInputType = {
+  name: string;
   label: string;
-  type: string;
-  name:string;
-  placeholder: string;
   error?: string[];
-  requried?: boolean;
   textarea?: boolean;
-  
 };
 
 const FormInput = ({
   label,
-  type,
-  placeholder,
-  error = [],
   name,
-  requried = false,
+  error = [],
+  required = false,
   textarea = false,
-}: FormInputType) => {
+  ...rest
+}: FormInputType &
+  InputHTMLAttributes<HTMLInputElement> &
+  TextareaHTMLAttributes<HTMLTextAreaElement>) => {
   return (
     <div>
       <label className="flex flex-col">
         <div className="flex gap-x-1">
           {label}
-          {requried && <span className="text-red-500">*</span>}
+          {required && <span className="text-red-500">*</span>}
         </div>
 
         {textarea ? (
           <textarea
-          name={name}
-            required={requried}
-            className="h-28 resize-none rounded-sm p-2 text-black focus:outline-none focus:ring-4 transition focus:ring-amber-500"
-            placeholder={placeholder}
+            name={name}
+            required={required}
+            className="h-28 resize-none rounded-sm p-2 text-black transition focus:outline-none focus:ring-4 focus:ring-amber-500"
+            {...rest}
           />
         ) : (
           <input
-            type={type}
             name={name}
-            required={requried}
-            placeholder={placeholder}
-            className="rounded-sm p-2 text-black focus:outline-none focus:ring-4 transition focus:ring-amber-500"
+            required={required}
+            className="rounded-sm p-2 text-black transition focus:outline-none focus:ring-4 focus:ring-amber-500"
+            {...rest}
           />
         )}
         <span className="w-full text-amber-500">{error}</span>
