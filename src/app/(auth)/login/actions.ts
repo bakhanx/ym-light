01e211b63648b8ba6e3ae50e6f6094cc2757f906
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-04-03 23:50:44 +0900

    required error

--- src/app/(auth)/login/actions.ts
+++ src/app/(auth)/login/actions.ts
@@ -1,22 +1,35 @@
-import {z} from "zod"
+import {
+  PASSWORD_REGEX,
+  PASSWORD_REGEX_ERROR,
+  PASSWORD_REQUIRED_ERROR,
+  USERID_REQUIRED_ERROR,
+} from "@/libs/constants";
+import { z } from "zod";
+
+const loginSchema = z.object({
+  userId: z
+    .string({
+      required_error: USERID_REQUIRED_ERROR,
+    })
+    .toLowerCase(),
+  password: z.string({
+    required_error: PASSWORD_REQUIRED_ERROR,
+  }),
+});
 
 
-const registerSchema = z.object({
-    userId : z.string().toLowerCase(),
-    password: z.string()
-})
 
-export const loginActions = async (prevState :any, formData:FormData)=>{
-    const data = {
-        userId : formData.get("userId"),
-        password: formData.get("password",)
-    }
+export const loginActions = async (prevState: any, formData: FormData) => {
+  const data = {
+    userId: formData.get("userId"),
+    password: formData.get("password"),
+  };
 
-    const result = registerSchema.safeParse(data);
-    if(!result.success){
-        return result.error.flatten();
-    }
-    else{
-        console.log(result.data);
-    }
-}
+  const result = loginSchema.safeParse(data);
+  if (!result.success) {
+    console.log(result.error.flatten());
+    return result.error.flatten();
+  } else {
+    console.log(result.data);
+  }
+};
