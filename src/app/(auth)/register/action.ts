b34af7d3201db9c4ac414496f6ad58ae3638de45
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-04-05 21:59:35 +0900

    토큰 state

--- src/app/(auth)/register/action.ts
+++ src/app/(auth)/register/action.ts
@@ -35,31 +35,55 @@ const registerSchema = z
     phone: z
       .string()
       .trim()
-      .refine((phone) => {
-        if (phone === "") {
+      .refine(
+        (phone) => {
+          if (phone === "") {
+            return true;
+          }
+          if (!validator.isMobilePhone(phone || "", "ko-KR")) {
+            return false;
+          }
           return true;
-        }
-        if (!validator.isMobilePhone(phone || "", "ko-KR")) {
-          return false;
-        }
-        return true;
-      }, {message:"유효하지 않은 번호입니다."}),
+        },
+        { message: "유효하지 않은 번호입니다." },
+      ),
 
     email: z.string(),
+    token: z.coerce
+      .number({
+        required_error: "인증번호를 입력하세요.",
+      })
+      .min(10000, "6자리 입력해주세요")
+      .max(99999, "6자리 입력해주세요").optional(),
   })
   .refine(checkPassowrd, {
     message: PASSWORD_CONFIRM_ERROR,
     path: ["password_confirm"],
   });
 
-const tokenSchema = z.coerce
-  .number({
-    required_error: "인증번호를 입력하세요.",
-  })
-  .min(10000)
-  .max(99999);
+// const tokenSchema = z.coerce
+//   .number({
+//     required_error: "인증번호를 입력하세요.",
+//   })
+//   .min(10000)
+//   .max(99999);
 
-export const registerAction = (prevState: any, formData: FormData) => {
+// export const tokenAction = (prevState: any, formData: FormData) => {
+//   const token = formData.get("token");
+
+//   if (!prevState.token) {
+//     const result = tokenSchema.safeParse(token);
+//     if (!result.success) {
+//       return {
+//         token: false,
+//       };
+//     } else {
+//       return { token: true };
+//     }
+//   }
+// };
+
+export const registerAction = async (prevState: any, formData: FormData) => {
   const data = {
     userId: formData.get("userId"),
     password: formData.get("password"),
@@ -69,17 +93,19 @@ export const registerAction = (prevState: any, formData: FormData) => {
     email: formData.get("email"),
   };
 
-  const data2 = {
-    token: formData.get("token"),
-  };
+  const token = formData.get("token");
 
-  const result = registerSchema.safeParse(data);
-
-  const result2 = tokenSchema.safeParse(data2);
-
-  if (!result.success) {
-    return result.error.flatten();
-  } else {
-    console.log(result.data);
+  if (!prevState.token) {
+    const result = registerSchema.safeParse(data);
+    if (!result.success) {
+      return { token: false, error: result.error.flatten() };
+    } else {
+      const result = await registerSchema.spa(token);
+      console.log("torken spa : ",result);
+      if (!result.success) {
+        return { token: true, error: result.error.flatten() };
+      } else {
+      }
+    }
   }
 };
