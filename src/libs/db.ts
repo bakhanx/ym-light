148ee89908a0f8e2a9b3e3bb5320c6e5b2d05f84
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-04-12 17:23:05 +0900

    dummy 제거

--- src/libs/db.ts
+++ src/libs/db.ts
@@ -2,29 +2,4 @@ import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
 
-const createUser = async () => {
-  const user = await db.user.create({
-    data: {
-      loginId: "asd",
-      username: "test",
-      password: "1234",
-    },
-  });
-};
-const createToken = async () => {
-  const token = await db.sMSToken.create({
-    data: {
-      token: "1234",
-      user: {
-        connect: {
-          id: 1,
-        },
-      },
-    },
-  });
-};
-
-// createUser();
-// createToken();
-
 export default db;
