a94760f30e0108280c5c9bc21d637089510f3a61
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-04-17 17:42:37 +0900

    로그인 유무 접근 제한

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,14 +2,25 @@ import { NextRequest, NextResponse } from "next/server";
 import React from "react";
 import getSession from "./libs/session";
 
+const publicPaths = new Set(["/login", "/register"]);
+const privatePaths = new Set(["/profile"]);
+
 const middleware = async (req: NextRequest) => {
   const session = await getSession();
-
-  if (req.nextUrl.pathname === "/profile") {
-    if (!session.id) {
-      return NextResponse.redirect(new URL("/", req.url));
-    }
+  const isPublicPath = publicPaths.has(req.nextUrl.pathname);
+  const isPrivatePath = privatePaths.has(req.nextUrl.pathname);
+  // 비로그인 유저 접근 제한
+  if (!session.id && isPrivatePath) {
+    return NextResponse.redirect(new URL("/", req.url));
   }
+  // 로그인 유저 접근 제한
+  else if (session.id && isPublicPath) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+};
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
 
 export default middleware;
