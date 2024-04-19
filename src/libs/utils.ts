7ea5216ea9e251b7de5c3fdf5577c67f446200ac
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-04-19 20:57:37 +0900

    가격 변환

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,3 +1,9 @@
-export function cls(...classnames : string[]){
-    return classnames.join(" ");
+export function cls(...classnames: string[]) {
+  return classnames.join(" ");
 }
+
+type localeType = "ko-KR" | "en-US" | "ja-JP" | "zh-CN";
+
+export const formatOfPrice = (price: number, locale: localeType ="ko-KR") => {
+  return price.toLocaleString(locale);
+};
