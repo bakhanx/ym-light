fc8957cfe95162020779701e173b9c05a9835751
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-04-24 21:00:45 +0900

    admin

--- src/components/top-navigation.tsx
+++ src/components/top-navigation.tsx
@@ -56,6 +56,10 @@ const TopNav = async () => {
         <li>
           <Link href="/faq">FAQ</Link>
         </li>
+        <li>
+          <Link href="/manage">Admin</Link>
+        </li>
+        
 
         {user ? (
           <>
