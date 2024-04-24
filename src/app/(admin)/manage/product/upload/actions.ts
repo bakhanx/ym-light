146d9af94d9e079ca9e8ad5d9e9009a9e6259930
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-04-24 21:00:18 +0900

    상품 업로드

---
+++ src/app/(admin)/manage/product/upload/actions.ts
@@ -0,0 +1,72 @@
+"use server";
+
+import db from "@/libs/db";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+const productSchema = z.object({
+  name: z.string({ required_error: "상품명을 입력해주세요." }),
+  price: z.coerce.number({ required_error: "가격을 입력해주세요." }),
+  discount: z.coerce.number().optional(),
+  color: z.string().optional(),
+  material: z.string().optional(),
+  size: z.string().optional(),
+  bulb: z.string({ required_error: "전구타입을 입력해주세요." }),
+  manufacturer: z.string({ required_error: "제조사를 입력해주세요." }),
+  description: z.string().optional(),
+  options: z.string().optional(),
+  photo0: z.string({ required_error: "사진을 등록해주세요." }),
+  photo1: z.string().optional(),
+  photo2: z.string().optional(),
+  photo3: z.string().optional(),
+});
+
+const uploadProduct = async (_:any , formData: FormData) => {
+  const data = {
+    name: formData.get("name"),
+    price: formData.get("price"),
+    discount: formData.get("discount"),
+    color: formData.get("color"),
+    material: formData.get("material"),
+    size: formData.get("size"),
+    bulb: formData.get("bulb"),
+    manufacturer: formData.get("manufacturer"),
+    description: formData.get("description"),
+    options: formData.get("options"),
+    photo0: formData.get("photo0"),
+    photo1: formData.get("photo1"),
+    photo2: formData.get("photo2"),
+    photo3: formData.get("photo3"),
+  };
+
+  const result = productSchema.safeParse(data);
+  if (!result.success) {
+    return result.error.flatten();
+  } else {
+    const product = await db.product.create({
+      data: {
+        title: result.data.name,
+        price: result.data.price,
+        discount: result.data.discount || 0,
+        color: result.data.color || "",
+        material: result.data.material || "",
+        size: result.data.size || "",
+        bulb: result.data.bulb,
+        manufacturer: result.data.manufacturer,
+        description: result.data.description || "",
+        options: result.data.options || "",
+        photo: result.data.photo0,
+
+        purchaser: undefined,
+        userId: 0,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    redirect(`/prducts/${product.id}`);
+  }
+};
+
+export default uploadProduct;
