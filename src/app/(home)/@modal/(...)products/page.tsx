11ababc3592e6b371f3c3f22e21c3f3cb1765ef9
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-05-01 18:35:40 +0900

    Modal UI

---
+++ src/app/(home)/@modal/(...)products/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Modal = () => {
+  return (
+    <div className="absolute z-50 h-full w-full bg-black opacity-60">
+      <div>Products</div>
+    </div>
+  );
+};
+
+export default Modal;
