ff79c0ae86a041f288c58520ce077495a38ab19a
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-05-02 16:23:11 +0900

    Top navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className}`}>
+        <TopNav />
         <div className="">{children}</div>
       </body>
     </html>
