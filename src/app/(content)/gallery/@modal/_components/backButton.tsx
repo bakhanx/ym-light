cb67c023d7edab571f53b7f7c97620c5dc20b66e
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-05-03 16:22:39 +0900

    디렉토리 이동

--- src/app/(content)/gallery/@modal/_components/backButton.tsx
+++ src/app/(content)/gallery/@modal/_components/backButton.tsx
@@ -12,7 +12,7 @@ const BackButton = () => {
   return (
     <button
       onClick={onCloseClick}
-      className="absolute right-5 top-5 text-neutral-200"
+      className="absolute right-5 top-5 z-50 text-neutral-200"
     >
       <XMarkIcon className="size-10" />
     </button>
