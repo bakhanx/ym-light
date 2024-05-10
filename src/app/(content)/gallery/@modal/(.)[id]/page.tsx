ec065324cd6e1cd684f1170354cd18a47b767571
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-05-10 21:08:04 +0900

    UI

--- src/app/(content)/gallery/@modal/(.)[id]/page.tsx
+++ src/app/(content)/gallery/@modal/(.)[id]/page.tsx
@@ -1,19 +1,34 @@
-import { PhotoIcon } from "@heroicons/react/16/solid";
+import { BoltIcon, PhotoIcon } from "@heroicons/react/16/solid";
 import { notFound } from "next/navigation";
-import { unstable_cache as nextCache } from "next/cache";
+
 import React from "react";
 import db from "@/libs/db";
 
 import Image from "next/image";
-import Link from "next/link";
-import EnterButton from "../_components/enterButton";
+
 import BackButton from "../_components/backButton";
+import DateTime from "@/components/datetime";
 
 type Props = {
   params: {
     id: string;
   };
 };
+
+` getGallery Schema
+id
+writer
+title
+photo
+content
+created_at (time ago)
+isUpdated
+likes
+comments
+
+link (첨부 링크)
+`
+
 
 const getProduct = async (id: number) => {
   const product = await db.product.findUnique({
@@ -25,6 +40,7 @@ const getProduct = async (id: number) => {
       title: true,
       photo: true,
       price: true,
+      created_at: true,
     },
   });
   return product;
@@ -44,7 +60,7 @@ const Modal = async ({ params }: Props) => {
 
   return (
     <div className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center">
-      <div className="absolute flex h-[75%] w-[50%] flex-col  justify-center bg-slate-50 bg-opacity-50">
+      <div className="absolute flex h-[75%] w-[50%] flex-col  justify-center bg-slate-50 bg-opacity-60">
         <div className="flex h-full w-full items-center justify-center shadow-xl">
           <BackButton />
           <div className="flex h-full w-full max-w-screen-sm  flex-col justify-start gap-x-5">
@@ -63,10 +79,19 @@ const Modal = async ({ params }: Props) => {
             </div>
 
             {/* Text */}
-            <div className="flex flex-col p-2">
+            <div className="flex flex-col p-3 text-sm">
               <ul>
-                <li>상품명 : {product.title}</li>
+                <li className="font-semibold flex items-center gap-x-1">
+                  YM Light
+                  <div className="flex h-3 w-3 items-center justify-center rounded-full bg-black">
+                    <BoltIcon className="rounded-ful h-3 w-3 text-amber-300" />
+                  </div>
+                </li>
+                <li>{product.title}</li>
                 <li>가격 : {product.price}</li>
+                <li>
+                  <DateTime date={product.created_at} />{" "}
+                </li>
               </ul>
             </div>
           </div>
