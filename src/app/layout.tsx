40587320d03678cbfd77b050a39a2d8f8e4aea3d
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-05-13 18:54:47 +0900

    폰트변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Noto_Sans_KR } from "next/font/google";
 import "../styles/globals.css";
 import TopNav from "@/components/top-navigation";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
+
+const noto_sans_kr = Noto_Sans_KR({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+  style: ["normal"],
+});
 
 export const metadata: Metadata = {
   title: {
@@ -21,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>
+      <body className={`${noto_sans_kr.className}`}>
         <TopNav />
         <div className="">{children}</div>
       </body>
