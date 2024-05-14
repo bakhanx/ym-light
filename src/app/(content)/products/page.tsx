90417b8e5378b93d3f9141094dbd09d81dcf5d96
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-05-14 17:44:50 +0900

    상품 UI

--- src/app/(content)/products/page.tsx
+++ src/app/(content)/products/page.tsx
@@ -25,51 +25,55 @@ const Product = async () => {
   const products = await getProducts();
 
   return (
-    <div className="pt-20 max-w-screen-2xl mx-auto " >
+    <div className="mx-auto max-w-screen-2xl pt-20 ">
       <div>등록된 상품</div>
       <div className="grid grid-cols-4 gap-5">
         {products.map((product, index) => (
-          <Link key={product.id} href={`products/${product.id}`} className="hover:border border-orange-600" >
-          <div className="flex flex-col p-2">
-            <div className="">
-              <div className="pb-2 text-xl font-bold text-amber-600">
-                {index + 1}
-              </div>
-              <div className="relative aspect-square w-full rounded-md overflow-hidden">
-                <Image
-                  alt={product.title}
-                  src={`${product.photo}/sharpen=1,fit=scale-down,w=640`}
-                  fill
-                  objectFit="cover"
-                />
-              </div>
-              <p className="text-lg">{product.title}</p>
+          <Link
+            key={product.id}
+            href={`products/${product.id}`}
+            className="border-orange-600 hover:border"
+          >
+            <div className="flex flex-col p-2">
               <div className="">
-                {product.discount ? (
-                  <>
-                    <div className="text-gray-500 line-through ">
+                <div className="pb-2 text-xl font-bold text-amber-600">
+                  {index + 1}
+                </div>
+                <div className="relative aspect-square w-full overflow-hidden rounded-md">
+                  <Image
+                    alt={product.title}
+                    src={`${product.photo}/sharpen=1,fit=scale-down,w=640`}
+                    fill
+                    objectFit="cover"
+                  />
+                </div>
+                <p className="text-lg">{product.title}</p>
+                <div className="">
+                  {product.discount ? (
+                    <>
+                      <div className="text-gray-500 line-through ">
+                        {formatOfPrice(product.price)}원
+                      </div>
+                      <div className="flex gap-x-2  text-lg">
+                        <span className="font-bold text-red-600">
+                          {product.discount}%
+                        </span>
+                        <span className="font-semibold">
+                          {formatOfPrice(
+                            product?.price * ((100 - product.discount) / 100),
+                          )}
+                          원
+                        </span>
+                      </div>
+                    </>
+                  ) : (
+                    <div className="text-lg font-semibold">
                       {formatOfPrice(product.price)}원
                     </div>
-                    <div className="flex gap-x-2  text-lg">
-                      <span className="font-bold text-red-600">
-                        {product.discount}%
-                      </span>
-                      <span className="font-semibold">
-                        {formatOfPrice(
-                          product?.price * ((100 - product.discount) / 100),
-                        )}
-                        원
-                      </span>
-                    </div>
-                  </>
-                ) : (
-                  <div className="text-lg font-semibold">
-                    {formatOfPrice(product.price)}원
-                  </div>
-                )}
+                  )}
+                </div>
               </div>
             </div>
-          </div>
           </Link>
         ))}
       </div>
