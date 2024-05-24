ed4502f6e23ef974219e0c6d466208bb60216255
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-05-24 17:18:38 +0900

    갤러리 관리 추가

--- src/app/(admin)/manage/page.tsx
+++ src/app/(admin)/manage/page.tsx
@@ -1,18 +1,17 @@
-"use client"
+"use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import React from "react";
 
-
 const Manage = () => {
-  const pathName = usePathname();
-  console.log(pathName)
   return (
     <div className="pt-20">
       <div className="flex gap-x-2 p-10">
         <button className="boerder-sm border border-blue-500 p-2">
-          <Link href={`${pathName}/product`}>상품관리</Link>
+          <Link href={`/manage/product`}>상품관리</Link>
+        </button>
+        <button className="boerder-sm border border-blue-500 p-2">
+          <Link href={`/manage/gallery`}>갤러리관리</Link>
         </button>
         <button className="boerder-sm border border-blue-500 p-2">
           소개관리
