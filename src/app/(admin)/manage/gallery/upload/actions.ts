1ad344457589c2b3cb5658f06f144727df5aaa2d
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-05-27 11:27:49 +0900

    갤러리 업로드용

--- src/app/(admin)/manage/gallery/upload/actions.ts
+++ src/app/(admin)/manage/gallery/upload/actions.ts
@@ -1,20 +1,25 @@
 "use server";
+import db from "@/libs/db";
+import { revalidatePath, revalidateTag } from "next/cache";
+import { redirect } from "next/navigation";
 import { z } from "zod";
 
 const formSchema = z.object({
   content: z
-    .string()
+    .string({required_error : "내용을 입력해주세요."})
     .min(1, "내용을 입력해주세요.")
     .max(1000, "글자 수 한도를 초과하였습니다."),
+  photo: z.string({ required_error: "사진을 등록해주세요." }),
   tag: z
     .string()
     .min(1, "내용을 입력해주세요.")
     .max(30, "태그 수가 너무 많습니다."),
 });
 
-export const handleForm = async (prevState: any, formData: FormData) => {
+export const uploadGallery = async (formData: FormData, galleryId?: number) => {
   const data = {
     content: formData.get("content"),
+    photo0: formData.get("photo"),
     tag: formData.get("tag"),
   };
 
@@ -22,6 +27,57 @@ export const handleForm = async (prevState: any, formData: FormData) => {
   if (!result.success) {
     return result.error.flatten();
   } else {
+    const isExistsTag = Boolean(
+      await db.tag.findUnique({
+        where: {
+          name: result.data.tag,
+        },
+      }),
+    );
+
+    if (!isExistsTag) {
+      await db.tag.create({
+        data: {
+          name: result.data.tag,
+        },
+      });
+    }
+
+    const gallery = galleryId
+      ? await db.gallery.update({
+          where: {
+            id: Number(galleryId),
+          },
+          data: {
+            content: result.data.content,
+            tags: {
+              connect: {
+                name: result.data.tag,
+              },
+            },
+            photo: result.data.photo,
+          },
+          select: {
+            id: true,
+          },
+        })
+      : await db.gallery.create({
+          data: {
+            content: result.data.content,
+            tags: {
+              connect: {
+                name: result.data.tag,
+              },
+            },
+            photo: result.data.photo,
+          },
+          select: {
+            id: true,
+          },
+        });
     console.log(result.data);
+    revalidateTag("gallery");
+    revalidatePath(`gallery/${gallery.id}`);
+    redirect(`/gallery`);
   }
 };
