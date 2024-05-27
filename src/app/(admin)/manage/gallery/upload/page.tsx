2f2e15232ab63ef7ab5123ef16d5ccfce10f90ef
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-05-27 14:02:44 +0900

    form 범위 확장

--- src/app/(admin)/manage/gallery/upload/page.tsx
+++ src/app/(admin)/manage/gallery/upload/page.tsx
@@ -87,16 +87,16 @@ const Upload = () => {
 
   return (
     <div className="h-screen border-2 border-blue-500 pt-20">
-      <div className="mx-auto flex h-full max-w-screen-xl items-center">
-        <form action={action}>
+      <div className="m-auto flex h-full max-w-screen-xl items-center">
+        <form action={action} className="w-full">
           <div className="flex w-full px-10">
             {/* 미리보기 */}
-            <div className="left flex  w-[50%] border-2 border-gray-300">
+            <div className="left flex w-[50%] border-2 border-gray-300">
               <div className="w-full border p-10">
                 {/* Image */}
                 {/* <div className="relative">
                 <PhotoIcon className="w-full" />
-              </div> */}
+                </div> */}
 
                 <div className="my-banner-image">
                   <label
@@ -132,7 +132,7 @@ const Upload = () => {
                 {/* Text */}
                 <div className="flex flex-col p-3 ">
                   <ul>
-                    <li className="flex items-center gap-x-1">
+                    <li className="flex items-center gap-x-1 w-full">
                       <div className="font-semibold">YM Light</div>
                       <div className="flex h-4 w-4 items-center justify-center rounded-full bg-black">
                         <BoltIcon className="h-3 w-3 rounded-full text-amber-300" />
@@ -141,9 +141,10 @@ const Upload = () => {
                       <div>·</div>
                       <div className="text-gray-500">2024-00-00</div>
                     </li>
-                    <li className="pt-2">{formValue[0]}</li>
 
-                    <li className="pt-5 font-semibold text-blue-600">
+                    <li className="pt-2 h-24">{formValue[0]}</li>
+
+                    <li className="pt-5 w-full h-10 font-semibold text-blue-600">
                       {formValue[1]}
                     </li>
                   </ul>
@@ -161,37 +162,35 @@ const Upload = () => {
                 </p>
 
                 <div className="w-full pt-5">
-                  <form action={action}>
-                    <div className="flex flex-col gap-y-5">
-                      <FormInput
-                        label="내용"
-                        name="content"
-                        type="text"
-                        placeholder="산뜻한 하루의 시작"
-                        error={state?.fieldErrors.content}
-                        required
-                        textarea
-                        minLength={1}
-                        maxLength={1000}
-                        onBlur={(e) => handleBlur(e, 0)}
-                        className="resize-none border-2 p-2"
-                      />
-                      <FormInput
-                        label="태그"
-                        name="tag"
-                        type="text"
-                        placeholder="#명품"
-                        error={state?.fieldErrors.tag}
-                        maxLength={30}
-                        onBlur={(e) => handleBlur(e, 1)}
-                        className="h-10 border-2 p-2"
-                      />
-                    </div>
+                  <div className="flex flex-col gap-y-5">
+                    <FormInput
+                      label="내용"
+                      name="content"
+                      type="text"
+                      placeholder="산뜻한 하루의 시작"
+                      error={state?.fieldErrors.content}
+                      required
+                      textarea
+                      minLength={1}
+                      maxLength={1000}
+                      onBlur={(e) => handleBlur(e, 0)}
+                      className="resize-none border-2 p-2"
+                    />
+                    <FormInput
+                      label="태그"
+                      name="tag"
+                      type="text"
+                      placeholder="#명품"
+                      error={state?.fieldErrors.tag}
+                      maxLength={30}
+                      onBlur={(e) => handleBlur(e, 1)}
+                      className="h-10 border-2 p-2"
+                    />
+                  </div>
 
-                    <div className="pt-20">
-                      <FormButton name="업로드 하기" />
-                    </div>
-                  </form>
+                  <div className="pt-20">
+                    <FormButton name="업로드 하기" />
+                  </div>
                 </div>
               </div>
             </div>
