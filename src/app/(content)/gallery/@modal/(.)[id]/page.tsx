6c25bc531184a0270251c6ed597dd0866d8ec3b0
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-05-29 16:51:15 +0900

    UI 수정

--- src/app/(content)/gallery/@modal/(.)[id]/page.tsx
+++ src/app/(content)/gallery/@modal/(.)[id]/page.tsx
@@ -8,6 +8,10 @@ import Image from "next/image";
 
 import BackButton from "../_components/backButton";
 import DateTime from "@/components/datetime";
+import {
+  ChatBubbleLeftEllipsisIcon,
+  HandThumbUpIcon,
+} from "@heroicons/react/24/outline";
 
 type Props = {
   params: {
@@ -18,7 +22,6 @@ type Props = {
 ` getGallery Schema
 id
 writer
-title
 photo
 content
 created_at (time ago)
@@ -27,23 +30,22 @@ likes
 comments
 
 link (첨부 링크)
-`
+`;
 
-
-const getProduct = async (id: number) => {
-  const product = await db.product.findUnique({
+const getGallery = async (id: number) => {
+  const gallery = await db.gallery.findUnique({
     where: {
       id,
     },
     select: {
       id: true,
-      title: true,
+      content: true,
       photo: true,
-      price: true,
       created_at: true,
+      updated_at: true,
     },
   });
-  return product;
+  return gallery;
 };
 
 // const getCachedProduct = nextCache(
@@ -53,23 +55,24 @@ const getProduct = async (id: number) => {
 // );
 
 const Modal = async ({ params }: Props) => {
-  const product = await getProduct(Number(params.id));
-  if (!product) {
+  const gallery = await getGallery(Number(params.id));
+  if (!gallery) {
     return notFound();
   }
 
   return (
     <div className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center">
-      <div className="absolute flex h-[75%] w-[50%] flex-col  justify-center bg-slate-50 bg-opacity-60">
+      <div className="absolute w-full h-full bg-black opacity-70" />
+      <div className="absolute flex h-[75%] w-[50%] flex-col justify-center bg-slate-50 ">
         <div className="flex h-full w-full items-center justify-center shadow-xl">
           <BackButton />
-          <div className="flex h-full w-full max-w-screen-sm  flex-col justify-start gap-x-5">
+          <div className="flex h-full w-full max-w-screen-sm flex-col justify-start gap-x-5">
             {/* Image */}
             <div className="relative flex h-[75%] w-full">
-              {product.photo ? (
+              {gallery.photo ? (
                 <Image
-                  src={`${product.photo}/sharpen=1,fit=scale-down,w=640`}
-                  alt="product"
+                  src={`${gallery.photo}/sharpen=1,fit=scale-down,w=640`}
+                  alt="gallery"
                   fill
                   objectFit="cover"
                 />
@@ -79,20 +82,31 @@ const Modal = async ({ params }: Props) => {
             </div>
 
             {/* Text */}
-            <div className="flex flex-col p-3 text-sm">
-              <ul>
-                <li className="font-semibold flex items-center gap-x-1">
-                  YM Light
-                  <div className="flex h-3 w-3 items-center justify-center rounded-full bg-black">
-                    <BoltIcon className="rounded-ful h-3 w-3 text-amber-300" />
+            <div className="flex h-[25%] w-full flex-col text-sm p-2">
+              <div className="flex h-full flex-col">
+                {/* button - Like, Comment*/}
+                <div className="flex gap-x-2 py-2 ">
+                  <HandThumbUpIcon className="h-5 w-5" />
+                  <ChatBubbleLeftEllipsisIcon className="h-5 w-5" />
+                </div>
+
+                <div className="flex h-full flex-col justify-between ">
+                  {/* Writer */}
+                  <div className="flex items-center gap-x-1">
+                    <div className="font-semibold">YM Light</div>
+                    <div className="flex h-3 w-3 items-center justify-center rounded-full bg-black">
+                      <BoltIcon className="rounded-ful h-3 w-3 text-amber-300" />
+                    </div>
+
+                    <div>{gallery.content}</div>
                   </div>
-                </li>
-                <li>{product.title}</li>
-                <li>가격 : {product.price}</li>
-                <li>
-                  <DateTime date={product.created_at} />{" "}
-                </li>
-              </ul>
+
+                  {/* Date */}
+                  <div className="text-gray-500">
+                    <DateTime date={gallery.created_at} />
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
