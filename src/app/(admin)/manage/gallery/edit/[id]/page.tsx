8533af2bee78473cd6126f753c11cb2dd426b053
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-05-29 17:40:52 +0900

    갤러리 수정 페이지

---
+++ src/app/(admin)/manage/gallery/edit/[id]/page.tsx
@@ -0,0 +1,37 @@
+import db from "@/libs/db";
+import Upload from "../../upload/page";
+
+
+const getGallery = async (id: string) => {
+  const gallery = await db.gallery.findUnique({
+    where: {
+      id: Number(id),
+    },
+    include:{
+        tags:{
+            select:{
+                id:true,
+                name:true
+            }
+        }
+    }
+    
+  });
+  return gallery;
+};
+
+
+const EditProduct = async ({ params }: { params: { id: string } }) => {
+  const gallery = await getGallery(params.id);
+  console.log(gallery);
+
+  return (
+    <div className="">
+      <div className="[&>div]:pt-0">
+        <Upload gallery={gallery} isEdit={true}  />;
+      </div>
+    </div>
+  );
+};
+
+export default EditProduct;
