a03b772152e22becaf4e60dbc05320b7d0d649dc
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-05-30 18:44:14 +0900

    갤러리 수동 갱신 기능

--- src/app/(admin)/manage/gallery/page.tsx
+++ src/app/(admin)/manage/gallery/page.tsx
@@ -1,18 +1,29 @@
+import { revalidateTag } from "next/cache";
 import Link from "next/link";
 import React from "react";
+
+const revalidateGallery = async () => {
+  "use server";
+  revalidateTag("gallery");
+};
 
 const Gallery = () => {
   return (
     <div className="pt-20">
       <div>갤러리 관리</div>
       <div className="flex gap-x-2 p-10">
-      <button className="boerder-sm border border-blue-500 p-2">
-        <Link href={`gallery/upload`}>업로드</Link>
-      </button>
-      <button className="boerder-sm border border-blue-500 p-2">
-        <Link href={`gallery/edit`}>수정</Link>
-      </button>
-    </div>
+        <button className="boerder-sm border border-blue-500 p-2">
+          <Link href={`gallery/upload`}>업로드</Link>
+        </button>
+        <button className="boerder-sm border border-blue-500 p-2">
+          <Link href={`gallery/edit`}>수정</Link>
+        </button>
+        <form action={revalidateGallery}>
+          <button className="boerder-sm border border-blue-500 p-2">
+            갤러리 정보 갱신
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
