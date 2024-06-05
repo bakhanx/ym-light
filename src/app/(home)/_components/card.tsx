d975637b583c4cd8c6d155bbf72f9eeb4939c1e4
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-06-05 21:35:53 +0900

    카드 크기 조정

--- src/app/(home)/_components/card.tsx
+++ src/app/(home)/_components/card.tsx
@@ -53,7 +53,8 @@ const Card = ({ name, discount, photoURL }: CardProps) => {
       onMouseOut={handleMouseOut}
       style={cardStyle}
     >
-      <div className="rounded-xl bg-gray-800 max-[480px]:w-36 xl:h-[474px] xl:w-[300px] sm:w-[200px] sm:h-[316px] border-4">
+      {/* 카드 황금비율 1:1.58 */}
+      <div className="rounded-xl bg-gray-800 xl:h-[474px] xl:w-[300px] min-[480px]:w-[200px] min-[480px]:h-[316px]  border-4 w-[150px] h-[237px]">
         <div className="relative h-full rounded-xl bg-slate-200 border-4">
           <Image
             src={`${photoURL}/fit=scale-down,w=480,sharpen=1`}
