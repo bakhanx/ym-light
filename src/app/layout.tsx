09e533cd72455b0199bdd7cddd871097aff9908e
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-06-17 18:13:28 +0900

    기본 padding-top

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${noto_sans_kr.className}`}>
         <TopNav />
-        <div className="">{children}</div>
+        <div className="sm:pt-20 pt-32">{children}</div>
       </body>
     </html>
   );
