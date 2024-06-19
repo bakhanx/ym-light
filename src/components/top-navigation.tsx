9d9da0550270ba37b593cf96b53ff9b1937cbfb2
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-06-19 18:04:22 +0900

    반응형 UI 수정

--- src/components/top-navigation.tsx
+++ src/components/top-navigation.tsx
@@ -19,27 +19,24 @@ const getUser = async () => {
     });
     if (user) return user;
   }
-  NotFound()
+  NotFound();
 };
-
 
 const TopNav = async () => {
   const user = await getUser();
-  const logOut = async ()=>{
+  const logOut = async () => {
     "use server";
     const session = await getSession();
     await session.destroy();
-    redirect("/")
-  }
+    redirect("/");
+  };
   return (
-    <div className="fixed z-50 sm:flex sm:h-14 w-full items-center justify-between border-b-2 border-yellow-500 bg-black lg:px-20 md:px-10 sm:py-10 sm:text-white h-32">
-
-      <div className="bg-gradient-to-tr from-yellow-500 to-yellow-200 bg-clip-text lg:text-2xl font-bold text-transparent text-xl shrink-0 h-16 px-4 items-center flex">
+    <div className="fixed z-50 h-32 w-full items-center justify-between border-b-2 border-yellow-500 bg-black sm:flex sm:h-14 sm:px-10 sm:py-10 sm:text-white md:px-20 lg:px-20">
+      <div className="flex h-16 shrink-0 items-center bg-gradient-to-tr from-yellow-500 to-yellow-200 bg-clip-text px-4 text-xl font-bold text-transparent sm:px-0 lg:text-2xl">
         <Link href="/">YM Light</Link>
       </div>
 
-      <ul className="md:flex md:divide-x-2 text-sm md:text-base [&>li]:px-2 [&>li]:py-1 sm:columns-5 bg-white [&>li]:border sm:[&>li]:border-none text-center grid grid-cols-4 sm:bg-black">
-
+      <ul className="grid grid-cols-4 bg-white text-center text-sm sm:flex sm:bg-black md:divide-x-2 md:text-base [&>li]:border [&>li]:px-2 [&>li]:py-1 sm:[&>li]:border-none h-16 items-center">
         <li>
           <Link href="/products">조명</Link>
         </li>
@@ -59,7 +56,6 @@ const TopNav = async () => {
           <Link href="/manage">관리자</Link>
         </li>
 
-
         {user ? (
           <>
             <li>{user.username}님</li>
@@ -77,9 +73,11 @@ const TopNav = async () => {
 
         <li>
           <Link href="/cart">
-            <span className="flex h-6 w-6 items-center justify-center">
+            장바구니
+            {/* icon */}
+            {/* <span className="flex h-6 w-6 items-center justify-center">
               <ShoppingCartIcon className="h-6 w-6" />
-            </span>
+            </span> */}
           </Link>
         </li>
 
