80023fe3a0c1d010c90a0b386a8c38451a54a349
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-06-21 12:36:56 +0900

    모바일 UI

--- src/components/top-navigation.tsx
+++ src/components/top-navigation.tsx
@@ -31,64 +31,67 @@ const TopNav = async () => {
     redirect("/");
   };
   return (
-    <div className="fixed z-50 h-32 w-full items-center justify-between border-b-2 border-yellow-500 bg-black sm:flex sm:h-14 sm:px-10 sm:py-10 sm:text-white md:px-20 lg:px-20">
-      <div className="flex h-16 shrink-0 items-center bg-gradient-to-tr from-yellow-500 to-yellow-200 bg-clip-text px-4 text-xl font-bold text-transparent sm:px-0 lg:text-2xl">
-        <Link href="/">YM Light</Link>
-      </div>
+    <div className="fixed z-50 h-36 w-full items-center justify-between border-b-2 border-yellow-500 bg-black text-white sm:flex sm:h-14 sm:py-10">
+      <div className="mx-auto flex w-full max-w-screen-2xl flex-col justify-between px-4 sm:flex-row md:px-20">
+        <div className="flex h-16 shrink-0 items-center bg-gradient-to-tr from-yellow-500 to-yellow-200 bg-clip-text text-xl font-bold text-transparent  md:text-2xl">
+          <Link href="/">YM Light</Link>
+        </div>
 
-      <ul className="grid grid-cols-4 bg-white text-center text-sm sm:flex sm:bg-black md:divide-x-2 md:text-base [&>li]:border [&>li]:px-2 [&>li]:py-2 sm:[&>li]:border-none h-16 items-center">
-        <li>
-          <Link href="/products">조명</Link>
-        </li>
-        <li>
-          <Link href="/gallery">갤러리</Link>
-        </li>
-        <li>
-          <Link href="/about">소개</Link>
-        </li>
-        <li>
-          <Link href="/contact">문의</Link>
-        </li>
-        <li>
-          <Link href="/faq">FAQ</Link>
-        </li>
-        <li>
-          <Link href="/manage">관리자</Link>
-        </li>
-
-        {user ? (
-          <>
-            <li>{user.username}님</li>
-            <li>
-              <form action={logOut}>
-                <button>로그아웃</button>
-              </form>
-            </li>
-          </>
-        ) : (
+        <ul className="grid h-16 w-full sm:w-auto grid-cols-4  items-center text-sm  sm:flex md:divide-x-2 md:text-base [&>li]:flex [&>li]:h-8 [&>li]:items-center  [&>li]:justify-center [&>li]:px-2 sm:[&>li]:border-none [&>li]:border ">
           <li>
-            <Link href="/login">로그인</Link>
+            <Link href="/products">조명</Link>
           </li>
-        )}
+          <li>
+            <Link href="/gallery">갤러리</Link>
+          </li>
+          <li>
+            <Link href="/about">소개</Link>
+          </li>
+          <li>
+            <Link href="/contact">문의</Link>
+          </li>
+          <li>
+            <Link href="/faq">FAQ</Link>
+          </li>
+          <li>
+            <Link href="/manage">관리자</Link>
+          </li>
 
-        <li>
-          <Link href="/cart">
-            장바구니
-            {/* icon */}
-            {/* <span className="flex h-6 w-6 items-center justify-center">
+          {user ? (
+            <>
+              <li>{user.username}님</li>
+              <li>
+                <form action={logOut}>
+                  <button>로그아웃</button>
+                </form>
+              </li>
+            </>
+          ) : (
+            <li>
+              <Link href="/login">로그인</Link>
+            </li>
+          )}
+
+          <li>
+            <Link href="/cart">
+              장바구니
+              {/* icon */}
+              {/* <span className="flex h-6 w-6 items-center justify-center">
               <ShoppingCartIcon className="h-6 w-6" />
             </span> */}
-          </Link>
-        </li>
+            </Link>
+          </li>
 
-        {/* <li className="text-black">
+          {/* <li className="text-black">
           <select>
             <option>한국어</option>
             <option>English</option>
             <option>中國語</option>
           </select>
         </li> */}
-      </ul>
+        </ul>
+      </div>
+      {/* <div className="h-1 w-full bg-gradient-to-r from-amber-100 via-amber-500 to-yellow-200 sm:hidden" /> */}
     </div>
   );
 };
