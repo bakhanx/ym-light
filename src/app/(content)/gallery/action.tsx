4e8655ca1b3891448f5cbcdba05530b24a50c206
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-06-21 22:26:16 +0900

    갤러리 가져오기

---
+++ src/app/(content)/gallery/action.tsx
@@ -0,0 +1,23 @@
+"use server";
+
+import db from "@/libs/db";
+
+const NUMBER_OF_GALLERY = 3;
+
+const getMoreGalleryList = async (page: number) => {
+  const galleryList = await db.gallery.findMany({
+    select: {
+      id: true,
+      photo: true,
+    },
+    skip: NUMBER_OF_GALLERY * page,
+    take: NUMBER_OF_GALLERY,
+    orderBy: {
+      created_at: "desc",
+    },
+  });
+
+  return galleryList;
+};
+
+export default getMoreGalleryList;
