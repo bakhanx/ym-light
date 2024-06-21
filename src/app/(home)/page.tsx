0b42a933e1eccc63876bded9def6e10d69de0fac
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-06-21 12:36:37 +0900

    Footer 분리

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -72,7 +72,7 @@ export default async function Home() {
   return (
     <>
       <div className="h-[576px] sm:h-[768px]">
-        <div className="relative h-full w-full">
+        <div className="relative h-full w-full flex justify-center">
           <Image
             src={bannerImage}
             fill
@@ -84,7 +84,7 @@ export default async function Home() {
 
           <div className="absolute h-full w-full bg-black opacity-50" />
 
-          <div className="absolute z-10 flex h-full flex-col justify-center pl-4 text-white sm:pl-10 md:pl-20 ">
+          <div className="absolute z-10 flex h-full flex-col max-w-screen-2xl md:px-20 px-4 w-full justify-center text-white ">
             <div>
               <span className="gradient-text animate-gradient text-3xl font-bold sm:text-5xl">
                 YM Light
@@ -141,72 +141,7 @@ export default async function Home() {
         </div>
       </Suspense>
 
-      <footer className="w-full bg-black text-white">
-        <div className="mx-auto flex max-w-screen-2xl flex-col px-10 py-10">
-          <div className="flex flex-col justify-between gap-y-4 md:flex-row">
-            <div>
-              <p className="pb-1 font-bold">CUSTOMER CENTER</p>
-              <div className="text-sm text-gray-300">0000-0000</div>
-              <table className="mt-2 text-gray-300">
-                <tbody>
-                  <tr className="text-left text-sm">
-                    <th className="">평일</th>
-                    <th className="pl-4">휴일</th>
-                  </tr>
-                  <tr className="text-sm font-bold">
-                    <td>09:00 ~ 18:00</td>
-                    <td className="pl-4">10:00 ~ 15:00</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
 
-            <div>
-              <p className="pb-1 font-bold">COMPANY</p>
-              <ul className="flex flex-col gap-y-1 text-sm text-gray-300">
-                <li>법인명(상호) : 영맨조명</li>
-                <li>
-                  <div className="flex">
-                    <span>대표 : 박홍섭</span>
-                    <span className="px-2">|</span>
-                    <span>개인정보관리자 : 박한솔</span>
-                  </div>
-                </li>
-                <li>이메일 : ymlight@gmail.com</li>
-                <li>전화 : 0000-0000</li>
-                <li>팩스 : 02-0000-0000</li>
-                <li>사업자등록번호 : 000-00-00000</li>
-
-                <li>통신판매신고번호 제 2024-서울-0000호</li>
-              </ul>
-            </div>
-
-            {/* <div>
-            <p className="font-bold">ADDRESS</p>
-            <ul className="text-sm text-gray-300">
-              <li>위치:서울특별시 도봉구 길동로 11-11</li>
-            </ul>
-          </div> */}
-
-            <div>
-              <p className="pb-1 font-bold">BANK INFO</p>
-              <ul className="text-sm text-gray-300">
-                <li>예금주 : 영맨조명 박홍섭</li>
-                <li>영맨은행 122-333-00000</li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="pt-10">
-            <p className="flex flex-col text-sm text-gray-300">
-              <span>
-                Copyright © (c) <strong>ymlight.com</strong>
-              </span>
-              <span>All rights reserved. | 호스팅사업자 : 와이엠라이트</span>
-            </p>
-          </div>
-        </div>
-      </footer>
     </>
   );
 }
