684d049b1c0feb94466573ce15db831f90610d54
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-06-25 18:25:09 +0900

    UI 수정

--- src/app/(admin)/manage/gallery/page.tsx
+++ src/app/(admin)/manage/gallery/page.tsx
@@ -9,17 +9,17 @@ const revalidateGallery = async () => {
 
 const Gallery = () => {
   return (
-    <div className="pt-20">
+    <div className="pt-10 mx-auto h-[60vh] max-w-screen-2xl">
       <div>갤러리 관리</div>
-      <div className="flex gap-x-2 p-10">
-        <button className="boerder-sm border border-blue-500 p-2">
+      <div className="flex sm:gap-x-8 gap-y-8 p-10 flex-col  md:flex-row items-center [&>button]:w-40 ">
+        <button className="boerder-sm border border-blue-500 p-4">
           <Link href={`gallery/upload`}>업로드</Link>
         </button>
-        <button className="boerder-sm border border-blue-500 p-2">
+        <button className="boerder-sm border border-blue-500 p-4">
           <Link href={`gallery/edit`}>수정</Link>
         </button>
         <form action={revalidateGallery}>
-          <button className="boerder-sm border border-blue-500 p-2">
+          <button className="boerder-sm border border-blue-500 p-4 w-40">
             갤러리 정보 갱신
           </button>
         </form>
