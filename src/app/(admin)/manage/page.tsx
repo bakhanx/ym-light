031e84a121cf45b49eb32a82c741093a0dcdcb70
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-06-25 18:24:34 +0900

    접근 비허용

--- src/app/(admin)/manage/page.tsx
+++ src/app/(admin)/manage/page.tsx
@@ -5,21 +5,21 @@ import React from "react";
 
 const Manage = () => {
   return (
-    <div className="pt-20">
-      <div className="flex gap-x-2 p-10">
-        <button className="boerder-sm border border-blue-500 p-2">
+    <div className="sm:h-[60vh] max-w-screen-2xl mx-auto">
+      <div className="flex sm:gap-x-8 gap-y-8 p-10 flex-col  md:flex-row items-center [&>button]:w-40 ">
+        <button className="boerder-sm border border-blue-500 p-4">
           <Link href={`/manage/product`}>상품관리</Link>
         </button>
-        <button className="boerder-sm border border-blue-500 p-2">
+        <button className="boerder-sm border border-blue-500 p-4">
           <Link href={`/manage/gallery`}>갤러리관리</Link>
         </button>
-        <button className="boerder-sm border border-blue-500 p-2">
+        <button className="boerder-sm border border-blue-500 p-4 cursor-not-allowed text-gray-300">
           소개관리
         </button>
-        <button className="boerder-sm border border-blue-500 p-2">
+        <button className="boerder-sm border border-blue-500 p-4 cursor-not-allowed text-gray-300">
           연락관리
         </button>
-        <button className="boerder-sm border border-blue-500 p-2">
+        <button className="boerder-sm border border-blue-500 p-4 cursor-not-allowed text-gray-300">
           FAQ관리
         </button>
       </div>
