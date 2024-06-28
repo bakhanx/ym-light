813f1cec58b6ed847a7800fdacab540c61549c95
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-06-28 18:57:24 +0900

    모바일 UI 수정

--- src/app/(content)/cart/page.tsx
+++ src/app/(content)/cart/page.tsx
@@ -6,7 +6,6 @@ import Products from "./_components.tsx/products";
 import Link from "next/link";
 import { CheckIcon, TrashIcon } from "@heroicons/react/24/outline";
 
-
 const Cart = () => {
   const [totalQuantity, setTotalQuantity] = useState(3);
   const [isSelectAllClick, setisSelectAllClick] = useState(true);
@@ -21,26 +20,31 @@ const Cart = () => {
         {/* Header */}
         <div className="divide-y-[1px] shadow-xl">
           {/* title  */}
-          <div className="py-10">
-            <div className="inner-content xl:max-w-screen-xl lg:max-w-screen-lg px-4 lg:px-0">
-              <span className="text-3xl font-bold">장바구니</span>
+          <div className="flex items-center divide-x-[1px] py-5 sm:flex-col sm:items-stretch sm:divide-y-[1px] sm:py-0">
+            <div className="sm:py-5">
+              <div className="inner-content px-4 lg:max-w-screen-lg lg:px-0 xl:max-w-screen-xl">
+                <span className="text-xl font-bold sm:text-2xl md:text-3xl">
+                  장바구니
+                </span>
+              </div>
             </div>
-          </div>
 
-          {/* total Qunatity */}
-          <div className=" py-5">
-            <div className="inner-content xl:max-w-screen-xl lg:max-w-screen-lg px-4 lg:px-0">
-              <div className="flex gap-x-1 font-bold">
-                <span className="text-xl ">전체</span>
-                <div className="flex h-5 w-5 items-center justify-center rounded-md bg-amber-200 p-3 text-lg">
-                  <span> {totalQuantity}</span>
+            {/* total Qunatity */}
+            <div className="sm:py-5">
+              <div className="inner-content px-4 lg:max-w-screen-lg lg:px-0 xl:max-w-screen-xl">
+                <div className="flex gap-x-1 font-bold">
+                  <span className="text-base sm:text-lg md:text-xl">전체</span>
+                  <div className="flex h-5 w-5 items-center justify-center rounded-md bg-amber-200 p-3 text-lg">
+                    <span> {totalQuantity}</span>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+
           {/* check all */}
           <div className=" py-5 text-sm">
-            <div className="inner-content xl:max-w-screen-xl lg:max-w-screen-lg px-4 lg:px-0">
+            <div className="inner-content px-4 lg:max-w-screen-lg lg:px-0 xl:max-w-screen-xl">
               <div className="flex gap-x-2 divide-x-2">
                 <div className="">
                   <button
@@ -52,7 +56,7 @@ const Cart = () => {
                     )}
                     onClick={handleSelectAllClick}
                   >
-                     <CheckIcon className="h-3 w-3 stroke-[3px] " />
+                    <CheckIcon className="h-3 w-3 stroke-[3px] " />
 
                     <span>전체선택</span>
                   </button>
@@ -71,11 +75,11 @@ const Cart = () => {
         </div>
 
         {/* contents */}
-        <div className="bg-gray-100 py-10 ">
-          <div className="m-auto xl:max-w-screen-xl rounded-md bg-white shadow-xl max-w-screen-lg px-4 lg:px-0">
+        <div className="bg-gray-200 py-4 sm:py-10 ">
+          <div className="m-auto max-w-screen-lg rounded-md px-4 shadow-xl lg:px-0 xl:max-w-screen-xl">
             {/* header */}
             <div>
-              <div className="flex border-b border-t-2 border-t-black p-4 text-center font-bold">
+              <div className="hidden border-b border-t-2 border-t-black bg-white p-2 text-center font-bold sm:flex sm:p-4">
                 <button
                   className={cls(
                     isSelectAllClick
@@ -87,66 +91,70 @@ const Cart = () => {
                 >
                   <CheckIcon className="h-3 w-3 stroke-[3px] " />
                 </button>
-                <span className="lg:w-[70%] w-full">상품정보</span>
-                <span className="w-[15%] hidden lg:block">상품금액</span>
-                <span className="w-[15%] hidden lg:block">배송비</span>
+
+                <div className="hidden w-full sm:flex">
+                  <span className="w-[70%]">상품정보</span>
+                  <span className="w-[15%] ">상품금액</span>
+                  <span className="w-[15%] ">배송비</span>
+                </div>
               </div>
             </div>
             {/* products */}
             {Array(totalQuantity)
               .fill(0)
               .map((e, index) => (
-                <div key={index} className="border-b-[1px] border-gray-300">
+                <div
+                  key={index}
+                  className="my-4 rounded-md border-b-[1px] border-gray-300 bg-white"
+                >
                   <Products />
                 </div>
               ))}
 
             {/* Total */}
-            <div className="flex border-b px-4 py-6">
-              <div className="flex  w-[70%] items-center justify-center lg:gap-x-16 border-r md:gap-x-8 gap-x-4">
-                <div className="flex flex-col md:text-base text-sm">
+            <div className="flex rounded-md border-4 border-amber-300 bg-white sm:px-4 px-2 py-6 shadow-md sm:flex-row flex-col justify-center items-center">
+              <div className="flex sm:w-[70%] items-center justify-between lg:justify-center sm:gap-x-4 gap-x-1 sm:border-r md:gap-x-8 lg:gap-x-16 w-full px-4">
+                <div className="flex flex-col text-sm md:text-base">
                   <span className="text-center text-gray-400">상품금액</span>
                   <span className="font-bold">33,000,000원</span>
                 </div>
                 <div>
-                  <span className="lg:text-4xl text-2xl ">-</span>
+                  <span className="text-2xl lg:text-4xl ">-</span>
                 </div>
-                <div className="flex flex-col md:text-base text-sm">
+                <div className="flex flex-col text-sm md:text-base">
                   <span className="text-center text-gray-400">할인금액</span>
                   <span className="font-bold text-red-500">3,200,000원</span>
                 </div>
 
                 <div>
-                  <span className="lg:text-4xl text-2xl ">+</span>
+                  <span className="text-2xl lg:text-4xl ">+</span>
                 </div>
 
-                <div className="flex flex-col md:text-base text-sm">
+                <div className="flex flex-col text-sm md:text-base">
                   <span className="text-center text-gray-400">배송비</span>
                   <span className="font-bold text-green-500">7,000원</span>
                 </div>
               </div>
 
-              <div className="flex  w-[30%] flex-col items-center justify-center rounded-xl ">
-                <span className="font-bold text-gray-600">주문금액</span>
-                <span className="md:text-2xl text-base  font-bold text-blue-500">
+
+              <div className="flex sm:pt-0 mt-4 sm:w-[30%] w-full flex-col lg:items-center items-end justify-end pr-4 lg:pr-0 sm:mt-0">
+                <span className="font-bold text-gray-600">총 금액</span>
+                <span className="text-xl font-bold  text-blue-500 md:text-2xl">
                   28,507,000원
                 </span>
               </div>
             </div>
 
             <div className="flex justify-center gap-x-10 py-5">
-              <button className="rounded-md border border-amber-500 p-5 px-16 text-xl  font-bold text-amber-500">
+              <button className="rounded-md border border-amber-500 p-5 font-bold text-amber-500 shadow-md  sm:p-5 sm:px-16 px-8 sm:text-xl w-1/2 sm:w-auto">
                 돌아가기
               </button>
-              <button className="rounded-md bg-amber-500 p-5 px-16 text-xl font-bold text-white">
+              <button className="rounded-md bg-amber-500 p-5 font-bold text-white shadow-md sm:p-5 sm:px-16 px-8 sm:text-xl w-1/2 sm:w-auto">
                 주문하기
               </button>
             </div>
           </div>
         </div>
-
-        {/* footer */}
-        <div className="pt-20">footer</div>
       </div>
     </>
   );
