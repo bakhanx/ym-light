a3defd22983e6f278ef7ad34304e20f2ec6f8053
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-07-01 17:50:08 +0900

    반응형 UI

--- src/app/(admin)/manage/product/edit/page.tsx
+++ src/app/(admin)/manage/product/edit/page.tsx
@@ -23,42 +23,63 @@ const Edit = async () => {
   const products = await getProducts();
 
   return (
-    <div className="flex flex-col gap-y-5 p-2 pt-20 sm:text-base text-sm max-w-screen-2xl sm:px-20 px-4 m-auto">
-      <ul className="flex w-full gap-x-6 border-b-2 border-black py-4">
-        <li className="sm:w-[10%] w-[8%]"></li>
-        <li className="w-[2%] text-center">ID</li>
-        <li className="w-[5%] text-center">사진</li>
-        <li className="w-[30%]">상품명</li>
-        <li className="w-[20%]">최근 업데이트</li>
-        <li className="w-[20%]">게시일</li> 
-      </ul>
+    <div className="m-auto flex max-w-screen-2xl flex-col gap-y-5 p-4 text-sm md:text-base lg:px-20">
+      {/* Header */}
+      <div className="hidden w-full gap-x-6 border-b-2 border-black py-4 sm:flex">
+        <ul className="flex w-[10%] gap-x-2">
+          <li className="w-[80%] "></li>
+          <li className="w-[20%] text-center">ID</li>
+        </ul>
+
+        <ul className="flex w-[90%] md:gap-x-8 gap-x-2">
+          <li className="w-[10%] text-center">사진</li>
+          <ul className="flex w-full">
+            <li className="w-[50%] ">상품명</li>
+            <li className="w-[25%]">최근 수정일</li>
+            <li className="w-[25%]">등록일</li>
+          </ul>
+        </ul>
+      </div>
+
+      {/* Product List */}
       {products.map((product) => (
         <div
           key={product.id}
-          className="flex items-center gap-x-6 border-b-2 border-black pb-4 "
+          className="flex flex-col gap-x-6 border-b-2 border-black pb-4 sm:flex-row"
         >
-          <div className="flex flex-col xl:flex-row gap-y-1 sm:w-[10%] w-[8%] gap-x-2 ">
-            <Link href={`edit/${product.id}`}>
-              <button className="bg-slate-200 p-2">편집</button>
-            </Link>
-            <DeleteForm id={product.id} action={deleteProduct} />
+          <div className="flex flex-row-reverse  gap-x-2 md:w-[10%] sm:flex-row sm:justify-normal sm:items-center">
+            <div className="flex w-[80%] flex-row justify-end gap-x-2 gap-y-1 sm:flex-col sm:items-center sm:justify-normal">
+              <Link href={`edit/${product.id}`}>
+                <button className="bg-slate-200 p-2">편집</button>
+              </Link>
+              <DeleteForm id={product.id} action={deleteProduct} />
+            </div>
+
+            <div className="w-[20%] sm:text-center sm:flex ">{product.id}</div>
           </div>
 
-          <div className="w-[2%] text-center">{product.id}</div>
+          <div className="flex w-full gap-x-2 sm:w-[90%]  md:gap-x-8 sm:items-center pt-4 sm:pt-0">
+            <div className="relative aspect-square w-[30%] sm:w-[10%]">
+              <Image
+                fill
+                alt={String(product.id)}
+                src={`${product.photo}/thumbnail`}
+              />
+            </div>
 
-          <div className="relative aspect-square w-[5%]">
-            <Image
-              fill
-              alt={String(product.id)}
-              src={`${product.photo}/thumbnail`}
-            />
-          </div>
-          <div className="w-[30%]">{product.title}</div>
-          <div className="w-[20%]">
-            <DateTime date={product.updated_at} />
-          </div>
-          <div className="w-[20%]">
-            <DateTime date={product.created_at} />
+            <ul className="flex w-full flex-col sm:flex-row items-center justify-center">
+              <li className=" pb-4 font-bold sm:w-[50%] sm:pb-0 sm:font-normal">
+                {product.title}
+              </li>
+              <li className="text-xs sm:w-[25%] md:text-base">
+                <span className="sm:hidden text-gray-600">(최근 수정일) </span>
+                <DateTime date={product.updated_at} />
+              </li>
+              <li className="text-xs sm:w-[25%] md:text-base">
+                <span className="sm:hidden text-gray-600">(등록일) </span>
+                <DateTime date={product.created_at} />
+              </li>
+            </ul>
           </div>
         </div>
       ))}
