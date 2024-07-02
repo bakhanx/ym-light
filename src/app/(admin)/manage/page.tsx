2386ff76273a4e72d977d73cf399130d0b30eec1
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-07-02 16:46:37 +0900

    링크이동 리팩토링

--- src/app/(admin)/manage/page.tsx
+++ src/app/(admin)/manage/page.tsx
@@ -3,25 +3,27 @@
 import Link from "next/link";
 import React from "react";
 
+const navLinks = [
+  { key: "product", name: "상품관리", href: "/manage/product" },
+  { key: "gallery", name: "갤러리관리", href: "/manage/gallery" },
+  { key: "about", name: "소개관리", href: "/manage/about" },
+  { key: "contact", name: "연락관리", href: "/manage/contact" },
+  { key: "faq", name: "FAQ관리", href: "/manage/faq" },
+];
+
 const Manage = () => {
   return (
     <div className="mx-auto max-w-screen-2xl sm:h-[60vh]">
       <div className="flex flex-col items-center gap-y-8 p-10  sm:gap-x-8 md:flex-row [&>button]:w-40 ">
-        <button className="boerder-sm border border-blue-500 p-4">
-          <Link href={`/manage/product`}>상품관리</Link>
-        </button>
-        <button className="boerder-sm border border-blue-500 p-4">
-          <Link href={`/manage/gallery`}>갤러리관리</Link>
-        </button>
-        <button className="boerder-sm cursor-not-allowed border border-blue-500 p-4 text-gray-300 ">
-          소개관리
-        </button>
-        <button className="boerder-sm cursor-not-allowed border border-blue-500 p-4 text-gray-300 ">
-          연락관리
-        </button>
-        <button className="boerder-sm cursor-not-allowed border border-blue-500 p-4 text-gray-300 ">
-          FAQ관리
-        </button>
+        {navLinks.map((navLink) => (
+          <Link
+            key={navLink.key}
+            href={navLink.href}
+            className="boerder-sm w-40 border border-blue-500 p-4 text-center hover:bg-blue-200"
+          >
+            {navLink.name}
+          </Link>
+        ))}
       </div>
     </div>
   );
