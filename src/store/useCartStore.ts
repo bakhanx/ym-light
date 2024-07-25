ac42c4111f10059deaa6bc9b7a796d8d0a10254c
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-07-25 18:18:34 +0900

    장바구니store props 수정

--- src/store/useCartStore.ts
+++ src/store/useCartStore.ts
@@ -1,30 +1,27 @@
+import { Option, Product } from "@prisma/client";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
-type Product = {
-  id: number;
-  title: string;
-  color: string;
-  price: number;
-  options: {
-    id: number;
-    index: number;
-    name: string;
-    price: number | null;
-    stock: number;
-    productId: number;
-  }[];
-  discount: number | null;
+export type ProductWithOptions = {
+  product: Product & OptionType;
+};
+
+type OptionType = {
+  options: Option[];
 };
 
 type State = {
-  productInfoList: { product: Product; quantity: number }[];
+  productInfoList: {
+    product: Product & OptionType;
+    quantity: number;
+  }[];
   quantity: number;
   totalPrice: number;
 };
 
 type Actions = {
-  addToCart: (item: Product) => void;
-  removeFromCart: (item: Product) => void;
+  addToCart: ({product}: ProductWithOptions) => void;
+  removeFromCart: ({product}: ProductWithOptions) => void;
 };
 
 const INITIAL_STATE: State = {
@@ -33,48 +30,62 @@ const INITIAL_STATE: State = {
   totalPrice: 0,
 };
 
-export const useCartStore = create<State & Actions>((set, get) => ({
-  productInfoList: INITIAL_STATE.productInfoList,
-  quantity: INITIAL_STATE.quantity,
-  totalPrice: INITIAL_STATE.totalPrice,
+export const useCartStore = create<State & Actions>()(
+  persist(
+    (set, get) => ({
+      productInfoList: INITIAL_STATE.productInfoList,
+      quantity: INITIAL_STATE.quantity,
+      totalPrice: INITIAL_STATE.totalPrice,
 
-  addToCart: (product: Product) => {
-    const productInfoList = get().productInfoList;
-    const isExistProduct = Boolean(
-      productInfoList.find(
-        (productInfo) => productInfo.product.id === product.id,
-      ),
-    );
-    if (isExistProduct) {
-      const updatedProductInfoList = productInfoList.map((productInfo) =>
-        productInfo.product.id === product.id
-          ? { ...productInfo, quantity: productInfo.quantity + 1 }
-          : productInfo,
-      );
-      set((state) => ({
-        productInfoList: updatedProductInfoList,
-        quantity: state.quantity + 1,
-        totalPrice: state.totalPrice + product.price,
-      }));
-    } else {
-      const updatedProductInfoList = [
-        ...productInfoList,
-        { product, quantity: 1 },
-      ];
-      set((state) => ({
-        productInfoList: updatedProductInfoList,
-        quantity: state.quantity + 1,
-        totalPrice: state.totalPrice + product.price,
-      }));
-    }
-  },
-  removeFromCart: (product: Product) => {
-    set((state) => ({
-      productInfoList: state.productInfoList.filter(
-        (productInfo) => productInfo.product.id !== product.id,
-      ),
-      quantity: state.quantity - 1,
-      totalPrice: state.totalPrice - product.price,
-    }));
-  },
-}));
+      addToCart: ({product}) => {
+        const productInfoList = get().productInfoList;
+        const isExistProduct = Boolean(
+          productInfoList.find(
+            (productInfo) => productInfo.product.id === product.id,
+          ),
+        );
+        if (isExistProduct) {
+          const updatedProductInfoList = productInfoList.map((productInfo) =>
+            productInfo.product.id === product.id
+              ? { ...productInfo, quantity: productInfo.quantity + 1 }
+              : productInfo,
+          );
+          set((state) => ({
+            productInfoList: updatedProductInfoList,
+            quantity: state.quantity + 1,
+            totalPrice: state.totalPrice + product.price,
+          }));
+        } else {
+          const updatedProductInfoList = [
+            ...productInfoList,
+            { product, quantity: 1 },
+          ];
+          set((state) => ({
+            productInfoList: updatedProductInfoList,
+            quantity: state.quantity + 1,
+            totalPrice: state.totalPrice + product.price,
+          }));
+        }
+      },
+      removeFromCart: ({product}) => {
+        set((state) => ({
+          productInfoList: state.productInfoList.filter(
+            (productInfo) => productInfo.product.id !== product.id,
+          ),
+          quantity: state.quantity - 1,
+          totalPrice: state.totalPrice - product.price,
+        }));
+      },
+    }),
+    {
+      name: "cart",
+      storage: createJSONStorage(() => sessionStorage),
+      partialize: (state) => ({
+        productInfoList: state.productInfoList,
+        quantity: state.quantity,
+        totalPrice: state.totalPrice,
+      }),
+      skipHydration: true,
+    },
+  ),
+);
