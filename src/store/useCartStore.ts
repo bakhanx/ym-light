818c4293a4ea5be1bdd4a597d53e4339b4ab692e
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-07-30 23:57:08 +0900

    product, option 분리

--- src/store/useCartStore.ts
+++ src/store/useCartStore.ts
@@ -2,94 +2,108 @@ import { Option, Product } from "@prisma/client";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
-export type ProductWithOptions = {
-  product: Product & OptionType;
+type ProductInfo = {
+  productInfo: {
+    product: Product;
+    quantity: number;
+  };
 };
-
-type OptionType = {
-  options: Option[];
-};
-
-type OptionList = {
-  selectedOptionList: Option[];
+type OptionInfoList = {
+  optionInfoList: {
+    option: Option;
+    quantity: number;
+  }[];
 };
 
 type State = {
-  productInfoList: {
-    product: Product & OptionType;
-    quantity: number;
-  }[];
-  quantity: number;
-  totalPrice: number;
+  cart: (ProductInfo & OptionInfoList)[];
+};
+
+type RemoveFromCart = {
+  productId : number;
+  optionId? : number;
 };
 
 type Actions = {
   addToCart: ({
-    product,
-    selectedOptionList,
-  }: ProductWithOptions & OptionList) => void;
-  removeFromCart: ({ product }: ProductWithOptions) => void;
+    productInfo,
+    optionInfoList,
+  }: ProductInfo & OptionInfoList) => void;
+  removeFromCart: ({ productId, optionId }: RemoveFromCart) => void;
 };
 
 const INITIAL_STATE: State = {
-  productInfoList: [],
-  quantity: 0,
-  totalPrice: 0,
+  cart: [],
 };
 
 export const useCartStore = create<State & Actions>()(
   persist(
     (set, get) => ({
-      productInfoList: INITIAL_STATE.productInfoList,
-      quantity: INITIAL_STATE.quantity,
-      totalPrice: INITIAL_STATE.totalPrice,
+      cart: INITIAL_STATE.cart,
 
-      addToCart: ({ product }) => {
-        const productInfoList = get().productInfoList;
-        const isExistProduct = productInfoList.some(
-          (productInfo) => productInfo.product.id === product.id,
+      addToCart: ({ productInfo, optionInfoList }) => {
+        const cart = get().cart;
+
+        const indexExistedProduct = cart.findIndex(
+          (item) => item.productInfo.product.id === productInfo.product.id,
         );
 
-        if (isExistProduct) {
-          const updatedProductInfoList = productInfoList.map((productInfo) =>
-            productInfo.product.id === product.id
-              ? { ...productInfo, quantity: productInfo.quantity + 1 }
-              : productInfo,
-          );
-          set((state) => ({
-            productInfoList: updatedProductInfoList,
-            quantity: state.quantity + 1,
-            totalPrice: state.totalPrice + product.price,
-          }));
+        if (indexExistedProduct) {
+          cart[indexExistedProduct].productInfo.quantity++;
         } else {
-          const updatedProductInfoList = [
-            ...productInfoList,
-            { product, quantity: 1 },
-          ];
-          set((state) => ({
-            productInfoList: updatedProductInfoList,
-            quantity: state.quantity + 1,
-            totalPrice: state.totalPrice + product.price,
-          }));
+          cart.push({ productInfo: productInfo, optionInfoList: [] });
         }
+
+        // Option
+        if (optionInfoList.length > 0) {
+          optionInfoList.map((optionInfo) => {
+            const indexExistedOption = cart[
+              indexExistedProduct
+            ].optionInfoList.findIndex(
+              (_optionInfo) => _optionInfo.option.id === optionInfo.option.id,
+            );
+            if (indexExistedOption) {
+              cart[indexExistedProduct].optionInfoList[indexExistedOption]
+                .quantity++;
+            } else {
+              cart[indexExistedProduct].optionInfoList.push(optionInfo);
+            }
+          });
+        }
+
+        set((state) => ({ cart: state.cart }));
       },
-      removeFromCart: ({ product }) => {
-        set((state) => ({
-          productInfoList: state.productInfoList.filter(
-            (productInfo) => productInfo.product.id !== product.id,
-          ),
-          quantity: state.quantity - 1,
-          totalPrice: state.totalPrice - product.price,
-        }));
+
+      removeFromCart: ({ productId, optionId}) => {
+        const cart = get().cart;
+
+        // 상품 제거
+        if (productId) {
+          const newCart = cart.filter(
+            ({ productInfo }) => productInfo.product.id !== productId,
+          );
+        }
+
+        // 옵션 제거
+        if (optionId) {
+          const productIndex = cart.findIndex(
+            (item) => item.productInfo.product.id === productId,
+          );
+          const newOptionList = cart[productIndex].optionInfoList.filter(
+            (optionInfo) => optionInfo.option.id !== optionId,
+          );
+          
+          cart[productIndex].optionInfoList = newOptionList
+
+        }
+        set((state) => ({ cart: state.cart }));
       },
     }),
     {
       name: "cart",
       storage: createJSONStorage(() => sessionStorage),
       partialize: (state) => ({
-        productInfoList: state.productInfoList,
-        quantity: state.quantity,
-        totalPrice: state.totalPrice,
+        cart: state.cart
       }),
       skipHydration: true,
     },
