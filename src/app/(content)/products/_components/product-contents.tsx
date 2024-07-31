c396f5c7a0b36dde352136d672c1247dc7b1d887
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-07-31 18:20:55 +0900

    옵션 유무에 따른 수량 선택 기능

--- src/app/(content)/products/_components/product-contents.tsx
+++ src/app/(content)/products/_components/product-contents.tsx
@@ -7,20 +7,60 @@ import {
   TruckIcon,
 } from "@heroicons/react/16/solid";
 import React, { useState } from "react";
-import Options from "./options";
+import Options, { selectedItemType } from "./options";
 import ProductInfo from "./productInfo";
 import Image from "next/image";
-import { ProductWithOptions, useCartStore } from "@/store/useCartStore";
-import { Option } from "@prisma/client";
+import { useCartStore } from "@/store/useCartStore";
+import { Option, Product } from "@prisma/client";
+
+type Options = {
+  options: Option[];
+};
+
+type ProductWithOptions = {
+  product: Product & Options;
+};
 
 const ProductContents = ({ product }: ProductWithOptions) => {
-  const { addToCart, quantity } = useCartStore((state) => state);
-  const [selectedOptionList, setSelectedOptionList] = useState<Option[]>([]);
-  const parentFunc = (item: Option[]) => {
+  const { addToCart, removeFromCart } = useCartStore((state) => state);
+  const [selectedOptionList, setSelectedOptionList] = useState<
+    selectedItemType[]
+  >([]);
+  const [quantity, setQuantity] = useState(
+    product.options.length > 0 ? 0 : 1,
+  );
+  console.log(selectedOptionList.length)
+  console.log(quantity)
+
+  // Option Props
+  const parentFunc = (item: selectedItemType[]) => {
     setSelectedOptionList(item);
   };
 
-  const handleAddToCart = () => {};
+  // Quantity Button
+  const handleButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>,
+    buttonType: "add" | "substract",
+  ) => {
+    event.preventDefault();
+
+    if (buttonType === "add") {
+      setQuantity((prev) => prev + 1);
+    } else if (buttonType === "substract") {
+      if (quantity === 1) return;
+      setQuantity((prev) => prev - 1);
+    }
+  };
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuantity(+event.target.value);
+  };
+  const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+    if (quantity < 1) {
+      setQuantity(1);
+    } else {
+      setQuantity(+event.target.value);
+    }
+  };
 
   return (
     <div className="my-column_bind flex flex-col divide-y-2 divide-slate-300 sm:flex-row sm:divide-x-2 sm:divide-y-0">
@@ -89,7 +129,6 @@ const ProductContents = ({ product }: ProductWithOptions) => {
           </div>
 
           {/* Option */}
-
           {product.options.length > 0 ? (
             <Options
               options={product.options}
@@ -98,19 +137,48 @@ const ProductContents = ({ product }: ProductWithOptions) => {
               parentFunc={parentFunc}
             />
           ) : (
-            <div className="flex items-end justify-end gap-x-5 py-5 pt-16">
-              <div className="text-gray-500">총 금액</div>
-              <span className="w-36 text-right text-xl font-bold">
-                <span className="text-lg sm:text-2xl">
-                  {product?.discount
-                    ? formatOfPrice(
-                        product?.price * ((100 - product.discount) / 100),
-                      )
-                    : formatOfPrice(product?.price)}
-                  원
-                </span>
-              </span>
-            </div>
+            <>
+              <div className="flex items-end justify-between pb-4 pt-16">
+                <div className="count-btn flex text-black">
+                  <button
+                    className="flex h-6 w-6 items-center justify-center border p-2 hover:bg-orange-50"
+                    onClick={(e) => handleButtonClick(e, "substract")}
+                  >
+                    -
+                  </button>
+
+                  <input
+                    className="flex h-6 w-10 items-center justify-center border text-center"
+                    type="number"
+                    value={quantity}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  <button
+                    className="flex h-6 w-6 items-center justify-center border p-2 hover:bg-orange-50"
+                    onClick={(e) => handleButtonClick(e, "add")}
+                  >
+                    +
+                  </button>
+                </div>
+
+                <div className="flex items-end gap-x-5">
+                  <div className="text-gray-500">총 금액</div>
+                  <span className="w-36 text-right text-xl font-bold">
+                    <span className="text-lg sm:text-2xl">
+                      {product?.discount
+                        ? formatOfPrice(
+                            product?.price *
+                              ((100 - product.discount) / 100) *
+                              quantity,
+                          )
+                        : formatOfPrice(product?.price * quantity)}
+                      원
+                    </span>
+                  </span>
+                </div>
+              </div>
+            </>
           )}
 
           {/* 구매 장바구니 찜 버튼 */}
@@ -121,7 +189,12 @@ const ProductContents = ({ product }: ProductWithOptions) => {
             </button>
             <div className="flex gap-x-4">
               <button
-                onClick={() => addToCart({ product, selectedOptionList })}
+                onClick={() =>
+                  addToCart({
+                    productInfo: { product, quantity },
+                    optionInfoList: selectedOptionList,
+                  })
+                }
                 className="flex w-full items-center justify-center gap-x-1 rounded-md bg-blue-500 p-4 hover:bg-blue-600 sm:p-5"
               >
                 <ShoppingBagIcon className="h-4 w-4 stroke-2 sm:h-5 sm:w-5" />
