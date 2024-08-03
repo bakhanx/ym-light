83b929d078fd4b8779349f61a037cc880a806f27
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-08-03 18:25:08 +0900

    pending시 문구수정 & 로딩상태 UI

--- src/components/form-button.tsx
+++ src/components/form-button.tsx
@@ -1,7 +1,7 @@
 "use client";
-
-import React from "react";
 import { useFormStatus } from "react-dom";
+import Loader from "./loader";
+import PreventScroll from "./preventScroll";
 
 type FormButtonType = {
   name?: string;
@@ -10,15 +10,22 @@ type FormButtonType = {
 };
 const FormButton = ({ name, color }: FormButtonType) => {
   const { pending } = useFormStatus();
-
   return (
-    <button
-      disabled={pending}
-      className="flex w-full items-center justify-center gap-x-1 rounded-md bg-amber-400 p-5 font-semibold hover:bg-amber-500"
-    >
-      {name}
-      {pending && "처리중..."}
-    </button>
+    <>
+      <button
+        disabled={pending}
+        className="flex w-full items-center justify-center gap-x-1 rounded-md bg-amber-400 p-5 font-semibold hover:bg-amber-500"
+      >
+        {pending ? "처리중..." : name}
+      </button>
+
+      {pending && (
+        <div className="fixed left-0 top-0 h-full w-full bg-black opacity-50">
+          <Loader />
+          <PreventScroll />
+        </div>
+      )}
+    </>
   );
 };
 
