8dfdb776c4334beb3fcfbf079abf337206b01cc4
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-08-03 18:23:11 +0900

    스크롤방지

--- src/app/(content)/gallery/@modal/(.)[id]/page.tsx
+++ src/app/(content)/gallery/@modal/(.)[id]/page.tsx
@@ -12,6 +12,7 @@ import {
   ChatBubbleLeftEllipsisIcon,
   HandThumbUpIcon,
 } from "@heroicons/react/24/outline";
+import PreventScroll from "@/components/preventScroll";
 
 type Props = {
   params: {
@@ -45,7 +46,6 @@ const getGallery = async (id: number) => {
       created_at: true,
       updated_at: true,
     },
-    
   });
   return gallery;
 };
@@ -63,60 +63,64 @@ const Modal = async ({ params }: Props) => {
   }
 
   return (
-    <div className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center">
-      <div className="absolute h-full w-full bg-black opacity-70" />
+    <>
+      <PreventScroll />
+      <div className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center">
+        <div className="absolute h-full w-full bg-black opacity-70" />
 
-      <div className="absolute flex h-full w-full max-w-screen-sm flex-col justify-center bg-slate-50 sm:h-[75%] sm:w-[50%] ">
-        <div className="flex h-full w-full items-center justify-center shadow-xl">
-          <BackButton />
-          <div className="flex h-full w-full flex-col justify-start gap-x-5 ">
-            {/* Image */}
-            <div className="relative flex h-[75%] w-full bg-black">
-              {gallery.photo ? (
-                <>
-                  <Image
-                    src={`${gallery.photo}/public`}
-                    alt="gallery"
-                    quality={90}
-                    fill
-                    className="z-40"
-                    objectFit="contain"
-                  />
-                </>
-              ) : (
-                <PhotoIcon className="h-28" />
-              )}
-            </div>
+        <div className="absolute flex h-full w-full max-w-screen-sm flex-col justify-center bg-slate-50 sm:h-[75%] sm:w-[50%] ">
+          <div className="flex h-full w-full items-center justify-center shadow-xl">
+            <BackButton />
+            <div className="flex h-full w-full flex-col justify-start gap-x-5 ">
+              {/* Image */}
+              <div className="relative flex h-[75%] w-full bg-black">
+                {gallery.photo ? (
+                  <>
+                    <Image
+                      src={`${gallery.photo}/public`}
+                      alt="gallery"
+                      quality={90}
+                      fill
+                      className="z-40"
+                      objectFit="contain"
+                    />
+                  </>
+                ) : (
+                  <PhotoIcon className="h-28" />
+                )}
+              </div>
 
-            {/* Text */}
-            <div className="flex h-[25%] w-full flex-col p-2 text-sm">
-              <div className="flex h-full flex-col">
-                {/* button - Like, Comment*/}
-                <div className="flex gap-x-2 py-2 ">
-                  <HandThumbUpIcon className="h-5 w-5" />
-                  <ChatBubbleLeftEllipsisIcon className="h-5 w-5" />
-                </div>
+              {/* Text */}
+              <div className="flex h-[25%] w-full flex-col p-2 text-sm">
+                <div className="flex h-full flex-col">
+                  {/* button - Like, Comment*/}
+                  <div className="flex gap-x-2 py-2 ">
+                    <HandThumbUpIcon className="h-5 w-5" />
+                    <ChatBubbleLeftEllipsisIcon className="h-5 w-5" />
+                  </div>
 
-                <div className="flex h-full flex-col justify-between ">
-                  {/* Contents */}
-                  <div className="flex items gap-x-1">
+                  <div className="flex h-full flex-col justify-between ">
+                    {/* Contents */}
+                    <div className="items flex gap-x-1">
+                      <div className="shrink-0 font-semibold">YM Light</div>
 
-                    <div className="font-semibold shrink-0">YM Light</div>
+                      <div className="mt-1 flex h-3 w-3 items-center rounded-full bg-black ">
+                        <BoltIcon className="rounded-ful h-3 w-3 text-amber-300" />
+                      </div>
 
-                    <div className="flex h-3 w-3 mt-1 items-center rounded-full bg-black ">
-                      <BoltIcon className="rounded-ful h-3 w-3 text-amber-300" />
+                      <div className="w-[80%] whitespace-pre-wrap">
+                        {gallery.content}
+                      </div>
+                    </div>
+                    {/* Tags */}
+                    <div className="text-sm font-semibold text-blue-500">
+                      {gallery.tags.map((tag) => tag.name)}
                     </div>
 
-                    <div className="w-[80%] whitespace-pre-wrap">{gallery.content}</div>
-                  </div>
-                  {/* Tags */}
-                  <div className="text-sm font-semibold text-blue-500">
-                    {gallery.tags.map((tag) => tag.name)}
-                  </div>
-
-                  {/* Date */}
-                  <div className="text-gray-500">
-                    <DateTime date={gallery.created_at} />
+                    {/* Date */}
+                    <div className="text-gray-500">
+                      <DateTime date={gallery.created_at} />
+                    </div>
                   </div>
                 </div>
               </div>
@@ -124,7 +128,7 @@ const Modal = async ({ params }: Props) => {
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 export default Modal;
