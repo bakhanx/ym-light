73c6da13b5fdf66396d6835200e8f6249a595d64
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-08-13 23:24:40 +0900

    관리자용 채팅 리스트 페이지

--- src/app/(admin)/manage/chats/page.tsx
+++ src/app/(admin)/manage/chats/page.tsx
@@ -1,7 +1,32 @@
+import DateTime from "@/components/datetime";
+import db from "@/libs/db";
+import Link from "next/link";
 import React from "react";
 
-const Chat = () => {
-  return <div>채팅페이지</div>;
+const Chat = async () => {
+  const chatRooms = await db.chatRoom.findMany({
+    include: {
+      _count: true,
+      users: true,
+    },
+  });
+
+  return (
+    <div>
+      <p>채팅페이지</p>
+      <div className="flex flex-col gap-y-10 pt-10">
+        {chatRooms.map((room) => (
+          <div key={room.id} className="flex w-full gap-x-5 border-2 p-2">
+            <Link href={`/chats/${room.id}`} className="bg-gray-300 p-2">
+              입장
+            </Link>
+            <div className="w-64">{room.users[0].username}</div>
+            <DateTime date={room.updated_at} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
