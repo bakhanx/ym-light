67cb638a3dc6fc664170d36e29a6f02e73c3b1f7
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-08-20 22:50:20 +0900

    전구 컴포넌트 수정

--- src/components/bulb.tsx
+++ src/components/bulb.tsx
@@ -1,29 +1,10 @@
-"use client";
-
-import React, { useState } from "react";
+import React from "react";
 import styles from "@/styles/Bulb.module.css";
-import { cls } from "@/libs/utils";
 
 const Bulb = () => {
-  const [isOn, setIsOn] = useState(true);
   return (
     <div>
-      <div className={styles.bulb_container}>
-        <div className={styles.wire}></div>
-        <div className={styles.connector}>
-          <div className={styles.grove}></div>
-          <div className={styles.grove}></div>
-          <div className={styles.grove}></div>
-        </div>
-        <button
-          onClick={() => setIsOn(!isOn)}
-          className={cls(isOn ? `${styles.bulb_on}` : "", `${styles.bulb}`)}
-        >
-          <div className={styles.metal_wire}></div>
-          <div className={styles.metal_wire}></div>
-          <div className={styles.metal_wire}></div>
-        </button>
-      </div>
+      <div className={styles.light_container} />    
     </div>
   );
 };
