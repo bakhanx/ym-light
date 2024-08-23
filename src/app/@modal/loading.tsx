ac27d116c693e3de5e4a027c2671068969ed38a8
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-08-23 21:48:50 +0900

    로딩 UI

--- src/app/@modal/loading.tsx
+++ src/app/@modal/loading.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import Loader from "@/components/loader";
+import React from "react";
 
 const Loading = () => {
   return (
-    <div>
-      Loading...
+    <div className="fixed bottom-10 right-10 ">
+      <Loader />
     </div>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;
