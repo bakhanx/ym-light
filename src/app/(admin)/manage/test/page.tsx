048d3d8aa154ef977f53ef8dfd80c51681ff8869
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-08-26 20:34:07 +0900

    TEST: 전구컴포넌트

--- src/app/(admin)/manage/test/page.tsx
+++ src/app/(admin)/manage/test/page.tsx
@@ -1,11 +1,11 @@
+import Bulb from "@/components/bulb";
 import React from "react";
 
 const TestPage = () => {
   const a = undefined;
   return (
-    <div className="p-5">
-      <div className="py-10">테스트 페이지</div>
-      {a ?? 2}
+    <div className="p-5 h-screen bg-slate-900">
+     <Bulb/>
     </div>
   );
 };
