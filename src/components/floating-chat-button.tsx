35fb053940a59aed184c25fb461c565608a48c4a
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-08-29 20:25:55 +0900

    로딩 컴포넌트 UI

--- src/components/floating-chat-button.tsx
+++ src/components/floating-chat-button.tsx
@@ -3,22 +3,38 @@
 import { createChatRoom } from "@/app/(content)/chats/action";
 import { ChatBubbleLeftEllipsisIcon } from "@heroicons/react/16/solid";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
+import Loader from "./loader";
 
 const FloatingButton = () => {
   const router = useRouter();
+  const [isShowModal, setIsShowModal] = useState(false);
+
   const EnterChatRoom = () => {
+    setIsShowModal(true);
     createChatRoom().then((res) => {
-      router.push(`/chats/${res.roomId}`);
+      router.push(`/chats/${res.roomId}`, { scroll: false });
+      //깜빡임 방지
+      setTimeout(() => {
+        setIsShowModal(false);
+      }, 300);
     });
   };
   return (
-    <button
-      onClick={EnterChatRoom}
-      className="fixed bottom-10 right-10 z-50 flex h-20 w-20 items-center justify-center rounded-full bg-amber-500"
-    >
-      <ChatBubbleLeftEllipsisIcon className="h-10 w-10 text-white" />
-    </button>
+    <div>
+      <button
+        onClick={EnterChatRoom}
+        className="fixed bottom-10 right-10 z-40 flex h-20 w-20 items-center justify-center rounded-full bg-yellow-500"
+      >
+        <ChatBubbleLeftEllipsisIcon className="h-10 w-10 text-white" />
+      </button>
+      {isShowModal && (
+        <div className="fixed bottom-10 right-10 z-50 flex h-96 w-96 flex-col items-center justify-center rounded-lg border-4 border-amber-300 bg-white p-5">
+          <Loader />
+          채팅방 요청중...
+        </div>
+      )}
+    </div>
   );
 };
 
