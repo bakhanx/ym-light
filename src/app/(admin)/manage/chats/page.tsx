6f79524ffcc8ad0970ffad84ab34345d9b0a6afd
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-08-29 20:24:44 +0900

    유저명 표기

--- src/app/(admin)/manage/chats/page.tsx
+++ src/app/(admin)/manage/chats/page.tsx
@@ -12,7 +12,7 @@ const Chat = async () => {
   });
 
   return (
-    <div>
+    <div className="pt-20">
       <p>채팅페이지</p>
       <div className="flex flex-col gap-y-10 pt-10">
         {chatRooms.map((room) => (
@@ -20,7 +20,13 @@ const Chat = async () => {
             <Link href={`/chats/${room.id}`} className="bg-gray-300 p-2">
               입장
             </Link>
-            <div className="w-64">{room.users[0].username}</div>
+            <div className="w-64">
+              {
+                room.users.filter((user) => user.username !== "admin")[0]
+                  .username
+              }
+            </div>
+            <div className="w-64">{room.id}</div>
             <DateTime date={room.updated_at} />
           </div>
         ))}
