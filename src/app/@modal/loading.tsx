5b633d7c599e702ff3029ee2df524c3905df539f
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-08-29 20:24:54 +0900

    로딩 컴포넌트

--- src/app/@modal/loading.tsx
+++ src/app/@modal/loading.tsx
@@ -3,8 +3,9 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="fixed bottom-10 right-10 ">
+    <div className="fixed bottom-10 right-10 z-50 flex h-96 w-96 flex-col items-center justify-center rounded-lg border-4 border-amber-300 bg-white p-5">
       <Loader />
+      채팅방 입장중...
     </div>
   );
 };
