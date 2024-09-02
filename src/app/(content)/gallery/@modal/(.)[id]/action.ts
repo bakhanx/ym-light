6f2d9126a8e0af36e6cec8077dfd4e2c6736e7ae
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-09-02 19:20:01 +0900

    gallery like&dislike

---
+++ src/app/(content)/gallery/@modal/(.)[id]/action.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import db from "@/libs/db";
+import getSession from "@/libs/session";
+import { revalidateTag } from "next/cache";
+
+export const likeGallery = async (galleryId: number) => {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const session = await getSession();
+  try {
+    await db.like.create({
+      data: {
+        userId: session.id,
+        galleryId,
+      },
+    });
+    revalidateTag("gallery");
+    revalidateTag(`gallery-${galleryId}`);
+  } catch (e) {}
+};
+
+export const dislikeGallery = async (galleryId: number) => {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const session = await getSession();
+  try {
+    await db.like.delete({
+      where: {
+        id: {
+          userId: session.id,
+          galleryId,
+        },
+      },
+    });
+    revalidateTag("gallery");
+    revalidateTag(`gallery-${galleryId}`);
+  } catch (e) {}
+};
