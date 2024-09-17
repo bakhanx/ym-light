fabaf5fa9b53f9c39795d8cebdf65ce6ff5377e6
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-09-17 17:27:35 +0900

    상단 간격 조정

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -24,8 +24,8 @@ const Register = () => {
   const [state, dispatch] = useFormState(registerAction, initFormState);
 
   return (
-    <div className="h-screen bg-gray-800  text-white">
-      <div className="flex h-full mx-auto max-w-screen-lg items-center justify-center">
+    <div className=" bg-gray-800  text-white">
+      <div className="flex h-full mx-auto max-w-screen-lg items-center justify-center pt-32 pb-12">
         <form action={dispatch} className="w-80">
           <div className="flex flex-col gap-y-5">
             <FormInput
