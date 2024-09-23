dc0c3c724e012ca164eedae5f5212df66c3aa963
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-09-23 20:02:59 +0900

    관리자 세션 제한

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,18 +3,23 @@ import React from "react";
 import getSession from "./libs/session";
 
 const publicPaths = new Set(["/login", "/register"]);
-const privatePaths = new Set(["/profile"]);
-
+const privatePaths = new Set(["/profile", "/manage"]);
+const adminPaths = new Set(["/manage"]);
 const middleware = async (req: NextRequest) => {
   const session = await getSession();
   const isPublicPath = publicPaths.has(req.nextUrl.pathname);
   const isPrivatePath = privatePaths.has(req.nextUrl.pathname);
+  const isAdminPath = adminPaths.has(req.nextUrl.pathname);
   // 비로그인 유저 접근 제한
   if (!session.id && isPrivatePath) {
     return NextResponse.redirect(new URL("/", req.url));
   }
   // 로그인 유저 접근 제한
   else if (session.id && isPublicPath) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+  // 관리자 페이지 접근 제한
+  if(session.id !== 1 && isAdminPath) {
     return NextResponse.redirect(new URL("/", req.url));
   }
 };
