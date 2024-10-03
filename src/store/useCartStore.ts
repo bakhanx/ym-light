c9396ae24e6c4a83e755de7e08dfe611e056da88
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-10-03 13:25:59 +0900

    상품 제거 filter

--- src/store/useCartStore.ts
+++ src/store/useCartStore.ts
@@ -2,21 +2,23 @@ import { Option, Product } from "@prisma/client";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
-type ProductInfo = {
+type OptionInfoList = {
+  optionInfoList: {
+    option: Option;
+    quantity: number;
+    price: number;
+    totalPrice: number;
+  }[];
+};
+
+type ProductProps = {
   productInfo: {
     product: Product;
     quantity: number;
   };
 };
-type OptionInfoList = {
-  optionInfoList: {
-    option: Option;
-    quantity: number;
-    totalPrice: number;
-  }[];
-};
 
-export type CartItem = ProductInfo &
+export type CartItem = ProductProps &
   OptionInfoList & {
     checked: boolean;
   };
@@ -32,7 +34,7 @@ type Actions = {
   addToCart: ({
     productInfo,
     optionInfoList,
-  }: ProductInfo & OptionInfoList) => void;
+  }: ProductProps & OptionInfoList) => void;
   removeFromCart: ({ productId, optionId }: RemoveFromCart) => void;
 };
 const INITIAL_STATE: State = {
@@ -45,64 +47,79 @@ export const useCartStore = create<State & Actions>()(
       cart: INITIAL_STATE.cart,
 
       addToCart: ({ productInfo, optionInfoList }) => {
+        console.log(productInfo);
+        console.log(optionInfoList);
         const cart = get().cart;
         const indexExistedProduct = cart.findIndex(
           (item) => item.productInfo.product.id === productInfo.product.id,
         );
 
-        if (indexExistedProduct !== -1) {
+        console.log(indexExistedProduct);
+
+        // 장바구니에 없으면
+        if (indexExistedProduct === -1) {
+          cart.push({
+            productInfo: productInfo,
+            optionInfoList: optionInfoList,
+            checked: true,
+          });
+        }
+        // 이미 장바구니에 있으면
+        else {
           cart[indexExistedProduct].productInfo.quantity +=
             productInfo.quantity;
-        } else {
-          cart.push({ productInfo: productInfo, optionInfoList: [], checked:true });
         }
 
         // Option
-        const currentCartIndex = cart.findIndex(
-          (item) => item.productInfo.product.id === productInfo.product.id,
-        );
+        // const currentCartIndex = cart.findIndex(
+        //   (item) => item.productInfo.product.id === productInfo.product.id,
+        // );
 
-        if (optionInfoList.length > 0) {
-          optionInfoList.map((optionInfo) => {
-            const indexExistedOption = cart[
-              currentCartIndex
-            ].optionInfoList.findIndex(
-              (_optionInfo) => _optionInfo.option.id === optionInfo.option.id,
-            );
-            if (indexExistedOption >= 0) {
-              cart[currentCartIndex].optionInfoList[indexExistedOption]
-                .quantity++;
-            } else {
-              cart[currentCartIndex].optionInfoList.push(optionInfo);
-            }
-          });
-        }
+        // if (currentCartIndex !== -1) {
+
+        //   cart.push({ productInfo, optionInfoList, checked:true });
+
+        //   optionInfoList.map((optionInfo) => {
+        //     const indexExistedOption = cart[
+        //       currentCartIndex
+        //     ].optionInfoList.findIndex(
+        //       (_optionInfo) => _optionInfo.option?.id === optionInfo.option?.id,
+        //     );
+        //     if (indexExistedOption !== -1) {
+        //       cart[currentCartIndex].optionInfoList[indexExistedOption]
+        //         .quantity++;
+        //     } else {
+        //       cart[currentCartIndex].optionInfoList.push(optionInfo);
+        //     }
+        //   });
+        // }
 
         set((state) => ({ cart: state.cart }));
       },
 
-      removeFromCart: ({ productId, optionId }) => {
+      removeFromCart: ({ productId }) => {
         const cart = get().cart;
-
+       
         // 상품 제거
         if (productId) {
           const newCart = cart.filter(
             ({ productInfo }) => productInfo.product.id !== productId,
-          );
+          )
+          set((state)=> ({...state, cart: newCart}))
         }
 
         // 옵션 제거
-        if (optionId) {
-          const productIndex = cart.findIndex(
-            (item) => item.productInfo.product.id === productId,
-          );
-          const newOptionList = cart[productIndex].optionInfoList.filter(
-            (optionInfo) => optionInfo.option.id !== optionId,
-          );
+        // if (optionId) {
+        //   const productIndex = cart.findIndex(
+        //     (item) => item.productInfo.product.id === productId,
+        //   );
+        //   const newOptionList = cart[productIndex].optionInfoList.filter(
+        //     (optionInfo) => optionInfo.option?.id !== optionId,
+        //   );
 
-          cart[productIndex].optionInfoList = newOptionList;
-        }
-        set((state) => ({ cart: state.cart }));
+        //   cart[productIndex].optionInfoList = newOptionList;
+        // }
+        // set((state) => ({ cart: state.cart }));
       },
     }),
     {
