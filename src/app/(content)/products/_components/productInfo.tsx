7ddd22bfc2be4b406d041220c756fecd3f1964b6
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-10-03 12:15:12 +0900

    white box 제거

--- src/app/(content)/products/_components/productInfo.tsx
+++ src/app/(content)/products/_components/productInfo.tsx
@@ -5,10 +5,9 @@ type ProductInfoType = {
 
 const ProductInfo = ({ label, data }: ProductInfoType) => {
   return (
-    <div className="flex gap-x-5 ">
+    <div className="flex gap-x-5">
       <label className="w-12 text-slate-500">{label}</label>
-      <div className="flex items-center gap-x-1">
-        <div className="h-3 w-3 bg-white"></div>
+      <div className="flex items-center gap-x-1">      
         <p>{data}</p>
       </div>
     </div>
