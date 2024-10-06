9f4fca6bddb59952d941fd7910e57b0c8d818c9b
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-10-06 15:51:26 +0900

    장바구니 정보 가져오기

---
+++ src/app/getCartData.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import db from "@/libs/db";
+
+const getCartItems = async (userId: number) => {
+  if (userId) {
+    const cartData = await db.cart.findUnique({
+      where: {
+        userId,
+      },
+      select: {
+        cartItems: {
+          include: {
+            options: true,
+            product:true,
+          },
+        },
+        
+      },
+    });
+    return cartData;
+  }
+};
+
+export default getCartItems;
