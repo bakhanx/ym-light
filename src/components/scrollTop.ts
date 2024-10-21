376b892a0f2f6fa38a6da3c3d1dcbd8b6072f1df
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-10-21 23:59:19 +0900

    브라우저 상태에 따른 스크롤top 설정

--- src/components/scrollTop.ts
+++ src/components/scrollTop.ts
@@ -1,8 +1,7 @@
-'use client'
+"use client";
 
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-
 
 // 클라이언트 측에서 스크롤 위치를 최상단으로 이동
 // 서버 컴포넌트에서 useEffect를 사용할 수 없을 때, <ScrollTop/>을 import
@@ -10,21 +9,29 @@ import { useEffect } from "react";
 const ScrollTop = () => {
   const router = useRouter();
 
+  // 페이지새로고침, 페이지떠날때
   useEffect(() => {
     const handleRouteChange = () => {
       window.scrollTo(0, 0);
     };
 
-    const handlePopState = ()=>{
-        handleRouteChange();
-    }
+    const handlePopState = () => {
+      handleRouteChange();
+    };
 
-    window.addEventListener('popstate', handlePopState);
+    window.addEventListener("popstate", handlePopState);
 
     return () => {
-      window.removeEventListener('popstate', handlePopState);
+      window.removeEventListener("popstate", handlePopState);
     };
   }, [router]);
+
+  // 브라우저 뒤로가기 할때
+  useEffect(() => {
+    window.onbeforeunload = () => {
+      window.scrollTo(0, 0);
+    };
+  }, []);
 
   return null;
 };
