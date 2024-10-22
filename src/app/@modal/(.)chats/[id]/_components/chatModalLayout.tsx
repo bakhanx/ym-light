80f8773286aa5e7a2f5dd75d795994402d0b7f28
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-10-22 19:13:38 +0900

    header 문구 수정

--- src/app/@modal/(.)chats/[id]/_components/chatModalLayout.tsx
+++ src/app/@modal/(.)chats/[id]/_components/chatModalLayout.tsx
@@ -17,11 +17,11 @@ const ChatModalLayout = ({ user, chatRoom, initialMessages }: ChatsProps) => {
 
   return (
     <>
-      {isChatVisible && (
+      {isChatVisible && user && chatRoom && (
         <div className="fixed bottom-10 right-10 z-50 h-96 w-96  rounded-lg border-4 border-blue-950  bg-white">
           <div className="fixed flex h-12 w-96 -translate-x-1 -translate-y-1 items-center justify-between rounded-t-md border-4 border-b-0 border-blue-950 bg-gradient-to-tr from-blue-950 to-black px-2 text-white">
             <div className="flex w-full items-center justify-between gap-x-2 px-4">
-              <span>{user?.username}님과의 대화</span>
+              <span>{user.id > 0 ? "판매자" : user.username}님과의 대화</span>
               <span className="text-sm">
                 <span className="text-green-500">●</span> 영업중
               </span>
