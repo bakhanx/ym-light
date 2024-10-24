468b00e0407b04cfb634497de8acb6c0bc9e6cbb
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-10-24 21:45:53 +0900

    네이밍 변경

--- src/libs/utils.ts
+++
@@ -1,9 +0,0 @@
-export function cls(...classnames: string[]) {
-  return classnames.join(" ");
-}
-
-type localeType = "ko-KR" | "en-US" | "ja-JP" | "zh-CN";
-
-export const formatOfPrice = (price: number, locale: localeType ="ko-KR") => {
-  return price.toLocaleString(locale, {maximumFractionDigits: 0});
-};
