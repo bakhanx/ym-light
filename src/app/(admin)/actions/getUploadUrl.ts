b57074b17ed28a8f1c9af627cb8ec49585b879a1
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-10-26 18:38:43 +0900

    Rename: 확장자변경 tsx -> ts

---
+++ src/app/(admin)/actions/getUploadUrl.ts
@@ -0,0 +1,18 @@
+"use server";
+
+const getUploadUrl = async () => {
+  const response = await fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${process.env.CF_ACCOUNT_ID}/images/v2/direct_upload`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.CF_API_KEY}`,
+      },
+    },
+  );
+  const data = response.json();
+  console.log(data);
+  return data;
+};
+
+export default getUploadUrl;
