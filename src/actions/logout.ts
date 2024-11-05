34d2e5217fafed947f575c633ced3f27699e1733
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-11-05 18:47:46 +0900

    Perf: redirect 제거

--- src/actions/logout.ts
+++ src/actions/logout.ts
@@ -1,10 +1,7 @@
-"use server"
+"use server";
 import getSession from "@/utils/session";
-import { redirect } from "next/navigation";
-
 
 export const logOut = async () => {
   const session = await getSession();
-  await session.destroy();
-  redirect("/");
+  session.destroy();
 };
