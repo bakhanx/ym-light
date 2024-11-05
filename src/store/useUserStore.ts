6b784a092fdb64cf3f0b34dd1d4c8a216790cca6
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-11-05 19:38:17 +0900

    Feat: 장바구니 개수 추가/제거

--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -2,8 +2,9 @@ import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
 type User = {
-  username: string;
   id: number;
+  username: string;
+  cartItemCount: number;
 };
 
 type State = {
@@ -12,6 +13,8 @@ type State = {
 
 type Actions = {
   setUser: (user: User) => void;
+  addToCartItemCount: () => void;
+  substractToCartItemCount: () => void;
   removeUser: () => void;
 };
 
@@ -21,13 +24,38 @@ const INITIAL_STATE: State = {
 
 export const useUserStore = create<State & Actions>()(
   persist(
-    (set, get) => ({
+    (set) => ({
       user: INITIAL_STATE.user,
       setUser: (user) => set({ user }),
       removeUser: () => set({ user: null }),
+      addToCartItemCount: () =>
+        set((state) => {
+          if (state.user) {
+            console.log("state user : ", state.user)
+            return {
+              user: {
+                ...state.user,
+                cartItemCount: state.user.cartItemCount + 1,
+              },
+            };
+          }
+          return state;
+        }),
+      substractToCartItemCount: () =>
+        set((state) => {
+          if (state.user) {
+            return {
+              user: {
+                ...state.user,
+                cartItemCount: state.user.cartItemCount - 1,
+              },
+            };
+          }
+          return state;
+        }),
     }),
     {
-      name: "user",
+      name: "user-storage",
       storage: createJSONStorage(() => localStorage),
       partialize: (state) => ({ user: state.user }),
     },
