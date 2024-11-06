fd1d5b22a2dbacd8ff22d3b4f728de8c201b8ca7
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-11-06 20:35:19 +0900

    Feat: 헤더 컴포넌트

---
+++ src/app/(content)/cart/_components.tsx/cartHeader.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const CartHeader = ({ itemsCount }: { itemsCount: number }) => {
+  return (
+    <div className="flex items-center divide-x-[1px] py-5 sm:flex-col sm:items-stretch sm:divide-y-[1px] sm:py-0">
+      <div className="sm:py-5">
+        <div className="inner-content px-2 lg:max-w-screen-lg lg:px-0 xl:max-w-screen-xl">
+          <span className="text-xl font-bold sm:text-2xl md:text-3xl">
+            장바구니
+          </span>
+        </div>
+      </div>
+
+      {/* total Qunatity */}
+      <div className="sm:py-5">
+        <div className="inner-content px-2 lg:max-w-screen-lg lg:px-0 xl:max-w-screen-xl">
+          <div className="flex gap-x-1 font-bold">
+            <span className="text-base sm:text-lg md:text-xl">전체</span>
+            <div className="flex h-5 w-5 items-center justify-center rounded-md bg-amber-200 p-3 text-lg">
+              <span>{itemsCount}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartHeader;
