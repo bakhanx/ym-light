e19f057cc67ea98ed188669aa05a6e54fb8d0880
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-11-06 20:34:31 +0900

    Perf: 재사용 가능 변수/함수 처리

--- src/app/(content)/cart/page.tsx
+++ src/app/(content)/cart/page.tsx
@@ -1,66 +1,79 @@
 "use client";
 
-import { Suspense, useEffect, useState } from "react";
-import Products from "./_components.tsx/products";
-import { CheckIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useCartStore } from "@/store/useCartStore";
 import { orderCartItems } from "./actions/orderCartItems";
 import deleteCartItems from "@/app/(content)/cart/actions/deleteCartItems";
-import { cls } from "@/utils/cls";
-import { formatPrice } from "@/utils/formatPrice";
 import { useUserStore } from "@/store/useUserStore";
 import getCartItems from "./actions/getCartItems";
-import Loader from "@/components/loader";
-import { isLatLong } from "validator";
+
+import CheckAllButton from "./_components.tsx/checkAllButton";
+import DeleteButton from "./_components.tsx/deleteButton";
+import CartHeader from "./_components.tsx/cartHeader";
+
+import ProductList from "./_components.tsx/productList";
+import PriceSummary from "./_components.tsx/priceSummary";
+import ProductListHeader from "./_components.tsx/ProductListHeader";
 
 // Issue
 // 0. 선택하지 않은 옵션이 장바구니에 모두 담김
+const DELIVERY_PRICE = 7000;
 
 const Cart = () => {
   const [isSelectAllClick, setIsSelectAllClick] = useState(true);
 
   const { cart, removeFromCart, isDataLoaded, setInitData, setDataLoaded } =
-    useCartStore((state) => state);
+    useCartStore();
   const { substractToCartItemCount } = useUserStore();
   const { user } = useUserStore();
-  const DELIVERY_PRICE = 7000;
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
-  const checkedCarts = cart.filter((cartItem) => cartItem.checked);
-  const totalOriginalPrice = checkedCarts
-    .map((cartItem) => {
-      console.log(cartItem);
-      const productTotalPrice = cartItem.product.price * cartItem.quantity;
-      const optionTotalPrice =
-        cartItem.options.length > 0
-          ? cartItem.options
-              .map(
-                (option) =>
-                  (cartItem.product.price + (option.option.price || 0)) *
-                  option.quantity,
-              )
-              .reduce((acc, cur) => acc + cur, 0)
-          : 0;
-      return productTotalPrice + optionTotalPrice;
-    })
-    .reduce((acc, cur) => acc + cur, 0);
 
-  const totalDiscountPrice = checkedCarts
-    .map((cartItem) => {
-      return (
-        ((cartItem.product.price * (cartItem.product.discount || 0)) / 100) *
-        (cartItem.quantity +
-          cartItem.options
-            .map((option) => option.quantity)
-            .reduce((acc, cur) => acc + cur, 0))
-      );
-    })
-    .reduce((acc, cur) => acc + cur, 0);
+  const checkedCarts = useMemo(() => {
+    return cart.filter((cartItem) => cartItem.checked);
+  }, [cart]);
+  const totalOriginalPrice = useMemo(() => {
+    return checkedCarts
+      .map((cartItem) => {
+        console.log(cartItem);
+        const productTotalPrice = cartItem.product.price * cartItem.quantity;
+        const optionTotalPrice =
+          cartItem.options.length > 0
+            ? cartItem.options
+                .map(
+                  (option) =>
+                    (cartItem.product.price + (option.option.price || 0)) *
+                    option.quantity,
+                )
+                .reduce((acc, cur) => acc + cur, 0)
+            : 0;
+        return productTotalPrice + optionTotalPrice;
+      })
+      .reduce((acc, cur) => acc + cur, 0);
+  }, [checkedCarts]);
 
-  const totalDeliveryPrice = checkedCarts.length * DELIVERY_PRICE;
-  const totalAllPrice =
-    totalOriginalPrice - totalDiscountPrice + totalDeliveryPrice;
+  const totalDiscountPrice = useMemo(() => {
+    return checkedCarts
+      .map((cartItem) => {
+        return (
+          ((cartItem.product.price * (cartItem.product.discount || 0)) / 100) *
+          (cartItem.quantity +
+            cartItem.options
+              .map((option) => option.quantity)
+              .reduce((acc, cur) => acc + cur, 0))
+        );
+      })
+      .reduce((acc, cur) => acc + cur, 0);
+  }, [checkedCarts]);
+
+  const totalDeliveryPrice = useMemo(() => {
+    return checkedCarts.length * DELIVERY_PRICE;
+  }, [checkedCarts]);
+
+  const totalAllPrice = useMemo(() => {
+    return totalOriginalPrice - totalDiscountPrice + totalDeliveryPrice;
+  }, [totalOriginalPrice, totalDiscountPrice, totalDeliveryPrice]);
 
   // 장바구니 데이터 초기화 server -> zustand
   useEffect(() => {
@@ -79,7 +92,7 @@ const Cart = () => {
     getCart();
   }, [user, isDataLoaded, setDataLoaded, setInitData]);
 
-  const handleSelectAllClick = () => {
+  const handleSelectAllClick = useCallback(() => {
     setIsSelectAllClick((prev) => !prev);
     useCartStore.setState((state) => ({
       cart: state.cart.map((cartItem) => ({
@@ -87,9 +100,9 @@ const Cart = () => {
         checked: !isSelectAllClick,
       })),
     }));
-  };
+  }, [isSelectAllClick]);
 
-  const handleDeleteClick = () => {
+  const handleDeleteClick = useCallback(() => {
     if (checkedCarts.length > 0) {
       checkedCarts.forEach((item) => {
         removeFromCart({
@@ -99,43 +112,32 @@ const Cart = () => {
         substractToCartItemCount();
       });
     }
-  };
+  }, [checkedCarts, removeFromCart, substractToCartItemCount]);
 
-  const handleOrderClick = async (
-    event: React.MouseEvent<HTMLButtonElement>,
-  ) => {
-    event.preventDefault();
-    try {
-      const res = await orderCartItems(checkedCarts);
-      if (!res?.ok) {
-        alert("로그인을 해주세요.");
-        router.push("/login");
-      } else if (res?.ok) {
-        alert("주문이 완료되었습니다.");
-        checkedCarts.forEach((item) => {
-          removeFromCart({ productId: item.productId });
-        });
-        router.push("/");
-      } else {
-        alert("Error : 주문이 취소되었습니다. 관리자에게 문의해주세요.");
-        router.push("/contact");
+  const handleOrderClick = useCallback(
+    async (event: React.MouseEvent<HTMLButtonElement>) => {
+      event.preventDefault();
+      try {
+        const res = await orderCartItems(checkedCarts);
+        if (!res?.ok) {
+          alert("로그인을 해주세요.");
+          router.push("/login");
+        } else if (res?.ok) {
+          alert("주문이 완료되었습니다.");
+          checkedCarts.forEach((item) => {
+            removeFromCart({ productId: item.productId });
+          });
+          router.push("/");
+        } else {
+          alert("Error : 주문이 취소되었습니다. 관리자에게 문의해주세요.");
+          router.push("/contact");
+        }
+      } catch (error) {
+        console.log("Error : ", error);
       }
-    } catch (error) {
-      console.log("Error : ", error);
-    }
-
-    // await orderFromCart(checkedCarts)
-    //   .then((res) => {
-    //     if (res?.ok) {
-    //       alert("주문이 완료되었습니다.");
-    //       // 1. store 삭제
-    //       // 2. db 삭제
-    //     } else {
-    //       alert("Error : 주문이 취소되었습니다. 관리자에게 문의해주세요.");;
-    //     }
-    //   })
-    //   .finally(() => router.push("/"));
-  };
+    },
+    [checkedCarts, removeFromCart, router],
+  );
 
   const handleBackButton = () => {
     router.back();
@@ -147,60 +149,21 @@ const Cart = () => {
   return (
     <>
       <div className="wrapper pt-24">
-        {/* Header */}
         <div className="divide-y-[1px] shadow-xl ">
-          {/* title  */}
-          <div className="flex items-center divide-x-[1px] py-5 sm:flex-col sm:items-stretch sm:divide-y-[1px] sm:py-0">
-            <div className="sm:py-5">
-              <div className="inner-content px-4 lg:max-w-screen-lg lg:px-0 xl:max-w-screen-xl">
-                <span className="text-xl font-bold sm:text-2xl md:text-3xl">
-                  장바구니
-                </span>
-              </div>
-            </div>
+          <CartHeader itemsCount={cart.length} />
 
-            {/* total Qunatity */}
-            <div className="sm:py-5">
-              <div className="inner-content px-4 lg:max-w-screen-lg lg:px-0 xl:max-w-screen-xl">
-                <div className="flex gap-x-1 font-bold">
-                  <span className="text-base sm:text-lg md:text-xl">전체</span>
-                  <div className="flex h-5 w-5 items-center justify-center rounded-md bg-amber-200 p-3 text-lg">
-                    <span> {cart.length}</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {/* check all */}
+          {/* check */}
           <div className=" py-5 text-sm">
-            <div className="inner-content px-4 lg:max-w-screen-lg lg:px-0 xl:max-w-screen-xl">
+            <div className="inner-content px-2 lg:max-w-screen-lg lg:px-0 xl:max-w-screen-xl">
               <div className="flex gap-x-2 divide-x-2">
-                <div className="">
-                  <button
-                    className={cls(
-                      isSelectAllClick
-                        ? "border-amber-400 bg-amber-400 text-white"
-                        : " border-amber-400 ",
-                      "flex items-center gap-x-1 rounded-md border-2 p-1 text-gray-600",
-                    )}
+                <div>
+                  <CheckAllButton
                     onClick={handleSelectAllClick}
-                  >
-                    <CheckIcon className="h-3 w-3 stroke-[3px] " />
-
-                    <span>전체선택</span>
-                  </button>
+                    isSelect={isSelectAllClick}
+                  />
                 </div>
                 <div className="px-2">
-                  <button
-                    onClick={handleDeleteClick}
-                    className="flex items-center rounded-md border-2 p-1 text-gray-600"
-                  >
-                    <span>
-                      <TrashIcon className="h-4 w-4" />
-                    </span>
-                    <span>선택삭제</span>
-                  </button>
+                  <DeleteButton onClick={handleDeleteClick} />
                 </div>
               </div>
             </div>
@@ -209,97 +172,24 @@ const Cart = () => {
 
         {/* contents */}
         <div className="bg-gray-200 py-4 sm:py-10 ">
-          <div className="m-auto max-w-screen-lg rounded-md px-4 shadow-xl lg:px-0 xl:max-w-screen-xl">
+          <div className="m-auto max-w-screen-lg rounded-md px-2 shadow-xl lg:px-0 xl:max-w-screen-xl">
             {/* header */}
-            <div>
-              <div className="hidden border-b border-t-2 border-t-black bg-white p-2 text-center font-bold sm:flex sm:p-4">
-                <button
-                  className={cls(
-                    isSelectAllClick
-                      ? "border-amber-400 bg-amber-400 text-white"
-                      : " border-amber-400 text-gray-400 ",
-                    "absolute flex items-center gap-x-1 rounded-md border-2 p-1",
-                  )}
-                  onClick={handleSelectAllClick}
-                >
-                  <CheckIcon className="h-3 w-3 stroke-[3px] " />
-                </button>
+            <ProductListHeader
+              isSelect={isSelectAllClick}
+              onClick={handleSelectAllClick}
+            />
 
-                <div className="hidden w-full sm:flex">
-                  <span className="w-[70%]">상품정보</span>
-                  <span className="w-[15%] ">상품금액</span>
-                  <span className="w-[15%] ">배송비</span>
-                </div>
-              </div>
-            </div>
-
-            {isLoading ? (
-              <div className="py-20">
-                <Loader />
-              </div>
-            ) : (
-              <>
-                {/* products */}
-                {cart.length > 0 ? (
-                  cart.map((cartItem, index) => (
-                    <div
-                      key={index}
-                      className="my-4 rounded-md border-b-[1px] border-gray-300 bg-white"
-                    >
-                      <Products
-                        cartItem={cartItem}
-                        isSelectAllClick={isSelectAllClick}
-                        index={index}
-                      />
-                    </div>
-                  ))
-                ) : (
-                  <div className="w-full bg-white p-10 text-center">
-                    장바구니에 담긴 상품이 없습니다.
-                  </div>
-                )}
-              </>
-            )}
+            <ProductList cart={cart} isLoading={isLoading} />
 
             {/* Total */}
-            <div className="flex flex-col items-center justify-center rounded-md border-4 border-amber-300 bg-white px-2 py-6 shadow-md sm:flex-row sm:px-4">
-              <div className="flex w-full items-center justify-between gap-x-1 px-4 sm:w-[70%] sm:gap-x-4 sm:border-r md:gap-x-8 lg:justify-center lg:gap-x-16">
-                <div className="flex flex-col text-center text-sm md:text-base">
-                  <span className=" text-gray-400">상품금액</span>
-                  <span className="font-bold">
-                    {formatPrice(totalOriginalPrice)}원
-                  </span>
-                </div>
-                <div>
-                  <span className="text-2xl lg:text-4xl ">-</span>
-                </div>
-                <div className="flex flex-col text-center text-sm md:text-base ">
-                  <span className="text-gray-400">할인금액</span>
-                  <span className="font-bold text-red-500">
-                    {formatPrice(totalDiscountPrice)}원
-                  </span>
-                </div>
+            <PriceSummary
+              totalOriginalPrice={totalOriginalPrice}
+              totalDiscountPrice={totalDiscountPrice}
+              totalDeliveryPrice={totalDeliveryPrice}
+              totalAllPrice={totalAllPrice}
+            />
 
-                <div>
-                  <span className="text-2xl lg:text-4xl ">+</span>
-                </div>
-
-                <div className="flex flex-col text-center text-sm md:text-base">
-                  <span className="text-gray-400">배송비</span>
-                  <span className="font-bold text-green-500">
-                    {formatPrice(totalDeliveryPrice)}원
-                  </span>
-                </div>
-              </div>
-
-              <div className="mt-4 flex w-full flex-col items-end justify-end pr-4 sm:mt-0 sm:w-[30%] sm:pt-0 lg:items-center lg:pr-0">
-                <span className="font-bold text-gray-600">총 금액</span>
-                <span className="text-xl font-bold  text-blue-500 md:text-2xl">
-                  {formatPrice(totalAllPrice)}원
-                </span>
-              </div>
-            </div>
-
+            {/* Back & Order Button */}
             <div className="flex justify-center gap-x-10 py-5">
               <button
                 onClick={handleBackButton}
