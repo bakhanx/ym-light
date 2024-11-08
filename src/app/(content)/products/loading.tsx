d40cc99107d08f12884ded6df0e636af9bb7fde0
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-11-08 21:26:12 +0900

    Feat: 로딩 스켈레톤 페이지

---
+++ src/app/(content)/products/loading.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="mx-auto min-h-screen max-w-screen-2xl px-4 py-32 md:px-20">
+      <div className="h-8 w-1/4  bg-slate-200 text-lg font-semibold sm:text-2xl" />
+      <div className="grid grid-cols-2 gap-5 pt-10 sm:grid-cols-4">
+        {[...Array(8)].map((_, index) => (
+          <div key={index} className="rounded-md">
+            <div className="flex flex-col text-sm md:text-base">
+              <div className="">
+                <div className="relative aspect-square w-full overflow-hidden rounded-t-md border-[1px] border-white bg-slate-200"></div>
+                <div className="flex h-28  flex-col justify-between border-2 bg-slate-100 p-2">
+                  <p className="h-14  bg-slate-200"></p>
+                  <div className="">
+                    <div className="text-xs text-gray-500 line-through md:text-sm">
+                      <div className="h-4  bg-slate-200"></div>
+                    </div>
+                    <div className="flex gap-x-2">
+                     
+                    </div>
+                    <div className="font-semibold">
+                      <div className="h-4 w-full  bg-slate-200"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
