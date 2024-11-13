bbde669b0200f6a333bad034f8d3993cd8e256d8
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-11-13 23:07:34 +0900

    Refactor:  Table 컴포넌트로 교체

--- src/app/(admin)/manage/order/page.tsx
+++ src/app/(admin)/manage/order/page.tsx
@@ -1,6 +1,10 @@
 import db from "@/utils/db";
 import { formatPrice } from "@/utils/formatPrice";
+import Image from "next/image";
 import React from "react";
+import Table from "../_components/table";
+import { formatDate } from "@/utils/formatDate";
+import Card from "../_components/card";
 
 const Order = async () => {
   const orderList = await db.order.findMany({
@@ -18,62 +22,58 @@ const Order = async () => {
       },
     },
   });
-  return (
-    <div className="h-screen max-w-screen-lg px-2 pt-32 mx-auto">
-      <h1 className="">주문 리스트</h1>
-      <div className="pt-10">
-        <div className="flex  divide-x-2 border-2  border-b-2 border-gray-500 text-center p-2 [&>span]:p-2">
-          <span className="w-16">주문ID</span>
-          <span className="w-36">고객명</span>
-          <span className="w-16">상품ID</span>
-          <span className="w-96">상품명</span>
-          <span className="w-48">상품가격</span>
-          <span className="w-20">할인율</span>
-          <span className="w-16">수량</span>
-        </div>
-        {orderList.map((order) => (
-          <div
-            key={order.id}
-            className="flex items-center divide-x-2 border-2 border-t-0 p-2 border-gray-500"
-          >
-            <div className="w-16  text-center">{order.id}</div>
-            <div className="w-36 p-2">{order.user.username}</div>
-            <div className="divide-y-2">
-              {order.cartItems.map((item) => (
-                <div
-                  key={item.id}
-                  className="flex items-center divide-x-2 [&>div]:p-2"
-                >
-                  <div className="w-16  text-center">{item.id}</div>
-                  <div className="flex w-96 flex-col">
-                    <div className="">{item.product.title}</div>
-                    <div>
-                      {item.options.map((optionInfo) => (
-                        <div
-                          key={optionInfo.optionId}
-                          className="text-gray-500"
-                        >
-                          {optionInfo.option.name} {}
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                  <div className="w-48">
-                    {formatPrice(item.product.price)}원
-                  </div>
-                  {/* <div>{item.product.photo}</div> */}
-                  <div className="w-20  text-center">
-                    {item.product.discount || 0}%
-                  </div>
-                  <div className="w-16  text-center">
-                    {item.quantity ||
-                      item.options.map((option) => option.quantity)}
-                  </div>
+
+  const headers = ["주문 ID", "고객명", "상품 정보", "주문일자"];
+
+  const data = orderList.map((order) => {
+    const orderData = [
+      [order.id, "text-center"],
+      [order.user.username, "text-center"],
+      [
+        order.cartItems.map((item) => (
+          <div key={item.id} className="flex items-center gap-2 py-1">
+            <div className="flex flex-col">
+              <Image
+                src={`${item.product.photos[0]}/w=200`}
+                alt={item.product.title}
+                className="h-16 w-16 object-cover"
+                width={64}
+                height={64}
+              />
+              <span className="text-sm">id: {item.id}</span>
+            </div>
+            <div>
+              <span className="block overflow-hidden text-ellipsis whitespace-nowrap">
+                {item.product.title}
+              </span>
+              {item.options.map((optionInfo) => (
+                <div key={optionInfo.optionId} className="text-gray-500">
+                  {optionInfo.option.name}
                 </div>
               ))}
+              <div>{`${formatPrice(item.product.price)}원`}</div>
+              <div>{`할인율: ${item.product.discount || 0}%`}</div>
+              <div>{`수량: ${item.quantity}`}</div>
             </div>
           </div>
-        ))}
+        )),
+        "text-left",
+      ],
+      [formatDate(order.created_at), "text-left"],
+    ];
+    return orderData;
+  });
+
+  return (
+    <div className="mx-auto min-h-screen max-w-screen-xl px-2 pt-32 xl:px-0">
+      <h1 className="text-xl font-bold">주문 리스트</h1>
+      <div className="pt-10">
+        <div className="hidden lg:block">
+          <Table headers={headers} data={data} />
+        </div>
+        <div className="block lg:hidden">
+          <Card headers={headers} data={data} />
+        </div>
       </div>
     </div>
   );
