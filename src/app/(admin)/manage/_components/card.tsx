29c8e2c318bd0087ac30ba64401b2ca0945742a5
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-11-14 17:17:02 +0900

    Feat: 타입생성

--- src/app/(admin)/manage/_components/card.tsx
+++ src/app/(admin)/manage/_components/card.tsx
@@ -1,43 +1,50 @@
-// components/Card.tsx
 import React from "react";
-import Image from "next/image";
 
-export type CardProps = {
+type CellData = React.ReactNode | string | number | number[];
+export type RowData = [CellData, string]; // [:data, :style]
+export type TableData = RowData[][];
+
+type CardProps = {
   headers: string[];
-  data: (React.ReactNode | string | number | number[])[][][];
+  data: TableData;
 };
 
+
 const Card = ({ headers, data }: CardProps) => {
-  if (data.length === 0) {
-    return <div>주문 내역이 없습니다.</div>;
-  }
   return (
     <div className="block space-y-4 lg:hidden">
-      {data.map((row, rowIndex) => (
-        <div
-          key={rowIndex}
-          className="rounded-lg border border-gray-500 p-4 shadow-md"
-        >
-          {headers.map((header, headerIndex) => (
-            <div key={headerIndex} className="mb-2">
-              <span className="font-semibold">{header}:</span>
-              {Array.isArray(row[headerIndex][0])
-                ? row[headerIndex][0]?.map((item, itemIndex) =>
-                    typeof item === "object" && React.isValidElement(item) ? (
-                      <div key={itemIndex} className="mt-2">
-                        {item}
-                      </div>
-                    ) : (
-                      <span key={itemIndex} className="block">
-                        {item}
-                      </span>
-                    ),
-                  )
-                : row[headerIndex][0]}
-            </div>
-          ))}
+      {data.length === 0 ? (
+        <div className="rounded-lg border border-gray-500 p-4 shadow-md">
+          주문 내역이 없습니다.
         </div>
-      ))}
+      ) : (
+        data.map((row, rowIndex) => (
+          <div
+            key={rowIndex}
+            className="rounded-lg border border-gray-500 p-4 shadow-md"
+          >
+            {headers.map((header, headerIndex) => (
+              <div key={headerIndex} className="mb-2">
+                <span className="font-semibold">{header}:</span>{" "}
+                {Array.isArray(row[headerIndex][0])
+                  ? (row[headerIndex][0] as (|React.ReactNode|string|number)[]
+                    )?.map((item, itemIndex) =>
+                      typeof item === "object" && React.isValidElement(item) ? (
+                        <div key={itemIndex} className="mt-2">
+                          {item}
+                        </div>
+                      ) : (
+                        <span key={itemIndex} className="block">
+                          {item}
+                        </span>
+                      ),
+                    )
+                  : row[headerIndex][0]}
+              </div>
+            ))}
+          </div>
+        ))
+      )}
     </div>
   );
 };
