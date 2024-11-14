07279083c58e71542a7e64b45e720eb3eab1314a
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-11-14 23:42:03 +0900

    Refactor: 상품리스트

--- src/app/(admin)/manage/product/edit/page.tsx
+++ src/app/(admin)/manage/product/edit/page.tsx
@@ -1,12 +1,13 @@
 import db from "@/utils/db";
 import React from "react";
-
 import Link from "next/link";
-import Image from "next/image";
-import DeleteForm from "@/app/(admin)/_components/deleteForm";
-import { deleteProduct } from "../actions/deleteProduct";
+
 import { formatPrice } from "@/utils/formatPrice";
 import { formatDate } from "@/utils/formatDate";
+import DeleteForm from "@/app/(admin)/_components/deleteForm";
+import { deleteProduct } from "../actions/deleteProduct";
+import Table, { RowData } from "../../_components/table";
+import Card from "../../_components/card";
 
 const getProducts = async () => {
   const products = await db.product.findMany({
@@ -26,76 +27,52 @@ const getProducts = async () => {
 
   return products;
 };
+
 const Edit = async () => {
   const products = await getProducts();
 
+  const headers = [
+    "ID",
+    "사진",
+    "상품명",
+    "할인율",
+    "가격",
+    "최근 수정일",
+    "등록일",
+    "",
+  ];
+
+  const data: RowData[][] =
+    products.map((product) => [
+      [product.id, "text-center"],
+      [product.photos[0],"text-center"],
+      [product.title, "text-left"],
+      [`${product.discount || 0}%`, "text-center"],
+      [`${formatPrice(product.price)}원`, "text-left whitespace-nowrap"],
+      [formatDate(product.updated_at), "text-left"],
+      [formatDate(product.created_at), "text-left"],
+      [
+        <div key={product.id} className="flex gap-x-2 xl:flex-col whitespace-nowrap">
+          <Link href={`edit/${product.id}`}>
+            <button className="bg-slate-200 p-2">편집</button>
+          </Link>
+          <DeleteForm id={product.id} action={deleteProduct} />
+        </div>,
+        "text-center",
+      ],
+    ]);
+
   return (
-    <div className="m-auto h-screen flex max-w-screen-xl flex-col gap-y-5 pt-24 text-sm md:text-base xl:px-0 px-2">
-      {/* Header */}
-      <div className="hidden w-full gap-x-6 border-b-2 border-black py-4 sm:flex">
-        <ul className="flex w-[10%] gap-x-2">
-          <li className="w-[80%] "></li>
-          <li className="w-[20%] text-center">ID</li>
-        </ul>
-
-        <ul className="flex w-[90%] gap-x-2 md:gap-x-8">
-          <li className="w-[10%] text-center">사진</li>
-          <ul className="flex w-full">
-            <li className="w-[30%] ">상품명</li>
-            <li className="w-[10%] ">할인율</li>
-            <li className="w-[30%] ">가격</li>
-            <li className="w-[25%]">최근 수정일</li>
-            <li className="w-[25%]">등록일</li>
-          </ul>
-        </ul>
-      </div>
-
-      {/* Product List */}
-      {products.map((product) => (
-        <div
-          key={product.id}
-          className="flex flex-col gap-x-6 border-b-2 border-black pb-4 sm:flex-row"
-        >
-          <div className="flex flex-row-reverse  gap-x-2 sm:flex-row sm:items-center sm:justify-normal md:w-[10%]">
-            <div className="flex w-[80%] flex-row justify-end gap-x-2 gap-y-1 sm:flex-col sm:items-center sm:justify-normal">
-              <Link href={`edit/${product.id}`}>
-                <button className="bg-slate-200 p-2">편집</button>
-              </Link>
-              <DeleteForm id={product.id} action={deleteProduct} />
-            </div>
-
-            <div className="w-[20%] sm:flex sm:text-center ">{product.id}</div>
-          </div>
-
-          <div className="flex w-full gap-x-2 pt-4  sm:w-[90%] sm:items-center sm:pt-0 md:gap-x-8">
-            <div className="relative aspect-square w-[30%] sm:w-[10%]">
-              <Image
-                fill
-                alt={String(product.id)}
-                src={`${product.photos[0]}/thumbnail`}
-              />
-            </div>
-
-            <ul className="flex w-full flex-col items-center justify-center sm:flex-row">
-              <li className=" pb-4 font-bold sm:w-[30%] sm:pb-0 sm:font-normal">
-                {product.title}
-              </li>
-              <li className="sm:w-[10%]">
-                {product.discount && `${product.discount}%`}
-              </li>
-              <li className="sm:w-[30%]">{formatPrice(product.price)}원</li>
-              <li className="text-xs sm:w-[25%] md:text-base">
-                <span className="text-gray-600 sm:hidden">(최근 수정일) </span>
-                {formatDate(product.updated_at)}
-              </li>
-              <li className="text-xs sm:w-[25%] md:text-base">
-                <span className="text-gray-600 sm:hidden">(등록일) </span>
-                {formatDate(product.created_at)}
-              </li>
-            </ul>
-          </div>
+    <div className="mx-auto min-h-screen max-w-screen-xl px-2 pt-24 xl:px-0">
+      <h1 className="text-xl font-bold">상품 리스트</h1>
+      <div className="pt-10">
+        <div className="hidden lg:block">
+          <Table headers={headers} data={data} />
         </div>
-      ))}
+        <div className="block lg:hidden">
+          <Card headers={headers} data={data} />
+        </div>
+      </div>
     </div>
   );
 };
