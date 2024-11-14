eeb6c7a8ed8262cecc1867006e2a54eb6f3adf10
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-11-14 23:39:26 +0900

    Refactor: 채팅리스트

--- src/app/(admin)/manage/chats/page.tsx
+++ src/app/(admin)/manage/chats/page.tsx
@@ -1,82 +1,63 @@
 import db from "@/utils/db";
-import { useChatStore } from "@/store/useChatStore";
-import Link from "next/link";
 import React from "react";
+
 import ChatEnterButton from "./_components/chat-enter-button";
 import { formatDate } from "@/utils/formatDate";
+import Table, { RowData } from "../_components/table";
+import Card from "../_components/card";
 
-const Chat = async () => {
+const getChatRooms = async () => {
   const chatRooms = await db.chatRoom.findMany({
     include: {
       _count: true,
       users: true,
+      messages: {
+        select: {
+          payload: true,
+        },
+        take: 1,
+        orderBy: {
+          updated_at: "desc",
+        },
+      },
     },
     orderBy: {
       updated_at: "desc",
     },
   });
 
-  return (
-    <div className="min-h-screen md:px-20 px-4 sm:pt-20 pt-32 max-w-screen-2xl mx-auto">
-      <p>채팅페이지</p>
-      <div className="flex flex-col gap-y-10 pt-10">
-        <div className="">
-          {/* PC */}
-          <table className="hidden md:table border-collapse border border-gray-200 min-w-full">
-            <thead>
-              <tr className="bg-gray-100">
-                <th className="border border-gray-300 px-4 py-2">입장</th>
-                <th className="border border-gray-300 px-4 py-2">참여자</th>
-                <th className="border border-gray-300 px-4 py-2">채팅방 ID</th>
-                <th className="border border-gray-300 px-4 py-2">최근 업데이트</th>
-              </tr>
-            </thead>
-            <tbody>
-              {chatRooms.map((room) => (
-                <tr key={room.id} className="hover:bg-gray-50">
-                  <td className="border border-gray-300 px-4 py-2 w-20">
-                    <ChatEnterButton roomId={room.id} />
-                  </td>
-                  <td className="border border-gray-300 px-4 py-2">
-                    {room.users.filter((user) => user.username !== "admin")[0]?.username}
-                  </td>
-                  <td className="border border-gray-300 px-4 py-2">{room.id}</td>
-                  <td className="border border-gray-300 px-4 py-2">
-                    {formatDate(room.updated_at)}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+  return chatRooms;
+};
 
-          {/* Mobile */}
-          <div className="md:hidden">
-            {chatRooms.map((room) => (
-              <div key={room.id} className="border border-gray-200 p-4 mb-4">
-                <div className="flex justify-between mb-2">
-                  <span className="font-bold">참여자:</span>
-                  <span>
-                    {room.users.filter((user) => user.username !== "admin")[0]?.username}
-                  </span>
-                </div>
-                <div className="flex justify-between mb-2">
-                  <span className="font-bold">채팅방 ID:</span>
-                  <span>{room.id}</span>
-                </div>
-                <div className="flex justify-between mb-2">
-                  <span className="font-bold">최근 업데이트:</span>
-                  <span>{formatDate(room.updated_at)}</span>
-                </div>
-                <div className="flex justify-end">
-                  <ChatEnterButton roomId={room.id} />
-                </div>
-              </div>
-            ))}
-          </div>
+const ChatsList = async () => {
+  const chatRooms = await getChatRooms();
+  const headers = ["참여자", "채팅방 ID", "최근 메세지", "최근 업데이트", ""];
+
+  const data: RowData[][] = chatRooms.map((room) => [
+    [
+      room.users.filter((user) => user.loginId !== "admin")[0]?.username ||
+        "",
+      "text-center whitespace-nowrap",
+    ],
+    [room.id, "whitespace-nowrap"],
+    [room.messages[0]?.payload || [], ""],
+    [formatDate(room.updated_at), ""],
+    [<ChatEnterButton key={room.id} roomId={room.id} />, "text-center whitespace-nowrap"],
+  ]);
+
+  return (
+    <div className="mx-auto min-h-screen max-w-screen-2xl px-4 pt-28  md:px-20">
+      <h1 className="text-xl font-bold">채팅페이지</h1>
+      <div className="pt-10">
+        <div className="hidden md:block">
+          <Table headers={headers} data={data} />
+        </div>
+        <div className="block md:hidden">
+          <Card headers={headers} data={data} />
         </div>
       </div>
     </div>
   );
 };
 
-export default Chat;
+export default ChatsList;
