f9bd0c86e9dacf87a78592b9dab9d3dadc332266
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-11-15 18:34:46 +0900

    Style: 해상도별 레이아웃 조정

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -62,19 +62,20 @@ export default async function Home() {
   return (
     <div className="bg-black">
       <div className="h-screen ">
-        <div className="relative flex h-full w-full justify-center">
+        <div className="relative flex h-full w-full justify-center ">
           {/* Background */}
           <div className={lightFlicker.light_container} />
 
           {/* Text */}
-          <div className="absolute z-10 flex h-full w-full max-w-screen-xl flex-col justify-center px-2 pt-24 text-white xl:px-0">
+          <div className="absolute z-10 flex h-full w-full flex-col justify-center px-2 pt-24 xl:px-0 text-white sm:px-4 max-w-screen-xl">
             {/* <NeonText /> */}
-            <div className="">
+            <div className="w-full ">
               <div className={textFlicker.light_container} />
               <div
-                className="z-50 h-[60px] w-[500px] bg-no-repeat "
+                className="h-[60px] w-full  bg-no-repeat "
                 style={{
                   backgroundImage: `url(${description.src})`,
+                  backgroundSize: 'contain'
                 }}
               />
             </div>
@@ -83,13 +84,13 @@ export default async function Home() {
       </div>
 
       <Suspense fallback="Loading...">
-        <div className="mx-auto max-w-screen-xl px-2 py-5 text-white sm:py-10 xl:px-0">
+        <div className="mx-auto max-w-screen-xl px-2 py-5 text-white sm:px-4 sm:py-10 xl:px-0">
           <div className="flex flex-col gap-y-10 divide-y-2">
             <div className="my-product-wrap">
               <div className=" text-lg font-semibold sm:text-2xl">
                 새로 등록된 상품
               </div>
-              <div className="grid  gap-8 pt-4  min-[320px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 px-4">
+              <div className="grid  gap-8 px-4  pt-4 min-[320px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4">
                 {allProducts.map((product) => (
                   <Link key={product.id} href={`/products/${product.id}`}>
                     <Card
