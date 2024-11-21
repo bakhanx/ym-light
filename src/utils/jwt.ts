dc2223a1700c3ce77d2d449407acbe57ce9de35a
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-11-21 23:58:32 +0900

    Feat: jwt 커스텀 유틸

---
+++ src/utils/jwt.ts
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+
+const JWT_SECERT = process.env.JWT_SECRET!;
+
+type TokenPayload = {
+  userId: number;
+};
+
+export const createToken = (userId: number) => {
+  const token = jwt.sign({ userId }, JWT_SECERT, { expiresIn: "1h" });
+  return token;
+};
+
+export const verifyToken = (token: string): TokenPayload | null => {
+  try {
+    const decoded = jwt.verify(token, JWT_SECERT) as TokenPayload;
+    return decoded;
+  } catch (error) {
+    console.log("Invalid Token", error);
+    return null;
+  }
+};
+
+export const getUserIdFromToken = () => {
+  const token = sessionStorage.getItem("auth-token");
+  if (token) {
+    const decodedToken = verifyToken(token);
+    if (decodedToken) {
+      return decodedToken.userId;
+    }
+  }
+  return null;
+};
