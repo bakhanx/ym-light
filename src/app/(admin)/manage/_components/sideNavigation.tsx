2963d065640c5086dccd0022cd0738b3d8c0c9c8
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-11-27 22:30:37 +0900

    Perf: UI 수정

--- src/app/(admin)/manage/_components/sideNavigation.tsx
+++ src/app/(admin)/manage/_components/sideNavigation.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { cls } from "@/utils/cls";
-import { topNavLinks } from "@/utils/constants/topNavLinks";
+import { sideNavLinks } from "@/utils/constants/sideNavLinks";
 import { Bars3Icon } from "@heroicons/react/16/solid";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React, { useState } from "react";
 
 const SideNavigation = () => {
@@ -11,28 +12,58 @@ const SideNavigation = () => {
     setIsVisible((prev) => !prev);
   };
 
+  const pathname = usePathname();
+  console.log(pathname);
+
   return (
-    <div className="fixed right-0 pt-28 z-navigation">
-      <div className="flex w-full justify-end">
-        <button onClick={handleToggle}>
-          <Bars3Icon className="size-8 bg-black text-white" />
-        </button>
+    <>
+      {/* Desktop */}
+      <div className="mt-20 h-screen min-w-48 md:w-[calc((100%-1280px)/2)]  bg-slate-800 text-white">
+        <div className="p-3 text-center font-bold italic border-b-2">관리자 페이지</div>
+        <nav className="mt-4">
+          {sideNavLinks.map((link) => {
+            const Icon = link.icon;
+            const isActive = pathname === link.href
+            return (
+              <Link href={link.href} key={link.key} className=" ">
+                <div
+                  className={cls(
+                    isActive ? "bg-gray-700" : "bg-none",
+                    `flex items-center gap-x-4 rounded px-4 py-2.5 text-white transition duration-200 `,
+                  )}
+                >
+                  <Icon className="size-4 text-white" />
+                  {link.name}
+                </div>
+              </Link>
+            );
+          })}
+        </nav>
       </div>
 
-      <div
-        className={`${cls(isVisible ? "" : " translate-x-48 ", "absolute right-0 grid items-center gap-y-4 border duration-300 bg-blue-900 bg-opacity-90 p-1 text-sm text-white transition-transform sm:gap-x-8 md:text-base [&>button]:w-40")}`}
-      >
-        {topNavLinks.map((navLink) => (
-          <Link
-            key={navLink.key}
-            href={navLink.href}
-            className="border-sm w-20 p-1 text-center hover:bg-blue-200 sm:w-24"
-          >
-            {navLink.name}
-          </Link>
-        ))}
+      {/* Mobile */}
+      <div className="fixed right-0 z-navigation pt-28 sm:hidden">
+        <div className="flex w-full justify-end">
+          <button onClick={handleToggle}>
+            <Bars3Icon className="size-8 bg-black " />
+          </button>
+        </div>
+
+        <div
+          className={`${cls(isVisible ? "" : " translate-x-48 ", "absolute right-0 grid items-center gap-y-4 border bg-blue-900 bg-opacity-90 p-1 text-sm text-white transition-transform duration-300 sm:gap-x-8 md:text-base [&>button]:w-40")}`}
+        >
+          {sideNavLinks.map((navLink) => (
+            <Link
+              key={navLink.key}
+              href={navLink.href}
+              className="border-sm w-20 p-1 text-center hover:bg-blue-200 sm:w-24"
+            >
+              {navLink.name}
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
