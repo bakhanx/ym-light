6244575e6c710a33461b003573c215a381f92747
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-11-27 22:30:09 +0900

    Feat: 유저방문수차트 추가

--- src/app/(admin)/manage/page.tsx
+++ src/app/(admin)/manage/page.tsx
@@ -1,12 +1,26 @@
-"use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import UserVisitChart from "./_components/chart/userVisitChart";
+import OrdersChart from "./_components/chart/ordersChart";
+import StockChart from "./_components/chart/stockChart";
+import GalleryViewsChart from "./_components/chart/galleryViewsChart";
+import db from "@/utils/db";
+import getVisitCount from "./_components/chart/action/getVisitCount";
 
 const Manage = () => {
+
   return (
     <div className="h-screen">
-      <div className="pt-32">관리자 페이지
+      <div className="pt-20 ">관리자 페이지</div>
+      <div className="flex">
+        <div className="h-[400px] w-[800px]">
+         
+          <UserVisitChart />
+        </div>
+        {/* <OrdersChart fetchData={} />
+        <StockChart fetchData={} />
+        <GalleryViewsChart fetchData={} /> */}
       </div>
-      </div>
+    </div>
   );
 };
 
