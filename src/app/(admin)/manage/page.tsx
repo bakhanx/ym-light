6e1ab6c66c9ef6c67e5d28003e03fc673ba8a388
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-11-28 23:57:18 +0900

    Feat: 주문,재고 통계 추가

--- src/app/(admin)/manage/page.tsx
+++ src/app/(admin)/manage/page.tsx
@@ -2,23 +2,23 @@ import React, { useEffect, useState } from "react";
 import UserVisitChart from "./_components/chart/userVisitChart";
 import OrdersChart from "./_components/chart/ordersChart";
 import StockChart from "./_components/chart/stockChart";
-import GalleryViewsChart from "./_components/chart/galleryViewsChart";
+
 import db from "@/utils/db";
-import getVisitCount from "./_components/chart/action/getVisitCount";
 
-const Manage = () => {
-
+const Manage = async () => {
   return (
-    <div className="h-screen">
+    <div className="min-h-screen">
       <div className="pt-20 ">관리자 페이지</div>
-      <div className="flex">
+      <div className="flex flex-col gap-8">
         <div className="h-[400px] w-[800px]">
-         
           <UserVisitChart />
         </div>
-        {/* <OrdersChart fetchData={} />
-        <StockChart fetchData={} />
-        <GalleryViewsChart fetchData={} /> */}
+        <div className="h-[400px] w-[800px]">
+          <OrdersChart />
+        </div>
+        <div className="h-[400px] w-[800px]">
+          <StockChart />
+        </div>
       </div>
     </div>
   );
