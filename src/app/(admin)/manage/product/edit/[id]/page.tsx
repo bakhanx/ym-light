b68b29900f6fac4ab2d1f8f8e3886d170136b918
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-11-29 22:43:37 +0900

    Perf: fetch 제한

--- src/app/(admin)/manage/product/edit/[id]/page.tsx
+++ src/app/(admin)/manage/product/edit/[id]/page.tsx
@@ -1,25 +1,25 @@
-import db from "@/utils/db";
+"use client";
+
 import Upload from "../../upload/page";
+import { useEffect, useState } from "react";
+import getProduct from "@/app/(content)/products/actions/getProduct";
 
-const getProduct = async (id: string) => {
-  const product = await db.product.findUnique({
-    where: {
-      id: Number(id),
-    },
-    include:{
-      options: true
-    }
-  });
-  return product;
-};
+const EditProduct = ({ params }: { params: { id: string } }) => {
+  const { id } = params;
+  const [product, setProduct] = useState<any>(null);
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const fetchedProduct = await getProduct(+id);
+      console.log("fetch");
+      setProduct(fetchedProduct);
+    };
+    fetchProduct();
+  }, [id]);
 
-const EditProduct = async ({ params }: { params: { id: string } }) => {
-  const product = await getProduct(params.id);
-  console.log(product);
-
+  if (!product) return <div>Loading...</div>;
   return (
     <div>
-      <Upload product={product} isEdit={true} />;
+      <Upload product={product} isEdit={true} />
     </div>
   );
 };
