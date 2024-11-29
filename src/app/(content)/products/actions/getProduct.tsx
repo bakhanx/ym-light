6baeba05be00b008c03337e71ddc01dec29715e9
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-11-29 22:55:26 +0900

    Feat: include option

--- src/app/(content)/products/actions/getProduct.tsx
+++ src/app/(content)/products/actions/getProduct.tsx
@@ -1,9 +1,17 @@
-import db from "@/utils/db";
+"use server";
 
-const getProduct = async (id : number) => {
+import db from "@/utils/db";
+/**
+ * 상품정보를 가져오는 함수
+ * @param id - 상품 id
+ */
+const getProduct = async (id: number) => {
   const product = await db.product.findUnique({
     where: {
-      id : +id
+      id: +id,
+    },
+    include: {
+      options: true,
     },
   });
   return product;
