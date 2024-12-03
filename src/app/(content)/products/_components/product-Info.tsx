6ce8f2b48420e26e25b97faac34476f1d36ea8df
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-12-03 21:06:26 +0900

    Fix: 레이블 줄바꿈 막기

--- src/app/(content)/products/_components/product-Info.tsx
+++ src/app/(content)/products/_components/product-Info.tsx
@@ -6,7 +6,7 @@ type ProductInfoType = {
 const ProductInfo = ({ label, data }: ProductInfoType) => {
   return (
     <div className="flex gap-x-5">
-      <label className="w-12 text-slate-500">{label}</label>
+      <label className="w-12 text-slate-500 shrink-0">{label}</label>
       <div className="flex items-center gap-x-1">      
         <p>{data}</p>
       </div>
