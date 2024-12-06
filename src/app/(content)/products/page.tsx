a7f08c114e71e1650598c530ad2550fd515ca1b2
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-12-06 23:40:27 +0900

    Feat: 상품 정렬기능 추가

--- src/app/(content)/products/page.tsx
+++ src/app/(content)/products/page.tsx
@@ -1,14 +1,90 @@
-import React from "react";
-import ProductList from "./_components/productList";
-import getProducts from "./actions/getProducts";
+"use client";
 
-const ProductsContainer = async () => {
-  const products = await getProducts();
+import React, { useEffect, useState, useCallback } from "react";
+import { useSearchParams, usePathname } from "next/navigation";
+import { CategoryNav } from "./_components/categoryNav";
+import { SortOptionNav } from "./_components/SortOptions";
+import ProductList from "./_components/productList";
+import Pagination from "./_components/pagination";
+import getProducts from "./actions/getProducts";
+import { categoryMap, LIMIT_COUNT } from "./utils/constants";
+
+export type ProductListType = {
+  category: string;
+  id: number;
+  title: string;
+  price: number;
+  discount: number | null;
+  photos: string[];
+  stock: number;
+  created_at: Date;
+  updated_at: Date;
+};
+
+const ProductsLayout = () => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const category = searchParams.get("category") || "all";
+  const query = searchParams.get("query") || "";
+  const page = parseInt(searchParams.get("page") || "1");
+  const sortType = searchParams.get("sortType") || "popularity";
+
+  const [products, setProducts] = useState<ProductListType[]>([]);
+  const [totalPages, setTotalPages] = useState(1);
+
+  const fetchData = useCallback(async () => {
+    const { products, totalPages } = await getProducts({
+      page,
+      limit: LIMIT_COUNT,
+      category,
+      query,
+      sortType,
+    });
+    setProducts(products);
+    setTotalPages(totalPages);
+  }, [page, category, query, sortType]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  const handlePageChange = (newPage: number) => {
+    const newParams = new URLSearchParams(searchParams.toString());
+    newParams.set("page", newPage.toString());
+    window.history.pushState({}, "", `${pathname}?${newParams.toString()}`);
+    fetchData(); // Fetch data again after URL is updated
+  };
+
+  const handleSortTypeChange = (newSortType: string) => {
+    const newParams = new URLSearchParams(searchParams.toString());
+    newParams.set("sortType", newSortType);
+    if (totalPages === 1) {
+      newParams.delete("page");
+    } else {
+      newParams.set("page", "1");
+    }
+    window.history.pushState({}, "", `${pathname}?${newParams.toString()}`);
+    fetchData(); // Fetch data again after URL is updated
+  };
+
   return (
     <div className="mx-auto min-h-screen max-w-screen-xl px-4 py-32 xl:px-0">
+      <CategoryNav />
+      <div className="text-xl font-bold px-2 pt-8">{categoryMap[category]}</div>
+      <SortOptionNav
+        sortType={sortType}
+        handleSortTypeChange={handleSortTypeChange}
+      />
       <ProductList products={products} />
+      {totalPages > 1 && (
+        <Pagination
+          currentPage={page}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+        />
+      )}
     </div>
   );
 };
 
-export default ProductsContainer;
+export default ProductsLayout;
