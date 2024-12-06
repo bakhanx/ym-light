eeb40820589d9769ab568107f1c60989d3677696
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-12-06 23:42:41 +0900

    Feat: categoryMap, LimitCount

---
+++ src/app/(content)/products/utils/constants.ts
@@ -0,0 +1,9 @@
+export const categoryMap: Record<string, string> = {
+  all: "전체상품",
+  chandelier: "샹들리에",
+  neonsign: "네온사인",
+  walllamp: "벽등",
+  moodlamp: "무드등",
+};
+
+export const LIMIT_COUNT = 8;
