4025dfc0ea240129204a6bd0911cc66b253cf5a0
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-12-09 01:46:01 +0900

    Feat: 모바일용 추가

--- src/app/(content)/products/_components/sortOptionNav.tsx
+++ src/app/(content)/products/_components/sortOptionNav.tsx
@@ -1,5 +1,5 @@
-import { Dispatch, SetStateAction } from "react";
 import SortButton from "./sortButton";
+import { ChevronDownIcon } from "@heroicons/react/16/solid";
 
 const sortOptions = [
   { value: "popularity", label: "인기순" },
@@ -21,19 +21,40 @@ export const SortOptionNav = ({
   handleSortTypeChange,
 }: SortOptionNav) => {
   return (
-    <div className="hidden sm:block sm:mt-8 py-4 bg-gray-200 border-t-2 border-black">
-      <div className="flex gap-2 text-sm px-2">
-        {sortOptions.map((option) => (
-          <SortButton
-            key={option.value}
-            sortType={sortType}
-            handleSortTypeChange={handleSortTypeChange}
-            type={option.value}
-          >
-            {option.label}
-          </SortButton>
-        ))}
+    <>
+      {/* PC */}
+      <div className="hidden border-t-2 border-black bg-gray-200 py-4 sm:mt-8 sm:block w-full">
+        <div className="flex gap-2 px-2 text-sm">
+          {sortOptions.map((option) => (
+            <SortButton
+              key={option.value}
+              sortType={sortType}
+              handleSortTypeChange={handleSortTypeChange}
+              type={option.value}
+            >
+              {option.label}
+            </SortButton>
+          ))}
+        </div>
       </div>
-    </div>
+
+      {/* mobile */}
+      <div className="flex items-center justify-end sm:flex-col sm:items-baseline sm:justify-between">
+        <div className="relative pr-2 sm:hidden">
+          <select
+            value={sortType}
+            onChange={(e) => handleSortTypeChange(e.target.value)}
+            className="block w-full appearance-none rounded-md bg-transparent py-2 pr-6 text-end text-sm"
+          >
+            {sortOptions.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+          <ChevronDownIcon className="pointer-events-none absolute right-1 top-1/2 size-5 -translate-y-1/2 transform text-gray-500" />
+        </div>
+      </div>
+    </>
   );
 };
