05ff5edebf0520fd9bab156844ec0b3dc7169336
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-12-11 18:06:56 +0900

    Feat: RoomId 추가

--- src/store/useChatStore.ts
+++ src/store/useChatStore.ts
@@ -3,29 +3,35 @@ import { createJSONStorage, persist } from "zustand/middleware";
 
 type State = {
   isChatVisible: boolean;
+  chatroomId: string;
 };
 
 type Actions = {
   setIsChatVisible: (isChatVisible: boolean) => void;
+  setChatroomId: (chatroomId: string) => void;
 };
 
 const INITIAL_STATE: State = {
   isChatVisible: false,
+  chatroomId: "",
 };
 
 export const useChatStore = create<State & Actions>()(
   persist(
     (set, get) => ({
       isChatVisible: INITIAL_STATE.isChatVisible,
-
+      chatroomId: INITIAL_STATE.chatroomId,
       setIsChatVisible: (isChatVisible) => {
         set({ isChatVisible });
+      },
+      setChatroomId: (chatroomId) => {
+        set({ chatroomId });
       },
     }),
     {
       name: "chat",
       storage: createJSONStorage(() => sessionStorage),
-      partialize: (state) => ({ isChatVisible: state.isChatVisible }),
+      partialize: (state) => ({ isChatVisible: state.isChatVisible, chatroomId: state.chatroomId }),
     },
   ),
 );
