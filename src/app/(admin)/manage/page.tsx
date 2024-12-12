a0f86085a13a04af6c3632a2d4c057c2099780b4
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-12-12 22:18:28 +0900

    Style: 반응형 적용

--- src/app/(admin)/manage/page.tsx
+++ src/app/(admin)/manage/page.tsx
@@ -8,15 +8,15 @@ import db from "@/utils/db";
 const Manage = async () => {
   return (
     <div className="min-h-screen">
-      <div className="pt-20 ">관리자 페이지</div>
-      <div className="flex flex-col gap-8">
-        <div className="h-[400px] w-[800px]">
+      <div className="sm:pt-20 pt-24 ">관리자 페이지</div>
+      <div className="flex flex-col gap-8 items-center ">
+        <div className="h-auto w-full">
           <UserVisitChart />
         </div>
-        <div className="h-[400px] w-[800px]">
+        <div className="h-auto w-full">
           <OrdersChart />
         </div>
-        <div className="h-[400px] w-[800px]">
+        <div className="auto w-full">
           <StockChart />
         </div>
       </div>
