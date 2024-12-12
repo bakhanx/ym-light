3f724b41f6c45c3486356d9857cc8cc272867ff3
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-12-12 22:18:18 +0900

    Style: 가운데 정렬

--- src/app/(admin)/manage/layout.tsx
+++ src/app/(admin)/manage/layout.tsx
@@ -5,7 +5,9 @@ const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex">
       <SideNavigation />
-      <div className="p-4 max-w-screen-xl">{children}</div>
+      <div className="w-full justify-center">
+        <div className="max-w-screen-xl p-4">{children}</div>
+      </div>
     </div>
   );
 };
