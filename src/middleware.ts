decc88f22756ab6a267e6c629eea3cf45beeace5
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-01-14 19:23:41 +0900

    Feat: /my 리디렉션 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
-import React from "react";
 import getSession from "./utils/session";
 
 const publicPaths = new Set(["/login", "/register"]);
 const privatePaths = new Set(["/profile", "/manage"]);
 const adminPaths = "/manage"
+
 const middleware = async (req: NextRequest) => {
   const session = await getSession();
   const isPublicPath = publicPaths.has(req.nextUrl.pathname);
   const isPrivatePath = privatePaths.has(req.nextUrl.pathname);
   const isAdminPath = req.nextUrl.pathname.startsWith(adminPaths);
+
   // 비로그인 유저 접근 제한
   if (!session.id && isPrivatePath) {
     return NextResponse.redirect(new URL("/", req.url));
@@ -22,6 +23,13 @@ const middleware = async (req: NextRequest) => {
   if(session.id !== 1 && isAdminPath) {
     return NextResponse.redirect(new URL("/", req.url));
   }
+
+  // 강제 리디렉션
+  if(req.nextUrl.pathname === "/my"){
+    return NextResponse.redirect(new URL("/my/home", req.url))
+  }
+
+  return NextResponse.next();
 };
 
 export const config = {
