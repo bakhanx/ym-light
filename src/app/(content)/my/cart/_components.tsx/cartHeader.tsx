5aef6da46458db53c832ec309de37449343cfc1f
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-01-15 18:39:09 +0900

    Style: px 수정

--- src/app/(content)/my/cart/_components.tsx/cartHeader.tsx
+++ src/app/(content)/my/cart/_components.tsx/cartHeader.tsx
@@ -5,7 +5,7 @@ const CartHeader = ({ itemsCount }: { itemsCount: number }) => {
     <div className="flex items-center divide-x-[1px] py-5 sm:flex-col sm:items-stretch sm:divide-y-[1px] sm:py-0">
       <div className="sm:py-5">
         <div className="inner-content max-w-screen-xl px-2 sm:px-4 xl:px-0">
-          <span className="text-xl font-bold sm:text-2xl md:text-3xl">
+          <span className="text-xl font-bold sm:text-2xl md:text-3xl px-2">
             장바구니
           </span>
         </div>
@@ -15,7 +15,7 @@ const CartHeader = ({ itemsCount }: { itemsCount: number }) => {
       <div className="sm:py-5">
         <div className="inner-content max-w-screen-xl px-2 sm:px-4 xl:px-0">
           <div className="flex gap-x-1 font-bold">
-            <span className="text-base sm:text-lg md:text-xl">전체</span>
+            <span className="text-base sm:text-lg md:text-xl px-2">전체</span>
             <div className="flex h-5 w-5 items-center justify-center rounded-md bg-amber-200 p-3 text-lg">
               <span>{itemsCount}</span>
             </div>
