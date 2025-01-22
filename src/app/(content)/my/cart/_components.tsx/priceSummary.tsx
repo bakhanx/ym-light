1c4d2a6317b6ee9d28e42a9844e6fb7fcbca5b69
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-01-22 17:48:46 +0900

    Style: 모바일 텍스트 위치 수정

--- src/app/(content)/my/cart/_components.tsx/priceSummary.tsx
+++ src/app/(content)/my/cart/_components.tsx/priceSummary.tsx
@@ -13,8 +13,8 @@ const PriceSummary = ({
   totalAllPrice: number;
 }) => {
   return (
-    <div className="flex flex-col items-center justify-center rounded-md border-4 border-amber-300 bg-white px-2 py-6 shadow-md sm:flex-row sm:px-4">
-      <div className="flex w-full items-center justify-between gap-x-1 px-4 sm:w-[70%] sm:gap-x-4 sm:border-r md:gap-x-8 lg:justify-center lg:gap-x-16">
+    <div className="flex flex-col items-center justify-center rounded-md border-4 border-amber-300 bg-white px-2 py-6 shadow-md md:flex-row md:px-2">
+      <div className="flex w-full items-center justify-between gap-x-1 px-4 md:w-[70%] md:gap-x-4 md:border-r lg:justify-center lg:gap-x-12">
         <div className="flex flex-col text-center text-sm md:text-base">
           <span className=" text-gray-400">상품금액</span>
           <span className="font-bold">{formatPrice(totalOriginalPrice)}원</span>
@@ -41,9 +41,9 @@ const PriceSummary = ({
         </div>
       </div>
 
-      <div className="mt-4 flex w-full flex-col items-end justify-end pr-4 sm:mt-0 sm:w-[30%] sm:pt-0 lg:items-center lg:pr-0">
+      <div className="mt-4 flex w-full flex-col items-end justify-end pr-4 md:mt-0 md:w-[30%] md:pt-0 md:items-center md:pr-0 ">
         <span className="font-bold text-gray-600">총 금액</span>
-        <span className="text-xl font-bold  text-blue-500 md:text-2xl">
+        <span className="text-lg font-bold  text-blue-500 lg:text-2xl">
           {formatPrice(totalAllPrice)}원
         </span>
       </div>
