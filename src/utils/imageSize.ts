039a1482b9504992b7999c982ae41c19ef3be499
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-01-23 18:55:01 +0900

    Feat: 이미지 w, h default값 지정

--- src/utils/imageSize.ts
+++ src/utils/imageSize.ts
@@ -1,11 +1,17 @@
 import imageSize from "image-size";
 
+const DIMENSION_WIDTH = 1204;
+const DIMENSION_HEIGHT = 768;
+
 export const getImageSize = async (url: string) => {
   const response = await fetch(url);
   const arrayBuffer = await response.arrayBuffer();
   const uint8Array = new Uint8Array(arrayBuffer);
   const dimensions = imageSize(uint8Array);
-  return { width: dimensions.width, height: dimensions.height };
+  return {
+    width: dimensions.width || DIMENSION_WIDTH,
+    height: dimensions.height || DIMENSION_HEIGHT,
+  };
 };
 export const getImageData = async (url: string) => {
   const response = await fetch(url);
