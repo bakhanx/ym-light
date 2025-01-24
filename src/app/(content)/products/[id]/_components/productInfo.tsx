25e4dcac8172dc2a7673f271479c52ef64edf8d4
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-01-24 21:19:25 +0900

    Feat: 상품 정보

---
+++ src/app/(content)/products/[id]/_components/productInfo.tsx
@@ -0,0 +1,21 @@
+import Item from "./item";
+import Photos from "./photos";
+
+const ProductInfo = ({
+    InfoPhotosWithSize,
+  }: {
+    InfoPhotosWithSize: { src: string; width: number; height: number }[];
+  }) => (
+    <Item
+      title="상품 상세정보"
+      content={
+        InfoPhotosWithSize.length > 0 ? (
+          <Photos photosWithSize={InfoPhotosWithSize} />
+        ) : (
+          <p>표시할 게시물이 없습니다.</p>
+        )
+      }
+    />
+  );
+
+  export default ProductInfo
