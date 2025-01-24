b160af8ab88bd007e261abf4da4a39249711f96f
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-01-24 21:19:47 +0900

    Feat: 관련 상품

---
+++ src/app/(content)/products/[id]/_components/relatedProducts.tsx
@@ -0,0 +1,13 @@
+import Item from "./item";
+
+const RelatedProducts = () => (
+    <Item
+      title="관련 상품"
+      content={
+        <div>
+          <p>관련 상품 4개</p>
+        </div>
+      }
+    />
+  );
+  export default RelatedProducts
