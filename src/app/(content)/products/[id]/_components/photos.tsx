d349079fb9cb242b3c7d43594e8c81a0f88fc5c8
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-01-24 21:19:19 +0900

    Feat: 상세 사진

---
+++ src/app/(content)/products/[id]/_components/photos.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+const Photos = ({
+  photosWithSize,
+}: {
+  photosWithSize: { src: string; width: number; height: number }[];
+}) => (
+  <div className="flex flex-col">
+    {photosWithSize.map((photo) => (
+      <div
+        key={photo.src}
+        className="relative flex h-auto w-full justify-center object-contain"
+      >
+        <Image
+          src={photo.src}
+          alt={`Product Detail Image`}
+          width={photo.width}
+          height={photo.height}
+        />
+      </div>
+    ))}
+  </div>
+);
+
+export default Photos;
