8e7808c8084613b4a2302c6fdf793553cebef0cc
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-01-24 21:19:41 +0900

    Feat: 상품 문의

---
+++ src/app/(content)/products/[id]/_components/qna.tsx
@@ -0,0 +1,14 @@
+import Item from "./item";
+
+const QnA = () => (
+    <Item
+      title="상품 문의"
+      content={
+        <div>
+          <p>문의글이 없습니다.</p>
+        </div>
+      }
+    />
+  );
+
+  export default QnA
