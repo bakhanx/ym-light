d20eca9abc64884b05bd6cf986e816f1a7d739ea
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-02-12 22:50:45 +0900

    Feat: 상품리스트 컴포넌트

---
+++ src/app/(home)/_components/product-section.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+
+import { Suspense } from "react";
+import CardSkeleton from "./card-skeleton";
+import Card from "./card";
+
+type ProductSectionProps = {
+  title: string;
+  products: {
+    id: number;
+    title: string;
+    photos: string[];
+    discount?: number;
+  }[];
+  count: number;
+};
+
+const ProductSection = ({ title, products, count }: ProductSectionProps) => (
+  <div className="my-product-wrap">
+    <div className="text-lg font-semibold sm:text-2xl">{title}</div>
+    <div className="grid grid-cols-2 gap-y-8 pt-4 min-[480px]:grid-cols-2 sm:grid-cols-2 sm:gap-y-16 md:grid-cols-4 xl:gap-16">
+      <Suspense fallback={<CardSkeleton count={count} />}>
+        {products?.map((product) => (
+          <Link key={product.id} href={`/products/${product.id}`}>
+            <Card
+              name={product.title}
+              photoURL={product.photos[0]}
+              discount={product.discount || undefined}
+            />
+          </Link>
+        ))}
+      </Suspense>
+    </div>
+  </div>
+);
+
+export default ProductSection;
