f40ec694e6bfe6c1608657c8096b41a6a3e600a9
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-03-04 23:15:04 +0900

    Refactor: 함수 전체 최적화

--- src/app/(content)/products/_components/options.tsx
+++ src/app/(content)/products/_components/options.tsx
@@ -8,7 +8,6 @@ import React, { useEffect, useMemo, useState } from "react";
 export type selectedItemType = {
   option: Option;
   quantity: number;
-  price: number;
   totalPrice: number;
 };
 
@@ -16,275 +15,156 @@ type OptionsType = {
   options: Option[];
   price: number;
   discount: number | null;
-  parentFunc: Function;
+  parentFunc: (items: selectedItemType[]) => void;
 };
 
 const Options = ({ options, price, discount, parentFunc }: OptionsType) => {
-  const calcPrice = (price: number) => {
-    if (discount) {
-      return price - (price * discount) / 100;
-    }
-    return price;
-  };
+  const calcPrice = useMemo(
+    () => (price: number) => {
+      return discount ? price - (price * discount) / 100 : price;
+    },
+    [discount],
+  );
 
   const [selectedItemList, setSelectedItemList] = useState<selectedItemType[]>(
     [],
   );
-  const [totalItemListPrice, setTotalItemListPrice] = useState(0);
-  const [totalOriginalPrice, setTotalOriginalPrice] = useState(0);
-  const [quantity, setQuantity] = useState(Array(options.length).fill(0));
-
   const [isOpenOption, setIsOpenOption] = useState(false);
 
-  const handleOpenOption = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setIsOpenOption((prev) => !prev);
-  };
+  const totalItemListPrice = useMemo(() => {
+    return selectedItemList.reduce((acc, item) => acc + item.totalPrice, 0);
+  }, [selectedItemList]);
 
-  const handleSelectOption = (index: number, e: any) => {
-    const temp = [...quantity];
-    temp[index]++;
-    setQuantity(temp);
-
-    const clickedItem = options[index];
-    const isExistItemInList = selectedItemList.find(
-      (selectedItem) => selectedItem.option.index === clickedItem.index,
+  const totalOriginalPrice = useMemo(() => {
+    return selectedItemList.reduce(
+      (acc, item) => acc + item.quantity * (price + (item.option.price || 0)),
+      0,
     );
+  }, [selectedItemList, price]);
 
-    if (!isExistItemInList) {
-      const newClickedItem = {
-        option: clickedItem,
-        price: clickedItem.price || 0,
-        quantity: 1,
-        totalPrice: calcPrice(price as number) + (clickedItem.price || 0),
-      };
+  const handleOpenOption = () => setIsOpenOption((prev) => !prev);
 
-      setSelectedItemList((prev) => [...prev, newClickedItem]);
-    } else {
-      const newSelectedItemList = selectedItemList.map((selectedItem) =>
-        selectedItem.option.index === clickedItem.index
-          ? {
-              ...selectedItem,
-              quantity: selectedItem.quantity + 1,
-              totalPrice:
-                selectedItem.totalPrice +
-                calcPrice(price) +
-                (clickedItem.price || 0),
-            }
-          : selectedItem,
-      );
-      setSelectedItemList(newSelectedItemList);
-    }
+  const handleSelectOption = (index: number) => {
+    setSelectedItemList((prev) => {
+      const existingItem = prev.find((item) => item.option.index === index);
+      if (existingItem) {
+        return prev.map((item) =>
+          item.option.index === index
+            ? {
+                ...item,
+                quantity: item.quantity + 1,
+                totalPrice:
+                  item.totalPrice + calcPrice(price) + (item.option.price || 0),
+              }
+            : item,
+        );
+      }
+      return [
+        ...prev,
+        {
+          option: options[index],
+          quantity: 1,
+          totalPrice: calcPrice(price) + (options[index].price || 0),
+        },
+      ];
+    });
     setIsOpenOption(false);
   };
 
-  const handleDeleteOption = (
-    index: number,
-    event: React.MouseEvent<HTMLButtonElement>,
-  ) => {
-    const selectedItemIndex = selectedItemList.findIndex(
-      (item) => item.option.id === options[index].id,
+  const handleDeleteOption = (index: number) => {
+    setSelectedItemList((prev) =>
+      prev.filter((item) => item.option.index !== index),
     );
-    console.log(selectedItemIndex);
-
-    if (selectedItemIndex !== -1) {
-      setSelectedItemList((prev) => {
-        const temp = [...prev];
-        temp.splice(selectedItemIndex, 1);
-        return temp;
-      });
-
-      setQuantity((prev) => {
-        const temp = [...prev];
-        temp[index] = 0;
-        return temp;
-      });
-    }
   };
 
-  const handleButtonClick = (
-    index: number,
-    buttonType: "add" | "substract",
-  ) => {
-    const newSelectedItemList = [...selectedItemList];
-    const foundItem = newSelectedItemList.find(
-      (newSelectedItem) => newSelectedItem.option.index === index,
+  const handleQuantityChange = (index: number, change: number) => {
+    setSelectedItemList((prev) =>
+      prev.map((item) =>
+        item.option.index === index
+          ? {
+              ...item,
+              quantity: Math.max(1, item.quantity + change),
+              totalPrice:
+                (calcPrice(price) + (item.option.price || 0)) *
+                Math.max(1, item.quantity + change),
+            }
+          : item,
+      ),
     );
-
-    if (foundItem) {
-      if (buttonType === "add") {
-        foundItem.quantity++;
-        foundItem.totalPrice +=
-          calcPrice(price) + (foundItem.option.price || 0);
-        setQuantity((prev) => {
-          const temp = [...prev];
-          temp[index]++;
-          return temp;
-        });
-      } else if (buttonType === "substract") {
-        if (quantity[index] === 1) return;
-        foundItem.quantity--;
-        foundItem.totalPrice -=
-          calcPrice(price) + (foundItem.option.price || 0);
-        setQuantity((prev) => {
-          const temp = [...prev];
-          temp[index]--;
-          return temp;
-        });
-      }
-    }
-    setSelectedItemList(newSelectedItemList);
   };
-
-  const handleChange = (index: number, e: any) => {
-    const temp = [...quantity];
-    temp[index] = e.target.value;
-    setQuantity(temp);
-  };
-
-  const handleBlur = (index: number, e: any) => {
-    let _quantity = 0;
-    if (quantity[index] < 1) {
-      setQuantity((prev) => {
-        const temp = [...prev];
-        temp[index] = 1;
-        return temp;
-      });
-      _quantity = 1;
-    } else {
-      _quantity = e.target.value;
-    }
-    const newSelectedItemList = [...selectedItemList];
-    const foundItem = newSelectedItemList.find(
-      (newSelectedItem) => newSelectedItem.option.index === index,
-    );
-    if (foundItem) {
-      foundItem.quantity = _quantity;
-      foundItem.totalPrice =
-        (calcPrice(price) + (foundItem.option.price || 0)) * foundItem.quantity;
-    }
-    setSelectedItemList(newSelectedItemList);
-  };
-
-  useEffect(() => {
-    setTotalItemListPrice(
-      selectedItemList
-        .map((item) => item.totalPrice)
-        .reduce((acc, cur) => acc + cur, 0),
-    );
-
-    const totalQuantity = selectedItemList
-      .map((item) => item.quantity)
-      .reduce((acc, cur) => acc + cur, 0);
-    const regularPrice = totalQuantity * price;
-    const OptionPrice = selectedItemList
-      .map((item) => item.quantity * (item.option.price || 0))
-      .reduce((acc, cur) => acc + cur, 0);
-    setTotalOriginalPrice(regularPrice + OptionPrice);
-  }, [selectedItemList, price]);
 
   useEffect(() => {
     parentFunc(selectedItemList);
-  }, [selectedItemList]);
+  }, [selectedItemList, parentFunc]);
 
   return (
     <div className="option pt-10">
-      <div>
-        <label className="text-sm font-semibold sm:text-base">상품옵션</label>
-        <div
-          className={cls(
-            "dropbox-option mt-2 border text-xs font-semibold text-slate-500 sm:text-sm",
-            isOpenOption ? "border-orange-300" : "border-slate-300",
-          )}
+      <label className="text-sm font-semibold sm:text-base">상품옵션</label>
+      <div
+        className={cls(
+          "dropbox-option mt-2 border text-xs font-semibold",
+          isOpenOption ? "border-orange-300" : "border-slate-300",
+        )}
+      >
+        <button
+          className="flex w-full justify-between border p-2 hover:bg-orange-50"
+          onClick={handleOpenOption}
         >
-          <button
-            className={cls(
-              "flex w-full justify-between border p-2 hover:cursor-pointer hover:bg-orange-50",
-              isOpenOption ? "bg-slate-100" : "",
-            )}
-            onClick={handleOpenOption}
-          >
-            <span>상품명</span>
-            <span>🔽</span>
-          </button>
-
-          <ul
-            className={cls(
-              "flex w-full flex-col divide-y-2 border bg-white",
-              isOpenOption ? "" : "hidden",
-            )}
-          >
-            {options?.map((option) => (
+          <span>상품명</span>
+          <span>🔽</span>
+        </button>
+        {isOpenOption && (
+          <ul className="flex w-full flex-col divide-y-2 border bg-white">
+            {options.map((option) => (
               <li
                 key={option.index}
-                className="p-2 hover:cursor-pointer hover:bg-orange-50"
-                onClick={(e) => handleSelectOption(option.index, e)}
+                className="p-2 hover:bg-orange-50"
+                onClick={() => handleSelectOption(option.index)}
               >
-                {` ${option.index + 1} : ${option.name}`}{" "}
-                {option.price ? `(+${formatPrice(option.price)}원)` : ``}{" "}
-                {` | `}
+                {`${option.index + 1} : ${option.name}`}{" "}
+                {option.price ? `(+${formatPrice(option.price)}원)` : ""} |{" "}
                 {`${option.stock}개 남음`}
               </li>
             ))}
           </ul>
-        </div>
+        )}
       </div>
-
-      {/* select result */}
       <div className="pt-10">
-        <div className="flex  flex-col gap-y-5">
-          {selectedItemList?.map((item) => (
-            <div
-              key={item.option.id}
-              className="flex flex-col  gap-y-5 border-b-2 bg-slate-50 p-3"
-            >
-              <div className="flex justify-between">
-                <span>{item.option.name}</span>
+        {selectedItemList.map((item) => (
+          <div
+            key={item.option.id}
+            className="flex flex-col gap-y-5 border-b-2 bg-slate-50 p-3"
+          >
+            <div className="flex justify-between">
+              <span>{item.option.name}</span>
+              <button onClick={() => handleDeleteOption(item.option.index)}>
+                ❌
+              </button>
+            </div>
+            <div className="flex justify-between">
+              <div className="count-btn flex">
                 <button
-                  onClick={(e) => handleDeleteOption(item.option.index, e)}
+                  className="flex h-6 w-6 items-center justify-center border p-2 hover:bg-orange-50"
+                  onClick={() => handleQuantityChange(item.option.index, -1)}
                 >
-                  ❌
+                  -
+                </button>
+                <span className="h-6 w-10 text-center">{item.quantity}</span>
+                <button
+                  className="flex h-6 w-6 items-center justify-center border p-2 hover:bg-orange-50"
+                  onClick={() => handleQuantityChange(item.option.index, 1)}
+                >
+                  +
                 </button>
               </div>
-
-              <div className="flex justify-between">
-                <div className="count-btn flex text-black">
-                  <button
-                    className="flex h-6 w-6 items-center justify-center border p-2 hover:bg-orange-50"
-                    onClick={(e) =>
-                      handleButtonClick(item.option.index, "substract")
-                    }
-                  >
-                    -
-                  </button>
-
-                  <input
-                    className="flex h-6 w-10 items-center justify-center border text-center"
-                    type="number"
-                    value={quantity[item.option.index]}
-                    onChange={(e) => handleChange(item.option.index, e)}
-                    onBlur={(e) => {
-                      handleBlur(item.option.index, e);
-                    }}
-                  />
-                  <button
-                    className="flex h-6 w-6 items-center justify-center border p-2 hover:bg-orange-50"
-                    onClick={(e) => handleButtonClick(item.option.index, "add")}
-                  >
-                    +
-                  </button>
-                </div>
-
-                <div className="item-price">
-                  <span>{formatPrice(item.totalPrice)}원</span>
-                </div>
-              </div>
+              <span>{formatPrice(item.totalPrice)}원</span>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
         <div className="px-3 pt-5">
           {totalOriginalPrice - totalItemListPrice > 0 && (
             <div className="flex items-end justify-end gap-x-10">
-              <span className="text-sm font-semibold text-red-500 ">
+              <span className="text-sm font-semibold text-red-500">
                 추가 할인
               </span>
               <span className="w-36 text-right font-bold text-red-500">
@@ -292,7 +172,7 @@ const Options = ({ options, price, discount, parentFunc }: OptionsType) => {
               </span>
             </div>
           )}
-          <div className="flex items-end justify-end gap-x-10 ">
+          <div className="flex items-end justify-end gap-x-10">
             <span className="font-semibold text-slate-700">총 금액</span>
             <span className="w-36 text-right text-xl font-bold">
               {formatPrice(totalItemListPrice)}원
