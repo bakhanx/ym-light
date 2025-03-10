e58d14cc7c56d28069820a6dcf5d2ee439c5812d
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-03-10 22:06:37 +0900

    Refactor: 토큰 리턴값 변경

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-const JWT_SECERT = process.env.JWT_SECRET!;
+const JWT_SECRET = process.env.JWT_SECRET!;
 
 type TokenPayload = {
   userId: number;
@@ -8,7 +8,7 @@ type TokenPayload = {
 
 export const createToken = (userId: number) => {
   try {
-    const token = jwt.sign({ userId }, JWT_SECERT, { expiresIn: "1h" });
+    const token = jwt.sign({ userId }, JWT_SECRET, { expiresIn: "1h" });
     return token;
   } catch (error) {
     console.log("토큰생성 에러", error);
@@ -18,7 +18,7 @@ export const createToken = (userId: number) => {
 
 export const verifyToken = (token: string): TokenPayload | null => {
   try {
-    const decoded = jwt.verify(token, JWT_SECERT) as TokenPayload;
+    const decoded = jwt.verify(token, JWT_SECRET) as TokenPayload;
     return decoded;
   } catch (error) {
     console.log("Invalid Token", error);
@@ -26,13 +26,9 @@ export const verifyToken = (token: string): TokenPayload | null => {
   }
 };
 
-export const getUserIdFromToken = () => {
-  const token = sessionStorage.getItem("jwt-token");
-  if (token) {
-    const decodedToken = verifyToken(token);
-    if (decodedToken) {
-      return decodedToken.userId;
-    }
-  }
-  return null;
+export const getUserIdFromToken = async (
+  token: string,
+): Promise<number | null> => {
+  const decodedToken = verifyToken(token);
+  return decodedToken ? decodedToken.userId : null;
 };
