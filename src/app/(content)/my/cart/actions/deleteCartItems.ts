561cf45764c5a15f1723ded10737c8eff480d476
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2025-03-11 19:24:44 +0900

    Chore: 콘솔로그 제거

--- src/app/(content)/my/cart/actions/deleteCartItems.ts
+++ src/app/(content)/my/cart/actions/deleteCartItems.ts
@@ -6,7 +6,6 @@ import getSession from "@/utils/session";
 const deleteCartItems = async (productId: number) => {
   const session = await getSession();
   try {
-    console.log(productId);
     if (productId) {
       const deleteItems = await db.cartItem.deleteMany({
         where: {
